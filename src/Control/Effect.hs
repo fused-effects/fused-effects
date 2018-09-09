7df413938eafdca0dc64f32d68baf5151c61b14c
Rob Rix <rob.rix@me.com>
2018-09-08 20:52:33 -0400

    Define an effect datatype.

--- src/Control/Effect.hs
+++ src/Control/Effect.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE PolyKinds, TypeOperators #-}
 module Control.Effect where
 
+data Eff effects a
+  = Return a
+  | Eff (effects (Eff effects) (Eff effects a))
+
 data (f :+: g) (m :: * -> *) a
   = L (f m a)
   | R (g m a)
