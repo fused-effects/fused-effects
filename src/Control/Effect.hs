086cf0360295fe9f1dcd89278e924beebd850b40
Rob Rix <rob.rix@me.com>
2018-09-09 10:32:11 -0400

    Define a runM handler.

--- src/Control/Effect.hs
+++ src/Control/Effect.hs
@@ -40,6 +40,10 @@ instance Functor sig => Effect (Lift sig) where
 instance Subset (Lift IO) sig => MonadIO (Eff sig) where
   liftIO = inject . Lift . fmap pure
 
+runM :: Monad m => Eff (Lift m) a -> m a
+runM (Return a) = return a
+runM (Eff (Lift op)) = op >>= runM
+
 
 data (f :+: g) (m :: * -> *) a
   = L (f m a)
