cdfcaac85c63e41b84bf62c1af54cd0a46baa63b
Rob Rix <rob.rix@me.com>
2018-09-08 21:22:42 -0400

    Define Subset constraints.

--- src/Control/Effect.hs
+++ src/Control/Effect.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE EmptyCase, ExistentialQuantification, PolyKinds, TypeOperators #-}
+{-# LANGUAGE EmptyCase, ExistentialQuantification, FlexibleInstances, MultiParamTypeClasses, PolyKinds, TypeOperators #-}
 module Control.Effect where
 
 data Eff effects a
@@ -21,3 +21,22 @@ data NonDet m a
   = Empty
   | Choose (m a) (m a)
   deriving (Eq, Ord, Show)
+
+
+class Subset sub sup where
+  inj :: sub m a -> sup m a
+  prj :: sup m a -> Maybe (sub m a)
+
+instance Subset sub sub where
+  inj = id
+  prj = Just
+
+instance Subset sub (sub :+: sup) where
+  inj = L . inj
+  prj (L f) = Just f
+  prj _     = Nothing
+
+instance Subset sub sup => Subset sub (sub' :+: sup) where
+  inj = R . inj
+  prj (R g) = prj g
+  prj _     = Nothing
