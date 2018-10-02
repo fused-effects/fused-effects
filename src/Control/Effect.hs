46c6acc24cb89fd15846081e395dc50e0ca86f77
Rob Rix <rob.rix@me.com>
2018-10-02 12:50:44 -0400

    Define effects using the Monad Transformers and Modular Algebraic Effects—What Binds Them Together recipe.

--- src/Control/Effect.hs
+++ src/Control/Effect.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE DeriveFunctor, EmptyCase, ExistentialQuantification, FlexibleContexts, FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, PatternSynonyms, PolyKinds, RankNTypes, StandaloneDeriving, TypeOperators, UndecidableInstances, ViewPatterns #-}
+{-# LANGUAGE DeriveFunctor, EmptyCase, ExistentialQuantification, FlexibleContexts, FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, PolyKinds, RankNTypes, StandaloneDeriving, TypeOperators, UndecidableInstances, ViewPatterns #-}
 module Control.Effect where
 
 import Control.Applicative (Alternative(..), liftA2)
 import Control.Monad ((<=<), ap, join, liftM)
 import Control.Monad.Fail
 import Control.Monad.IO.Class
-import Data.Functor.Identity
 import Prelude hiding (fail)
 
 data Eff effects a
@@ -17,7 +16,11 @@ type f ~> g = forall x . f x -> g x
 class Effect sig where
   emap :: Monad m => (m a -> m b) -> (sig m a -> sig m b)
 
-  handle :: (Monad m, Monad n, Functor c) => c () -> (forall x . c (m x) -> n (c x)) -> (sig m a -> sig n (c a))
+  handle :: (Carrier c, Functor (c n), Monad m, Monad n)
+         => (forall x . c n (m x) -> n (c n x))
+         -> sig m a
+         -> sig n (c n a)
+
 
 inject :: Subset effect sig => effect (Eff sig) a -> Eff sig a
 inject = Eff . inj
@@ -201,7 +204,7 @@ data Void m a
 
 instance Effect Void where
   emap _ v = case v of {}
-  handle _ _ v = case v of {}
+  handle _ v = case v of {}
 
 run :: Eff Void a -> a
 run (Return a) = a
@@ -213,7 +216,7 @@ newtype Lift sig m a = Lift { unLift :: sig (m a) }
 
 instance Functor sig => Effect (Lift sig) where
   emap f (Lift op) = Lift (fmap f op)
-  handle state handler (Lift op) = Lift (fmap (\ p -> handler (p <$ state)) op)
+  handle handler (Lift op) = Lift (fmap (handler . gen) op)
 
 instance Subset (Lift IO) sig => MonadIO (Eff sig) where
   liftIO = inject . Lift . fmap pure
@@ -232,12 +235,8 @@ instance (Effect l, Effect r) => Effect (l :+: r) where
   emap f (L l) = L (emap f l)
   emap f (R r) = R (emap f r)
 
-  handle state handler (L l) = L (handle state handler l)
-  handle state handler (R r) = R (handle state handler r)
-
-
-pattern Other :: r (Eff (l :+: r)) a -> Eff (l :+: r) a
-pattern Other s = Eff (R s)
+  handle handler (L l) = L (handle handler l)
+  handle handler (R r) = R (handle handler r)
 
 (\/) :: ( sig1           m a -> b)
      -> (          sig2  m a -> b)
@@ -247,210 +246,153 @@ pattern Other s = Eff (R s)
 
 
 data NonDet m a
-  = Empty'
-  | Choose' (Bool -> m a)
+  = Empty
+  | Choose (Bool -> m a)
   deriving (Functor)
 
 instance Effect NonDet where
-  emap _ Empty' = Empty'
-  emap f (Choose' k) = Choose' (f . k)
-
-  handle _ _ Empty' = Empty'
-  handle state handler (Choose' k) = Choose' (handler . (<$ state) . k)
-
-pattern Empty :: Subset NonDet effects => Eff effects a
-pattern Empty <- (project -> Just Empty')
-
-pattern Choose :: Subset NonDet effects => (Bool -> Eff effects a) -> Eff effects a
-pattern Choose k <- (project -> Just (Choose' k))
-
-{-# COMPLETE Return, Empty, Choose, Other #-}
+  emap _ Empty      = Empty
+  emap f (Choose k) = Choose (f . k)
+  handle _       Empty      = Empty
+  handle handler (Choose k) = Choose (handler . gen . k)
 
 instance Subset NonDet sig => Alternative (Eff sig) where
-  empty = inject Empty'
-  l <|> r = inject (Choose' (\ c -> if c then l else r))
+  empty = inject Empty
+  l <|> r = inject (Choose (\ c -> if c then l else r))
 
 runNonDet :: Effect sig => Eff (NonDet :+: sig) a -> Eff sig [a]
-runNonDet (Return a) = pure [a]
-runNonDet Empty      = pure []
-runNonDet (Choose k) = (++) <$> runNonDet (k True) <*> runNonDet (k False)
-runNonDet (Other op) = Eff (handle [()] (fmap join . traverse runNonDet) op)
+runNonDet = runListH . relay alg
+  where alg Empty      = ListH (pure [])
+        alg (Choose k) = join (ListH (liftA2 (++) (runNonDet (k True)) (runNonDet (k False))))
 
 
 data Reader r m a
-  = Ask' (r -> m a)
-  | forall b . Local' (r -> r) (m b) (b -> m a)
+  = Ask (r -> m a)
+  | forall b . Local (r -> r) (m b) (b -> m a)
 
 deriving instance Functor m => Functor (Reader r m)
 
 instance Effect (Reader r) where
+  emap f (Ask k) = Ask (f . k)
+  emap f (Local g m k) = Local g m (f . k)
 
-  emap f (Ask' k) = Ask' (f . k)
-  emap f (Local' g m k) = Local' g m (f . k)
-
-  handle state handler (Ask' k) = Ask' (handler . (<$ state) . k)
-  handle state handler (Local' f m k) = Local' f (handler (m <$ state)) (handler . fmap k)
-
-pattern Ask :: Subset (Reader r) effects => (r -> Eff effects a) -> Eff effects a
-pattern Ask k <- (project -> Just (Ask' k))
-
-pattern Local :: Subset (Reader r) effects => (r -> r) -> Eff effects b -> (b -> Eff effects a) -> Eff effects a
-pattern Local f m k <- (project -> Just (Local' f m k))
-
-{-# COMPLETE Return, Ask, Local, Other #-}
+  handle handler (Ask k)       = Ask (handler . gen . k)
+  handle handler (Local f m k) = Local f (handler (gen m)) (handler . fmap k)
 
 ask :: Subset (Reader r) sig => Eff sig r
-ask = inject (Ask' pure)
+ask = inject (Ask pure)
 
 local :: Subset (Reader r) sig => (r -> r) -> Eff sig a -> Eff sig a
-local f m = inject (Local' f m pure)
+local f m = inject (Local f m pure)
+
 
 runReader :: Effect sig => r -> Eff (Reader r :+: sig) a -> Eff sig a
-runReader _ (Return a)    = pure a
-runReader r (Ask k)       = runReader r (k r)
-runReader r (Local f m k) = runReader (f r) m >>= runReader r . k
-runReader r (Other op)    = runIdentity <$> Eff (handle (Identity ()) (fmap Identity . runReader r . runIdentity) op)
+runReader r m = runReaderH (relay alg m) r
+  where alg (Ask k)       = join (ReaderH (\ r -> runReader r (k r)))
+        alg (Local f m k) = join (ReaderH (\ r -> let fr = f r in fr `seq` runReader fr m >>= runReader r . k))
 
 
 data State s m a
-  = Get' (s -> m a)
-  | Put' s (m a)
+  = Get (s -> m a)
+  | Put s (m a)
   deriving (Functor)
 
 instance Effect (State s) where
-  emap f (Get' k) = Get' (f . k)
-  emap f (Put' s k) = Put' s (f k)
+  emap f (Get k) = Get (f . k)
+  emap f (Put s k) = Put s (f k)
 
-  handle state handler (Get' k) = Get' (handler . (<$ state) . k)
-  handle state handler (Put' s k) = Put' s (handler (k <$ state))
-
-pattern Get :: Subset (State s) effects => (s -> Eff effects a) -> Eff effects a
-pattern Get k <- (project -> Just (Get' k))
-
-pattern Put :: Subset (State s) effects => s -> Eff effects a -> Eff effects a
-pattern Put s k <- (project -> Just (Put' s k))
-
-{-# COMPLETE Return, Get, Put, Other #-}
+  handle handler (Get k)   = Get   (handler . gen . k)
+  handle handler (Put s k) = Put s (handler  (gen   k))
 
 get :: Subset (State s) sig => Eff sig s
-get = inject (Get' pure)
+get = inject (Get pure)
 
 put :: Subset (State s) sig => s -> Eff sig ()
-put s = inject (Put' s (pure ()))
+put s = inject (Put s (pure ()))
 
 runState :: Effect sig => s -> Eff (State s :+: sig) a -> Eff sig (s, a)
-runState s (Return a) = pure (s, a)
-runState s (Get k)    = runState s (k s)
-runState _ (Put s k)  = runState s k
-runState s (Other op) = Eff (handle (s, ()) (uncurry runState) op)
+runState s m = runStateH (relay alg m) s
+  where alg (Get k)   = join (StateH (\ s -> runState s (k s)))
+        alg (Put s k) = join (StateH (\ _ -> runState s  k   ))
 
 
-data Fail m a = Fail' String
+newtype Fail m a = Fail String
   deriving (Functor)
 
 instance Effect Fail where
-  emap _ (Fail' s) = Fail' s
-
-  handle _ _ (Fail' s) = Fail' s
-
-pattern Fail :: Subset Fail effects => String -> Eff effects a
-pattern Fail s <- (project -> Just (Fail' s))
-
-{-# COMPLETE Return, Fail, Other #-}
+  emap _ (Fail s) = Fail s
+  handle _ (Fail s) = Fail s
 
 instance Subset Fail sig => MonadFail (Eff sig) where
-  fail = inject . Fail'
+  fail = inject . Fail
 
 runFail :: Effect sig => Eff (Fail :+: sig) a -> Eff sig (Either String a)
-runFail (Return a) = pure (Right a)
-runFail (Fail s)   = pure (Left s)
-runFail (Other op) = Eff (handle (Right ()) (either (pure . Left) runFail) op)
+runFail = runEitherH . relay alg
+  where alg (Fail s) = EitherH (pure (Left s))
 
 
 data Exc exc m a
-  = Throw' exc
-  | forall b . Catch' (m b) (exc -> m b) (b -> m a)
+  = Throw exc
+  | forall b . Catch (m b) (exc -> m b) (b -> m a)
 
 deriving instance Functor m => Functor (Exc exc m)
 
 instance Effect (Exc exc) where
-  emap _ (Throw' exc) = Throw' exc
-  emap f (Catch' m h k) = Catch' m h (f . k)
+  emap _ (Throw exc)   = Throw exc
+  emap f (Catch m h k) = Catch m h (f . k)
 
-  handle _ _ (Throw' exc) = Throw' exc
-  handle state handler (Catch' m h k) = Catch' (handler (m <$ state)) (handler . (<$ state) . h) (handler . fmap k)
-
-pattern Throw :: Subset (Exc exc) effects => exc -> Eff effects a
-pattern Throw exc <- (project -> Just (Throw' exc))
-
-pattern Catch :: Subset (Exc exc) effects => Eff effects b -> (exc -> Eff effects b) -> (b -> Eff effects a) -> Eff effects a
-pattern Catch m h k <- (project -> Just (Catch' m h k))
-
-{-# COMPLETE Return, Throw, Catch, Other #-}
+  handle _       (Throw exc)   = Throw exc
+  handle handler (Catch m h k) = Catch (handler (gen m)) (handler . gen . h) (handler . fmap k)
 
 throw :: Subset (Exc exc) sig => exc -> Eff sig a
-throw = inject . Throw'
+throw = inject . Throw
 
 catch :: Subset (Exc exc) sig => Eff sig a -> (exc -> Eff sig a) -> Eff sig a
-catch m h = inject (Catch' m h pure)
+catch m h = inject (Catch m h pure)
 
 runExc :: Effect sig => Eff (Exc exc :+: sig) a -> Eff sig (Either exc a)
-runExc (Return a)    = pure (Right a)
-runExc (Throw e)     = pure (Left e)
-runExc (Catch m h k) = runExc m >>= either (either (pure . Left) (runExc . k) <=< runExc . h) (runExc . k)
-runExc (Other op)    = Eff (handle (Right ()) (either (pure . Left) runExc) op)
+runExc = runEitherH . relay alg
+  where alg :: Effect sig => Exc exc (Eff (Exc exc :+: sig)) (EitherH exc (Eff sig) a) -> EitherH exc (Eff sig) a
+        alg (Throw e)     = EitherH (pure (Left e))
+        alg (Catch m h k) = join (EitherH (runExc m >>= runExc . either (k <=< h) k))
 
 
 data Resumable exc m a
-  = forall b . Resumable' (exc b) (b -> m a)
+  = forall b . Resumable (exc b) (b -> m a)
 
 deriving instance Functor m => Functor (Resumable exc m)
 
 instance Effect (Resumable exc) where
-  emap f (Resumable' exc k) = Resumable' exc (f . k)
+  emap f (Resumable exc k) = Resumable exc (f . k)
 
-  handle state handler (Resumable' exc k) = Resumable' exc (handler . (<$ state) . k)
-
-pattern Resumable :: Subset (Resumable exc) effects => exc b -> (b -> Eff effects a) -> Eff effects a
-pattern Resumable exc k <- (project -> Just (Resumable' exc k))
-
-{-# COMPLETE Return, Resumable, Other #-}
+  handle handler (Resumable exc k) = Resumable exc (handler . gen . k)
 
 throwResumable :: Subset (Resumable exc) sig => exc a -> Eff sig a
-throwResumable exc = inject (Resumable' exc pure)
+throwResumable exc = inject (Resumable exc pure)
 
 runResumable :: Effect sig => (forall resume . exc resume -> Eff sig resume) -> Eff (Resumable exc :+: sig) a -> Eff sig a
-runResumable _ (Return a)        = pure a
-runResumable f (Resumable exc k) = f exc >>= runResumable f . k
-runResumable f (Other op)        = runIdentity <$> Eff (handle (Identity ()) (fmap Identity . runResumable f . runIdentity) op)
+runResumable f = runIdH . relay alg
+  where alg (Resumable exc k) = join (IdH (f exc >>= runResumable f . k))
 
 
 data Cut m a
-  = Cut'
-  | forall b . Call' (m b) (b -> m a)
+  = Cut
+  | forall b . Call (m b) (b -> m a)
 
 deriving instance Functor m => Functor (Cut m)
 
 instance Effect Cut where
-  emap _ Cut' = Cut'
-  emap f (Call' m k) = Call' m (f . k)
+  emap _ Cut = Cut
+  emap f (Call m k) = Call m (f . k)
 
-  handle _ _ Cut' = Cut'
-  handle state handler (Call' m k) = Call' (handler (m <$ state)) (handler . fmap k)
-
-pattern Cut :: Subset Cut sig => Eff sig a
-pattern Cut <- (project -> Just Cut')
-
-pattern Call :: Subset Cut sig => Eff sig b -> (b -> Eff sig a) -> Eff sig a
-pattern Call m k <- (project -> Just (Call' m k))
-
-{-# COMPLETE Return, Cut, Call, Other #-}
+  handle _ Cut = Cut
+  handle handler (Call m k) = Call (handler (gen m)) (handler . fmap k)
 
 cutfail :: Subset Cut sig => Eff sig a
-cutfail = inject Cut'
+cutfail = inject Cut
 
 call :: Subset Cut sig => Eff sig a -> Eff sig a
-call m = inject (Call' m pure)
+call m = inject (Call m pure)
 
 cut :: (Subset NonDet sig, Subset Cut sig) => Eff sig ()
 cut = skip <|> cutfail
@@ -470,21 +412,16 @@ skip = pure ()
 
 
 data Symbol m a
-  = Symbol' (Char -> Bool) (Char -> m a)
+  = Symbol (Char -> Bool) (Char -> m a)
   deriving (Functor)
 
 instance Effect Symbol where
-  emap f (Symbol' sat k) = Symbol' sat (f . k)
+  emap f (Symbol sat k) = Symbol sat (f . k)
 
-  handle state handler (Symbol' sat k) = Symbol' sat (handler . (<$ state) . k)
-
-pattern Symbol :: Subset Symbol sig => (Char -> Bool) -> (Char -> Eff sig a) -> Eff sig a
-pattern Symbol sat k <- (project -> Just (Symbol' sat k))
-
-{-# COMPLETE Return, Symbol, Other #-}
+  handle handler (Symbol sat k) = Symbol sat (handler . gen . k)
 
 satisfy :: Subset Symbol sig => (Char -> Bool) -> Eff sig Char
-satisfy sat = inject (Symbol' sat pure)
+satisfy sat = inject (Symbol sat pure)
 
 char :: Subset Symbol sig => Char -> Eff sig Char
 char c = satisfy (== c)
