fdc5dc9d93abab5d1e77b92c815224f05a69a02f
Rob Rix <rob.rix@me.com>
2018-10-02 11:35:35 -0400

    Define a Monad instance for ReaderH.

--- src/Control/Effect.hs
+++ src/Control/Effect.hs
@@ -76,6 +76,11 @@ instance Applicative m => Applicative (ReaderH r m) where
 
   ReaderH f <*> ReaderH a = ReaderH (\ r -> f r <*> a r)
 
+instance Monad m => Monad (ReaderH r m) where
+  return = pure
+
+  ReaderH a >>= f = ReaderH (\ r -> a r >>= \ a' -> runReaderH (f a') r)
+
 instance Carrier (ReaderH r) where
   joinl mf = ReaderH (\ r -> mf >>= \ f -> runReaderH f r)
 
