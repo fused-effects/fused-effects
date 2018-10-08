47f49f7a8be5ba33e9fd6f329191fa77688635a3
Rob Rix <rob.rix@me.com>
2018-10-08 16:12:51 -0400

    Define a TermAlgebra instance for Eff.

--- src/Control/Algebra.hs
+++ src/Control/Algebra.hs
@@ -1,6 +1,12 @@
-{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances, FunctionalDependencies #-}
 module Control.Algebra where
+
+import Control.Effect
 
 class TermAlgebra h f | h -> f where
   var :: a -> h a
   con :: f h (h a) -> h a
+
+instance TermAlgebra (Eff sig) sig where
+  var = Return
+  con = Eff
