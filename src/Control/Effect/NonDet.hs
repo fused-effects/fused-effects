339608be7c286100d5dc5f39441561e746cd31eb
Rob Rix <rob.rix@me.com>
2018-10-08 12:00:32 -0400

    Define a runNonDetSplit handler.

--- src/Control/Effect/NonDet.hs
+++ src/Control/Effect/NonDet.hs
@@ -3,13 +3,20 @@ module Control.Effect.NonDet
 ( NonDet(..)
 , Alternative(..)
 , runNonDet
+, runNonDetSplit
 ) where
 
 import Control.Applicative (Alternative(..), liftA2)
 import Control.Carrier.List
+import Control.Carrier.Split
 import Control.Effect
 
 runNonDet :: Effect sig => Eff (NonDet :+: sig) a -> Eff sig [a]
 runNonDet = runListH . relay alg
   where alg Empty      = ListH (pure [])
         alg (Choose k) = ListH (liftA2 (++) (runListH (k True)) (runListH (k False)))
+
+runNonDetSplit :: Effect sig => Eff (NonDet :+: sig) a -> Eff sig [a]
+runNonDetSplit = joinSplitH . relay alg
+  where alg Empty      = empty
+        alg (Choose k) = SplitH (runSplitH (k True) >>= maybe (runSplitH (k False)) (\ (a, q) -> pure (Just (a, q <|> k False))))
