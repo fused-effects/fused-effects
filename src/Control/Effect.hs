a29d19080a81414fbbd98b966e4e93d71896f771
Rob Rix <rob.rix@me.com>
2018-10-08 15:36:41 -0400

    Define a reinterpret2_2 handler.

--- src/Control/Effect.hs
+++ src/Control/Effect.hs
@@ -10,6 +10,7 @@ module Control.Effect
 , reinterpret
 , reinterpret2
 , reinterpret_2
+, reinterpret2_2
 , reinterpretRest
 , Effect(..)
 , Carrier(..)
@@ -104,6 +105,13 @@ reinterpret_2 :: (Effect eff, Effect sig, Effect new1, Effect new2, Carrier c f,
              -> (forall a . Eff (eff :+: sig) a -> c (Eff (new1 :+: new2 :+: sig)) a)
 reinterpret_2 alg = foldA (alg \/ reinterpretRest)
 {-# INLINE reinterpret_2 #-}
+
+reinterpret2_2 :: (Effect eff1, Effect eff2, Effect sig, Effect new1, Effect new2, Carrier c f, Monad (c (Eff (new1 :+: new2 :+: sig))))
+             => (forall a . eff1 (c (Eff (new1 :+: new2 :+: sig))) (c (Eff (new1 :+: new2 :+: sig)) a) -> c (Eff (new1 :+: new2 :+: sig)) a)
+             -> (forall a . eff2 (c (Eff (new1 :+: new2 :+: sig))) (c (Eff (new1 :+: new2 :+: sig)) a) -> c (Eff (new1 :+: new2 :+: sig)) a)
+             -> (forall a . Eff (eff1 :+: eff2 :+: sig) a -> c (Eff (new1 :+: new2 :+: sig)) a)
+reinterpret2_2 alg1 alg2 = foldA (alg1 \/ alg2 \/ reinterpretRest)
+{-# INLINE reinterpret2_2 #-}
 
 reinterpretRest :: (Effect sig, Effect new, Carrier c f, Monad (c (Eff (new :+: sig))))
                 => sig (c (Eff (new :+: sig))) (c (Eff (new :+: sig)) a)
