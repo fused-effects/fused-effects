2589478a7dbdf94e33f9f44eb30f88a907a54d5e
Rob Rix <rob.rix@me.com>
2018-10-08 11:44:21 -0400

    Move SplitH into its own module.

--- src/Control/Effect.hs
+++ src/Control/Effect.hs
@@ -30,9 +30,9 @@ module Control.Effect
 -- , parse
 ) where
 
-import Control.Applicative (Alternative(..), liftA2)
-import Control.Arrow ((***))
+import Control.Applicative (Alternative(..))
 import Control.Carrier
+import Control.Carrier.Split
 import Control.Monad (ap, join, liftM)
 import Control.Monad.Fail
 import Control.Monad.IO.Class
@@ -87,43 +87,6 @@ relay :: (Effect eff, Effect sig, Carrier c f, Monad (c (Eff sig)))
       -> (forall a . Eff (eff :+: sig) a -> c (Eff sig) a)
 relay alg = foldA (liftAlg alg)
 {-# INLINE relay #-}
-
-
-newtype SplitH m a = SplitH { runSplitH :: m (Maybe (a, SplitH m a)) }
-  deriving (Functor)
-
-joinSplitH :: Monad m => SplitH m a -> m [a]
-joinSplitH = (>>= maybe (pure []) (\ (a, q) -> (a :) <$> joinSplitH q)) . runSplitH
-
-altSplitH :: (Alternative m, Monad m) => SplitH m a -> m a
-altSplitH = (>>= maybe empty (\ (a, q) -> pure a <|> altSplitH q)) . runSplitH
-
-instance Applicative m => Applicative (SplitH m) where
-  pure a = SplitH (pure (Just (a, SplitH (pure Nothing))))
-
-  liftA2 f a b = SplitH (liftA2 (liftA2 (uncurry (***) . (f *** liftA2 f))) (runSplitH a) (runSplitH b))
-
-instance Monad m => Alternative (SplitH m) where
-  empty = SplitH (pure Nothing)
-
-  a <|> b = SplitH (runSplitH a >>= maybe (runSplitH b) (\ (a', q) -> pure (Just (a', q <|> b))))
-
-instance Monad m => Monad (SplitH m) where
-  return = pure
-
-  a >>= k = SplitH (runSplitH a >>= runSplitH . maybe empty (\ (a', q) -> k a' <|> (q >>= k)))
-
-instance Carrier SplitH [] where
-  joinl a = SplitH (a >>= runSplitH)
-
-  suspend = SplitH (pure (Just ([()], empty)))
-
-  resume []     = pure []
-  resume (a:as) = runSplitH a >>= maybe (resume as) (\ (a', q) -> (a' :) <$> resume (q : as))
-
-  wrap a = SplitH (a >>= \ a' -> case a' of
-    []     -> pure Nothing
-    a'':as -> pure (Just (a'', wrap (pure as))))
 
 
 data Void m k
