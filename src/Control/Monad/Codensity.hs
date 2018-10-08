7cfd60c627f11c19111b37e3a37a42bf70a75ea2
Rob Rix <rob.rix@me.com>
2018-10-08 16:40:47 -0400

    Define Functor, Applicative, & Monad instances for Codensity.

--- src/Control/Monad/Codensity.hs
+++ src/Control/Monad/Codensity.hs
@@ -1,4 +1,19 @@
 {-# LANGUAGE RankNTypes #-}
 module Control.Monad.Codensity where
 
+import Control.Monad (ap, liftM)
+
 newtype Codensity h a = Codensity { runCodensity :: forall x . (a -> h x) -> h x }
+
+instance Functor (Codensity h) where
+  fmap = liftM
+
+instance Applicative (Codensity h) where
+  pure a = Codensity ($ a)
+
+  (<*>) = ap
+
+instance Monad (Codensity h) where
+  return = pure
+
+  Codensity m >>= f = Codensity (\ k -> m (\ a -> runCodensity (f a) k))
