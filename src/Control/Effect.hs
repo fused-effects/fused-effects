0d25a59ed75b5930d82fb26d0a819d2f64d21196
Rob Rix <rob.rix@me.com>
2018-10-08 11:50:10 -0400

    Move Cut into its own module.

--- src/Control/Effect.hs
+++ src/Control/Effect.hs
@@ -16,23 +16,18 @@ module Control.Effect
 , runM
 , NonDet(..)
 , Fail(..)
-, Cut(..)
-, cutfail
-, call
-, cut
 , Symbol(..)
 , satisfy
 , char
 , digit
-, expr
-, term
-, factor
+-- , expr
+-- , term
+-- , factor
 -- , parse
 ) where
 
 import Control.Applicative (Alternative(..))
 import Control.Carrier
-import Control.Carrier.Split
 import Control.Monad (ap, join, liftM)
 import Control.Monad.Fail
 import Control.Monad.IO.Class
@@ -166,42 +161,6 @@ instance Subset Fail sig => MonadFail (Eff sig) where
   fail = send . Fail
 
 
-data Cut m k
-  = Cut
-  | forall b . Call (m b) (b -> k)
-
-deriving instance Functor (Cut m)
-
-instance Effect Cut where
-  hfmap _ Cut        = Cut
-  hfmap f (Call m k) = Call (f m) k
-
-  handle _     Cut        = Cut
-  handle state (Call m k) = Call (resume (m <$ state)) (wrap . resume . fmap k)
-
-cutfail :: Subset Cut sig => Eff sig a
-cutfail = send Cut
-
-call :: Subset Cut sig => Eff sig a -> Eff sig a
-call m = send (Call m pure)
-
-cut :: (Subset NonDet sig, Subset Cut sig) => Eff sig ()
-cut = skip <|> cutfail
-
-skip :: Applicative m => m ()
-skip = pure ()
-
--- runCut :: Subset NonDet sig => Eff (Cut :+: sig) a -> Eff sig a
--- runCut = go empty
---   where go :: Subset NonDet sig => Eff sig a -> Eff (Cut :+: sig) a -> Eff sig a
---         go q (Return a) = pure a <|> q
---         go q Empty      = q
---         go _ Cut        = empty
---         go q (Choose k) = go (go q (k False)) (k True)
---         go q (Call m k) = go empty m >>= go q . k
---         go q (Other op) = Eff (hfmap (go empty) op) <|> q
-
-
 data Symbol m k
   = Symbol (Char -> Bool) (Char -> k)
   deriving (Functor)
@@ -220,19 +179,19 @@ char c = satisfy (== c)
 digit :: (Subset NonDet sig, Subset Symbol sig) => Eff sig Char
 digit = foldr ((<|>) . char) empty ['0'..'9']
 
-expr :: (Subset Cut sig, Subset NonDet sig, Subset Symbol sig) => Eff sig Int
-expr = do
-  i <- term
-  call ((i +) <$ char '+' <* cut <*> expr) <|> pure i
+-- expr :: (Subset Cut sig, Subset NonDet sig, Subset Symbol sig) => Eff sig Int
+-- expr = do
+--   i <- term
+--   call ((i +) <$ char '+' <* cut <*> expr) <|> pure i
 
-term :: (Subset Cut sig, Subset NonDet sig, Subset Symbol sig) => Eff sig Int
-term = do
-  i <- factor
-  call ((i *) <$ char '*' <* cut <*> term) <|> pure i
+-- term :: (Subset Cut sig, Subset NonDet sig, Subset Symbol sig) => Eff sig Int
+-- term = do
+--   i <- factor
+--   call ((i *) <$ char '*' <* cut <*> term) <|> pure i
 
-factor :: (Subset Cut sig, Subset NonDet sig, Subset Symbol sig) => Eff sig Int
-factor = read <$> some digit
-     <|> char '(' *> expr <* char ')'
+-- factor :: (Subset Cut sig, Subset NonDet sig, Subset Symbol sig) => Eff sig Int
+-- factor = read <$> some digit
+--      <|> char '(' *> expr <* char ')'
 
 -- parse :: Subset NonDet sig => String -> Eff (Symbol :+: sig) a -> Eff sig a
 -- parse input = fmap snd . flip runStateH input . relay alg
--- src/Control/Effect/Cut.hs
+++ src/Control/Effect/Cut.hs
@@ -1,1 +1,40 @@
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, FlexibleContexts, StandaloneDeriving, TypeOperators #-}
 module Control.Effect.Cut where
+
+import Control.Applicative (Alternative(..))
+import Control.Effect
+
+data Cut m k
+  = Cut
+  | forall b . Call (m b) (b -> k)
+
+deriving instance Functor (Cut m)
+
+instance Effect Cut where
+  hfmap _ Cut        = Cut
+  hfmap f (Call m k) = Call (f m) k
+
+  handle _     Cut        = Cut
+  handle state (Call m k) = Call (resume (m <$ state)) (wrap . resume . fmap k)
+
+cutfail :: Subset Cut sig => Eff sig a
+cutfail = send Cut
+
+call :: Subset Cut sig => Eff sig a -> Eff sig a
+call m = send (Call m pure)
+
+cut :: (Subset NonDet sig, Subset Cut sig) => Eff sig ()
+cut = skip <|> cutfail
+
+skip :: Applicative m => m ()
+skip = pure ()
+
+-- runCut :: Subset NonDet sig => Eff (Cut :+: sig) a -> Eff sig a
+-- runCut = go empty
+--   where go :: Subset NonDet sig => Eff sig a -> Eff (Cut :+: sig) a -> Eff sig a
+--         go q (Return a) = pure a <|> q
+--         go q Empty      = q
+--         go _ Cut        = empty
+--         go q (Choose k) = go (go q (k False)) (k True)
+--         go q (Call m k) = go empty m >>= go q . k
+--         go q (Other op) = Eff (hfmap (go empty) op) <|> q
