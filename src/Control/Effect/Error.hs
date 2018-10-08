e45d619110da04b4b1ee4f1db47a653c7b70386a
Rob Rix <rob.rix@me.com>
2018-10-08 13:19:46 -0400

    Rename runExc to runError.

--- src/Control/Effect/Error.hs
+++ src/Control/Effect/Error.hs
@@ -25,7 +25,7 @@ catch :: Subset (Error exc) sig => Eff sig a -> (exc -> Eff sig a) -> Eff sig a
 catch m h = send (Catch m h pure)
 
 
-runExc :: Effect sig => Eff (Error exc :+: sig) a -> Eff sig (Either exc a)
-runExc = runEitherH . relay alg
+runError :: Effect sig => Eff (Error exc :+: sig) a -> Eff sig (Either exc a)
+runError = runEitherH . relay alg
   where alg (Throw e)     = EitherH (pure (Left e))
         alg (Catch m h k) = EitherH (runEitherH m >>= runEitherH . either (k <=< h) k)
