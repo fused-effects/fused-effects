5168a5b037c7deb8586fb22b5d56dae928e4c9b4
Rob Rix <rob.rix@me.com>
2018-10-09 12:25:58 -0400

    Run Error in the Codensity monad.

--- src/Control/Effect/Error.hs
+++ src/Control/Effect/Error.hs
@@ -1,9 +1,10 @@
-{-# LANGUAGE DeriveFunctor, ExistentialQuantification, FlexibleContexts, StandaloneDeriving, TypeOperators #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, StandaloneDeriving, TypeOperators, UndecidableInstances #-}
 module Control.Effect.Error where
 
-import Control.Carrier.Either
+import Control.Applicative (liftA2)
 import Control.Effect
 import Control.Monad ((<=<))
+import Control.Monad.Codensity
 
 data Error exc m k
   = Throw exc
@@ -25,7 +26,33 @@ catch :: Subset (Error exc) sig => Eff sig a -> (exc -> Eff sig a) -> Eff sig a
 catch m h = send (Catch m h pure)
 
 
-runError :: TermMonad m sig => Eff (Error exc :+: sig) a -> m (Either exc a)
-runError = runEitherH . interpret alg
-  where alg (Throw e)     = EitherH (pure (Left e))
-        alg (Catch m h k) = EitherH (runEitherH m >>= runEitherH . either (k <=< h) k)
+runError :: TermMonad m sig => Codensity (EitherH exc m) a -> m (Either exc a)
+runError = runEitherH . runCodensity var
+
+newtype EitherH e m a = EitherH { runEitherH :: m (Either e a) }
+  deriving (Functor)
+
+instance Applicative m => Applicative (EitherH e m) where
+  pure a = EitherH (pure (Right a))
+
+  EitherH f <*> EitherH a = EitherH (liftA2 (<*>) f a)
+
+instance Monad m => Monad (EitherH e m) where
+  return = pure
+
+  EitherH a >>= f = EitherH (a >>= either (pure . Left) (runEitherH . f))
+
+instance Carrier (Either e) (EitherH e) where
+  joinl mf = EitherH (mf >>= runEitherH)
+
+  suspend f = f (Right ())
+
+  resume = either (pure . Left) runEitherH
+
+  wrap = EitherH
+
+instance TermMonad m sig => TermAlgebra (EitherH e m) (Error e :+: sig) where
+  var = gen
+  con = alg \/ interpretRest
+    where alg (Throw e)     = EitherH (pure (Left e))
+          alg (Catch m h k) = EitherH (runEitherH m >>= runEitherH . either (k <=< h) k)
