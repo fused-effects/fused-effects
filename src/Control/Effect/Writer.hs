cf34196749640a596a49317628802e7aac6b2fb1
Rob Rix <rob.rix@me.com>
2018-10-09 11:57:39 -0400

    Run Writer effects in the Codensity monad.

--- src/Control/Effect/Writer.hs
+++ src/Control/Effect/Writer.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE DeriveFunctor, FlexibleContexts, PolyKinds, TypeOperators #-}
+{-# LANGUAGE DeriveFunctor, FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, PolyKinds, TypeOperators, UndecidableInstances #-}
 module Control.Effect.Writer where
 
-import Control.Carrier.Writer
+import Control.Applicative (liftA2)
 import Control.Effect
+import Control.Monad.Codensity
 import Data.Bifunctor (first)
 
 data Writer w m k = Tell w k
@@ -16,7 +17,37 @@ instance Effect (Writer w) where
 tell :: Subset (Writer w) sig => w -> Eff sig ()
 tell w = send (Tell w (pure ()))
 
+runWriter :: (TermMonad m sig, Monoid w) => Codensity (WriterH w m) a -> m (w, a)
+runWriter m = runWriterH (runCodensity var m)
 
-runWriter :: (TermMonad m sig, Monoid w) => Eff (Writer w :+: sig) a -> m (w, a)
-runWriter m = runWriterH (interpret alg m)
-  where alg (Tell w k) = WriterH (first (w <>) <$> runWriterH k)
+newtype WriterH w m a = WriterH { runWriterH :: m (w, a) }
+  deriving (Functor)
+
+instance (Monoid w, Applicative m) => Applicative (WriterH w m) where
+  pure a = WriterH (pure (mempty, a))
+
+  WriterH f <*> WriterH a = WriterH (liftA2 (<*>) f a)
+
+instance (Monoid w, Monad m) => Monad (WriterH w m) where
+  return = pure
+
+  WriterH a >>= f = WriterH (do
+    (w1, a') <- a
+    let fa = f a'
+    (w2, a'') <- fa `seq` runWriterH fa
+    let w = w1 <> w2
+    w `seq` pure (w, a''))
+
+instance Monoid w => Carrier ((,) w) (WriterH w) where
+  joinl mf = WriterH (mf >>= runWriterH)
+
+  suspend f = f (mempty, ())
+
+  resume (w, m) = first (w <>) <$> runWriterH m
+
+  wrap = WriterH
+
+instance (Monoid w, TermMonad m sig) => TermAlgebra (WriterH w m) (Writer w :+: sig) where
+  var = gen
+  con = alg \/ interpretRest
+    where alg (Tell w k) = WriterH (first (w <>) <$> runWriterH k)
