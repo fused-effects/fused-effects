a39f9a62c201dafc818a35a3fe04f9a5ca36e491
Rob Rix <rob.rix@me.com>
2018-10-09 11:58:25 -0400

    :fire: the Applicative instance for WriterH.

--- src/Control/Effect/Writer.hs
+++ src/Control/Effect/Writer.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE DeriveFunctor, FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, PolyKinds, TypeOperators, UndecidableInstances #-}
 module Control.Effect.Writer where
 
-import Control.Applicative (liftA2)
 import Control.Effect
 import Control.Monad.Codensity
 import Data.Bifunctor (first)
@@ -23,11 +22,6 @@ runWriter m = runWriterH (runCodensity var m)
 newtype WriterH w m a = WriterH { runWriterH :: m (w, a) }
   deriving (Functor)
 
-instance (Monoid w, Applicative m) => Applicative (WriterH w m) where
-  pure a = WriterH (pure (mempty, a))
-
-  WriterH f <*> WriterH a = WriterH (liftA2 (<*>) f a)
-
 instance Monoid w => Carrier ((,) w) (WriterH w) where
   joinl mf = WriterH (mf >>= runWriterH)
 
@@ -36,6 +30,8 @@ instance Monoid w => Carrier ((,) w) (WriterH w) where
   resume (w, m) = first (w <>) <$> runWriterH m
 
   wrap = WriterH
+
+  gen a = WriterH (pure (mempty, a))
 
 instance (Monoid w, TermMonad m sig) => TermAlgebra (WriterH w m) (Writer w :+: sig) where
   var = gen
