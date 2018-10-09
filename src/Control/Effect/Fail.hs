d4f502d394b54215f4ee9c35af78b6454a4d5069
Rob Rix <rob.rix@me.com>
2018-10-09 12:21:51 -0400

    Run Fail effects in the Codensity monad.

--- src/Control/Effect/Fail.hs
+++ src/Control/Effect/Fail.hs
@@ -1,12 +1,39 @@
-{-# LANGUAGE PolyKinds, TypeOperators #-}
+{-# LANGUAGE DeriveFunctor, FlexibleInstances, MultiParamTypeClasses, PolyKinds, TypeOperators, UndecidableInstances #-}
 module Control.Effect.Fail
 ( Fail(..)
 , runFail
 ) where
 
-import Control.Carrier.Either
+import Control.Applicative (liftA2)
 import Control.Effect
+import Control.Monad.Codensity
 
-runFail :: TermMonad m sig => Eff (Fail :+: sig) a -> m (Either String a)
-runFail = runEitherH . interpret alg
-  where alg (Fail s) = EitherH (pure (Left s))
+runFail :: TermMonad m sig => Codensity (EitherH String m) a -> m (Either String a)
+runFail = runEitherH . runCodensity var
+
+newtype EitherH e m a = EitherH { runEitherH :: m (Either e a) }
+  deriving (Functor)
+
+instance Applicative m => Applicative (EitherH e m) where
+  pure a = EitherH (pure (Right a))
+
+  EitherH f <*> EitherH a = EitherH (liftA2 (<*>) f a)
+
+instance Monad m => Monad (EitherH e m) where
+  return = pure
+
+  EitherH a >>= f = EitherH (a >>= either (pure . Left) (runEitherH . f))
+
+instance Carrier (Either e) (EitherH e) where
+  joinl mf = EitherH (mf >>= runEitherH)
+
+  suspend f = f (Right ())
+
+  resume = either (pure . Left) runEitherH
+
+  wrap = EitherH
+
+instance TermMonad m sig => TermAlgebra (EitherH String m) (Fail :+: sig) where
+  var = gen
+  con = alg \/ interpretRest
+    where alg (Fail s) = EitherH (pure (Left s))
