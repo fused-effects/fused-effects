c1d343887319495e7222173355dfa4a7b608d8e6
Rob Rix <rob.rix@me.com>
2018-10-09 13:04:25 -0400

    Run NonDet once in the Codeensity monad.

--- src/Control/Effect/NonDet.hs
+++ src/Control/Effect/NonDet.hs
@@ -8,7 +8,6 @@ module Control.Effect.NonDet
 ) where
 
 import Control.Applicative (Alternative(..), liftA2)
-import Control.Carrier.Maybe
 import Control.Carrier.Split
 import Control.Effect
 import Control.Monad.Codensity
@@ -35,10 +34,27 @@ instance TermMonad m sig => TermAlgebra (ListH m) (NonDet :+: sig) where
     where alg Empty = ListH (pure [])
           alg (Choose k) = ListH (liftA2 (++) (runListH (k True)) (runListH (k False)))
 
-runNonDetOnce :: TermMonad m sig => Eff (NonDet :+: sig) a -> m (Maybe a)
-runNonDetOnce = runMaybeH . interpret alg
-  where alg Empty      = MaybeH (pure Nothing)
-        alg (Choose k) = MaybeH (liftA2 (<|>) (runMaybeH (k True)) (runMaybeH (k False)))
+runNonDetOnce :: TermMonad m sig => Codensity (MaybeH m) a -> m (Maybe a)
+runNonDetOnce = runMaybeH . runCodensity var
+
+newtype MaybeH m a = MaybeH { runMaybeH :: m (Maybe a) }
+
+instance Carrier Maybe MaybeH where
+  joinl mf = MaybeH (mf >>= runMaybeH)
+
+  suspend f = f (Just ())
+
+  resume = maybe (pure Nothing) runMaybeH
+
+  wrap = MaybeH
+
+  gen a = MaybeH (pure (Just a))
+
+instance TermMonad m sig => TermAlgebra (MaybeH m) (NonDet :+: sig) where
+  var = gen
+  con = alg \/ interpretRest
+    where alg Empty      = MaybeH (pure Nothing)
+          alg (Choose k) = MaybeH (liftA2 (<|>) (runMaybeH (k True)) (runMaybeH (k False)))
 
 runNonDetSplit :: TermMonad m sig => Eff (NonDet :+: sig) a -> m [a]
 runNonDetSplit = joinSplitH . interpret alg
