7881238147ad3c9a7691bef3e4cef6ac4b7adbd8
Rob Rix <rob.rix@me.com>
2018-10-09 11:52:43 -0400

    Run State effects in the Codensity Monad.

--- src/Control/Effect/State.hs
+++ src/Control/Effect/State.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE DeriveFunctor, FlexibleContexts, PolyKinds, TypeOperators #-}
+{-# LANGUAGE DeriveFunctor, FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, PolyKinds, TypeOperators, UndecidableInstances #-}
 module Control.Effect.State where
 
-import Control.Carrier.State
 import Control.Effect
+import Control.Monad.Codensity
 
 data State s m k
   = Get (s -> k)
@@ -23,7 +23,40 @@ put :: (Subset (State s) sig, TermMonad m sig) => s -> m ()
 put s = send (Put s (pure ()))
 
 
-runState :: TermMonad m sig => s -> Eff (State s :+: sig) a -> m (s, a)
-runState s m = runStateH (interpret alg m) s
-  where alg (Get k)   = StateH (\ s -> runStateH (k s) s)
-        alg (Put s k) = StateH (\ _ -> runStateH  k    s)
+runState :: TermMonad m sig => s -> Codensity (StateH s m) a -> m (s, a)
+runState s m = runStateH (runCodensity var m) s
+
+newtype StateH s m a = StateH { runStateH :: s -> m (s, a) }
+  deriving (Functor)
+
+instance Monad m => Applicative (StateH s m) where
+  pure a = StateH (\ s -> pure (s, a))
+
+  StateH f <*> StateH a = StateH $ \ s -> do
+    (s',  f') <- f s
+    (s'', a') <- a s'
+    let fa = f' a'
+    fa `seq` pure (s'', fa)
+
+instance Monad m => Monad (StateH s m) where
+  return = pure
+
+  StateH a >>= f = StateH $ \ s -> do
+    (s', a') <- a s
+    let fa = f a'
+    fa `seq` runStateH fa s'
+
+instance Carrier ((,) s) (StateH s) where
+  joinl mf = StateH (\ s -> mf >>= \ f -> runStateH f s)
+
+  suspend f = StateH (\ s -> runStateH (f (s, ())) s)
+
+  resume (s, m) = runStateH m s
+
+  wrap = StateH . const
+
+instance TermMonad m sig => TermAlgebra (StateH s m) (State s :+: sig) where
+  var = pure
+  con = alg \/ interpretRest
+    where alg (Get   k) = StateH (\ s -> runStateH (k s) s)
+          alg (Put s k) = StateH (\ _ -> runStateH  k    s)
