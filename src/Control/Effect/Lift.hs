048c2cffb8643b2348fcfb13d6c1802a62ee1d99
Rob Rix <rob.rix@me.com>
2018-10-09 13:48:43 -0400

    Run Lift in the Codensity monad.

--- src/Control/Effect/Lift.hs
+++ src/Control/Effect/Lift.hs
@@ -1,10 +1,17 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Control.Effect.Lift
 ( Lift(..)
 , runM
 ) where
 
 import Control.Effect
-import Control.Monad (join)
+import Control.Monad.Codensity
 
-runM :: Monad m => Eff (Lift m) a -> m a
-runM = foldH pure (join . unLift)
+runM :: Monad m => Codensity (LiftH m) a -> m a
+runM = runLiftH . runCodensity var
+
+newtype LiftH m a = LiftH { runLiftH :: m a }
+
+instance Monad m => TermAlgebra (LiftH m) (Lift m) where
+  var = LiftH . pure
+  con = LiftH . (>>= runLiftH) . unLift
