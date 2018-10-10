938fe2b159a4faa37e39564a6214d39db6435a65
Rob Rix <rob.rix@me.com>
2018-10-10 14:32:04 -0400

    Define a Trace handler printing to stderr.

--- src/Control/Effect/Trace.hs
+++ src/Control/Effect/Trace.hs
@@ -1,11 +1,15 @@
-{-# LANGUAGE DeriveFunctor, FlexibleContexts, PolyKinds #-}
+{-# LANGUAGE DeriveFunctor, FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, PolyKinds, TypeOperators, UndecidableInstances #-}
 module Control.Effect.Trace
 ( Trace(..)
 , trace
+, runPrintingTrace
 ) where
 
 import Control.Effect.Handler
+import Control.Effect.Internal
 import Control.Effect.Sum
+import Control.Monad.IO.Class
+import System.IO
 
 data Trace m k = Trace String k
   deriving (Functor)
@@ -18,3 +22,14 @@ instance Effect Trace where
 
 trace :: (Subset Trace sig, Effectful sig m) => String -> m ()
 trace message = send (Trace message (pure ()))
+
+
+runPrintingTrace :: (MonadIO m, Effectful sig m) => Eff (PrintingH m) a -> m a
+runPrintingTrace = runPrintingH . interpret
+
+newtype PrintingH m a = PrintingH { runPrintingH :: m a }
+
+instance (MonadIO m, Carrier sig m) => Carrier (Trace :+: sig) (PrintingH m) where
+  gen = PrintingH . gen
+  alg = algT \/ (PrintingH . alg . handlePure runPrintingH)
+    where algT (Trace s k) = PrintingH (liftIO (hPutStrLn stderr s) *> runPrintingH k)
