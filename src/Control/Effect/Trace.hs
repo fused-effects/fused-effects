d8104aa5c941915e85a4db5a84845eaf88edceef
Rob Rix <rob.rix@me.com>
2018-10-10 14:24:01 -0400

    Define a trace constructor.

--- src/Control/Effect/Trace.hs
+++ src/Control/Effect/Trace.hs
@@ -1,5 +1,11 @@
-{-# LANGUAGE DeriveFunctor, PolyKinds #-}
+{-# LANGUAGE DeriveFunctor, FlexibleContexts, PolyKinds #-}
 module Control.Effect.Trace where
+
+import Control.Effect.Handler
+import Control.Effect.Sum
 
 data Trace m k = Trace String k
   deriving (Functor)
+
+trace :: (Subset Trace sig, Effectful sig m) => String -> m ()
+trace message = send (Trace message (pure ()))
