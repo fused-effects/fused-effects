2f2f42415acc3e50810e2961d74703077b2941ab
Rob Rix <rob.rix@me.com>
2018-10-10 13:56:14 -0400

    :fire: the internal Sum module.

--- src/Control/Effect/Sum.hs
+++ src/Control/Effect/Sum.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, PolyKinds, TypeOperators #-}
+{-# LANGUAGE DeriveFunctor, FlexibleInstances, MultiParamTypeClasses, PolyKinds, TypeOperators #-}
 module Control.Effect.Sum
 ( (:+:)(..)
 , (\/)
@@ -7,7 +7,25 @@ module Control.Effect.Sum
 ) where
 
 import Control.Effect.Handler
-import Control.Effect.Sum.Internal
+
+data (f :+: g) m k
+  = L (f m k)
+  | R (g m k)
+  deriving (Eq, Functor, Ord, Show)
+
+infixr 4 :+:
+
+instance (HFunctor l, HFunctor r) => HFunctor (l :+: r) where
+  hfmap f (L l) = L (hfmap f l)
+  hfmap f (R r) = R (hfmap f r)
+
+  fmap' f (L l) = L (fmap' f l)
+  fmap' f (R r) = R (fmap' f r)
+
+instance (Effect l, Effect r) => Effect (l :+: r) where
+  handle state handler (L l) = L (handle state handler l)
+  handle state handler (R r) = R (handle state handler r)
+
 
 -- | Lift algebras for either side of a sum into a single algebra on sums.
 (\/) :: ( sig1           m a -> b)
