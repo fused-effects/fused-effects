6e6878777d85abda57084acb6d53706b5c498557
Rob Rix <rob.rix@me.com>
2018-10-10 14:25:02 -0400

    Define an HFunctor instance for Trace.

--- src/Control/Effect/Trace.hs
+++ src/Control/Effect/Trace.hs
@@ -10,5 +10,8 @@ import Control.Effect.Sum
 data Trace m k = Trace String k
   deriving (Functor)
 
+instance HFunctor Trace where
+  hfmap _ (Trace s k) = Trace s k
+
 trace :: (Subset Trace sig, Effectful sig m) => String -> m ()
 trace message = send (Trace message (pure ()))
