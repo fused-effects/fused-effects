c7c56f3b174ec2ccdfa23f01e06b78a747569bc4
Rob Rix <rob.rix@me.com>
2018-10-11 12:30:10 -0400

    Rename the test case.

--- test/Control/Effect/NonDet/Spec.hs
+++ test/Control/Effect/NonDet/Spec.hs
@@ -31,5 +31,5 @@ spec = do
     it "collapses results of effects run after it" $
       run (runState 'a' (runNonDet (pure 'z' <|> put 'b' *> get <|> get))) `shouldBe` ('b', "zbb")
 
-    it "runs in higher-order positions" $
+    it "collapses results of higher-order effects run after it" $
       run (runError (runNonDet ((pure 'z' <|> throwError 'a') `catchError` pure))) `shouldBe` (Right "a" :: Either Char String)
