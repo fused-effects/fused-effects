c06ed916855ac2e8e7c6429079e582a14467726a
Rob Rix <rob.rix@me.com>
2018-10-11 10:16:10 -0400

    Test that ask produces a value.

--- test/Control/Effect/Reader/Spec.hs
+++ test/Control/Effect/Reader/Spec.hs
@@ -1,4 +1,10 @@
 module Control.Effect.Reader.Spec where
 
+import Control.Effect
+import Test.Hspec
+
 spec :: Spec
-spec = pure ()
+spec = do
+  describe "ask" $ do
+    it "produces a value" $ do
+      run (runReader "i" ((++) . ('h' :) <$> ask <*> ask)) `shouldBe` "hii"
