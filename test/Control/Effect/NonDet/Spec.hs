1dea4598ba5b1ba2868340a3025e8b7e8de1d904
Rob Rix <rob.rix@me.com>
2018-10-11 12:30:30 -0400

    Test that NonDet collects results from higher-order effects run before it.

--- test/Control/Effect/NonDet/Spec.hs
+++ test/Control/Effect/NonDet/Spec.hs
@@ -31,5 +31,8 @@ spec = do
     it "collapses results of effects run after it" $
       run (runState 'a' (runNonDet (pure 'z' <|> put 'b' *> get <|> get))) `shouldBe` ('b', "zbb")
 
+    it "collects results from higher-order effects run before it" $
+      run (runNonDet (runError ((pure 'z' <|> throwError 'a') `catchError` pure))) `shouldBe` [Right 'z', Right 'a' :: Either Char Char]
+
     it "collapses results of higher-order effects run after it" $
       run (runError (runNonDet ((pure 'z' <|> throwError 'a') `catchError` pure))) `shouldBe` (Right "a" :: Either Char String)
