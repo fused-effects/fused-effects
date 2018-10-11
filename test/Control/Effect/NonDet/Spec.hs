3c8d90d5880b81412dcc4a30aa7a7fa3c034509c
Rob Rix <rob.rix@me.com>
2018-10-11 12:09:57 -0400

    Test that <|> produces each branch’s result.

--- test/Control/Effect/NonDet/Spec.hs
+++ test/Control/Effect/NonDet/Spec.hs
@@ -8,3 +8,7 @@ spec = do
   describe "empty" $ do
     it "produces no values" $
       run (runNonDet empty) `shouldBe` ([] :: String)
+
+  describe "<|>" $ do
+    it "produces each branch’s result nondeterministically" $
+      run (runNonDet (pure 'a' <|> pure 'b')) `shouldBe` "ab"
