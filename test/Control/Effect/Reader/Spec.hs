644de0848d6265df8c0bbf6225bf305342516e87
Rob Rix <rob.rix@me.com>
2018-10-11 10:18:12 -0400

    Test that local modifies the environment.

--- test/Control/Effect/Reader/Spec.hs
+++ test/Control/Effect/Reader/Spec.hs
@@ -8,3 +8,7 @@ spec = do
   describe "ask" $ do
     it "produces a value" $
       run (runReader "i" ((++) . ('h' :) <$> ask <*> ask)) `shouldBe` "hii"
+
+  describe "local" $ do
+    it "modifies the environment with a function" $
+      run (runReader (0 :: Int) (local (succ :: Int -> Int) ask)) `shouldBe` (1 :: Int)
