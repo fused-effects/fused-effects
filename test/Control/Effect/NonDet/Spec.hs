f8ac1f8b65723406059a2e2bcd03fb175fed4b5f
Rob Rix <rob.rix@me.com>
2018-10-11 12:08:10 -0400

    Stub in a NonDet spec.

---
+++ test/Control/Effect/NonDet/Spec.hs
@@ -0,0 +1,6 @@
+module Control.Effect.NonDet.Spec where
+
+import Test.Hspec
+
+spec :: Spec
+spec = pure ()
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,12 @@
 module Main where
 
 import qualified Control.Effect.Spec
+import qualified Control.Effect.NonDet.Spec
 import qualified Control.Effect.Reader.Spec
 import Test.Hspec
 
 main :: IO ()
 main = hspec . parallel $ do
   describe "Control.Effect.Spec" Control.Effect.Spec.spec
+  describe "Control.Effect.NonDet.Spec" Control.Effect.NonDet.Spec.spec
   describe "Control.Effect.Reader.Spec" Control.Effect.Reader.Spec.spec
