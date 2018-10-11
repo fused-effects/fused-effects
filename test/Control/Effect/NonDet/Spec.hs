77e932ee5a03759200c1f41d9e5b78e3cf30da17
Rob Rix <rob.rix@me.com>
2018-10-11 13:46:21 -0400

    :fire: the laws section of the NonDet spec.
    It’s been subsumed by the doctests.

--- test/Control/Effect/NonDet/Spec.hs
+++ test/Control/Effect/NonDet/Spec.hs
@@ -14,16 +14,6 @@ spec = do
     it "produces each branch’s result nondeterministically" $
       run (runNonDet (pure 'a' <|> pure 'b')) `shouldBe` "ab"
 
-  describe "laws" $ do
-    prop "associativity" $
-      \ a b c -> run (runNonDet ((pure a <|> pure b) <|> pure c)) `shouldBe` run (runNonDet (pure a <|> (pure b <|> pure (c :: Char))))
-
-    prop "left-identity" $
-      \ b -> run (runNonDet (empty <|> pure b)) `shouldBe` [b :: Char]
-
-    prop "right-identity" $
-      \ a -> run (runNonDet (pure a <|> empty)) `shouldBe` [a :: Char]
-
   describe "interactions" $ do
     it "collects results of effects run before it" $
       run (runNonDet (runState 'a' (pure 'z' <|> put 'b' *> get <|> get))) `shouldBe` [('a', 'z'), ('b', 'b'), ('a', 'a')]
