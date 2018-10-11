479a9c9694b1810fbaa92c397461dc012b50a35c
Rob Rix <rob.rix@me.com>
2018-10-11 09:45:13 -0400

    Define an Effect instance for Fresh.

--- src/Control/Effect/Fresh.hs
+++ src/Control/Effect/Fresh.hs
@@ -12,3 +12,7 @@ deriving instance Functor (Fresh m)
 instance HFunctor Fresh where
   hfmap _ (Fresh     k) = Fresh k
   hfmap f (Reset i m k) = Reset i (f m) k
+
+instance Effect Fresh where
+  handle state handler (Fresh     k) = Fresh (handler . (<$ state) . k)
+  handle state handler (Reset i m k) = Reset i (handler (m <$ state)) (handler . fmap k)
