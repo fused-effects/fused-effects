7a2b46fbe7e1f3b5421e77d22faa1353308a78ed
Rob Rix <rob.rix@me.com>
2018-10-11 09:43:20 -0400

    Derive a Functor instance for Fresh.

--- src/Control/Effect/Fresh.hs
+++ src/Control/Effect/Fresh.hs
@@ -1,6 +1,8 @@
-{-# LANGUAGE ExistentialQuantification, PolyKinds #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, PolyKinds, StandaloneDeriving #-}
 module Control.Effect.Fresh where
 
 data Fresh m k
   = Fresh (Int -> k)
   | forall b . Reset Int (m b) (b -> k)
+
+deriving instance Functor (Fresh m)
