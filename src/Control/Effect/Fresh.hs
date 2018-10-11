1ff0833ed3033844c01f81e8250fccbd963ac19e
Rob Rix <rob.rix@me.com>
2018-10-11 09:55:19 -0400

    Define a handler for Fresh.

--- src/Control/Effect/Fresh.hs
+++ src/Control/Effect/Fresh.hs
@@ -1,11 +1,13 @@
-{-# LANGUAGE DeriveFunctor, ExistentialQuantification, FlexibleContexts, PolyKinds, StandaloneDeriving #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, PolyKinds, StandaloneDeriving, TypeOperators, UndecidableInstances #-}
 module Control.Effect.Fresh
 ( Fresh(..)
 , fresh
 , resetFresh
+, runFresh
 ) where
 
 import Control.Effect.Handler
+import Control.Effect.Internal
 import Control.Effect.Sum
 
 data Fresh m k
@@ -27,3 +29,16 @@ fresh = send (Fresh pure)
 
 resetFresh :: (Subset Fresh sig, Effectful sig m) => Int -> m a -> m a
 resetFresh i m = send (Reset i m pure)
+
+
+runFresh :: Effectful sig m => Int -> Eff (FreshH m) a -> m a
+runFresh i = fmap snd . flip runFreshH i . interpret
+
+newtype FreshH m a = FreshH { runFreshH :: Int -> m (Int, a) }
+
+instance Effectful sig m => Carrier (Fresh :+: sig) (FreshH m) where
+  gen a = FreshH (\ i -> pure (i, a))
+  alg = algF \/ algOther
+    where algF (Fresh      k) = FreshH (\ i -> runFreshH (k i) (succ i))
+          algF (Reset i' m k) = FreshH (\ i -> runFreshH m i' >>= \ (_, a) -> runFreshH (k a) i)
+          algOther op = FreshH (\ i -> alg (handle (i, ()) (uncurry (flip runFreshH)) op))
