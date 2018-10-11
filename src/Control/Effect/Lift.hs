87ce3619b624743c6feadd34f480772892b969d8
Rob Rix <rob.rix@me.com>
2018-10-11 17:29:44 -0400

    :memo: runM.

--- src/Control/Effect/Lift.hs
+++ src/Control/Effect/Lift.hs
@@ -9,6 +9,7 @@ import Control.Effect.Handler
 import Control.Effect.Internal
 import Control.Effect.Lift.Internal
 
+-- | Extract a 'Lift'ed 'Monad'ic action from an effectful computation.
 runM :: Monad m => Eff (LiftH m) a -> m a
 runM = runLiftH . interpret
 
