feb6b4e7d91ff2944c31a439e0b1f4f989d1a610
Rob Rix <rob.rix@me.com>
2018-10-11 17:36:29 -0400

    Test the behaviour of tell.

--- src/Control/Effect/Writer.hs
+++ src/Control/Effect/Writer.hs
@@ -21,6 +21,8 @@ instance Effect (Writer w) where
   handle state handler (Tell w k) = Tell w (handler (k <$ state))
 
 -- | Write a value to the log.
+--
+--   prop> fst (run (runWriter (replicateM (abs n) (tell (Sum w))))) == stimes (abs n) (Sum w)
 tell :: (Member (Writer w) sig, Carrier sig m) => w -> m ()
 tell w = send (Tell w (gen ()))
 
@@ -35,3 +37,12 @@ instance (Monoid w, Effectful sig m) => Carrier (Writer w :+: sig) (WriterH w m)
   alg = algW \/ (WriterH . alg . handle (mempty, ()) (uncurry runWriter'))
     where algW (Tell w k) = WriterH (first (w <>) <$> runWriterH k)
           runWriter' w = fmap (first (w <>)) . runWriterH
+
+
+-- $setup
+-- >>> :seti -XFlexibleContexts
+-- >>> import Test.QuickCheck
+-- >>> import Control.Effect.Void
+-- >>> import Control.Monad (replicateM)
+-- >>> import Data.Monoid (Sum(..))
+-- >>> import Data.Semigroup (stimes)
