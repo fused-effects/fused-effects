243c827b68c1492e6592ebdc42d778ce7cef7eef
Rob Rix <rob.rix@me.com>
2018-10-11 11:25:05 -0400

    Test that we can reinterpret effects into other effects.

--- test/Control/Effect/Spec.hs
+++ test/Control/Effect/Spec.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts, FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, UndecidableInstances #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, TypeOperators, UndecidableInstances #-}
 module Control.Effect.Spec where
 
 import Control.Effect
@@ -9,6 +9,9 @@ spec = do
   describe "Eff" $ do
     it "can be wrapped for better type inference" $
       run (runHasEnv (runEnv "i" ((++) <$> askEnv <*> askEnv))) `shouldBe` "ii"
+
+    it "can reinterpret effects into other effects" $
+      run (runState "a" ((++) <$> reinterpretReader (local ('b':) ask) <*> get)) `shouldBe` ("a", "baa")
 
 
 askEnv :: (Member (Reader env) sig, Carrier sig m) => HasEnv env m env
@@ -26,3 +29,20 @@ instance Carrier sig carrier => Carrier sig (HasEnv env carrier) where
   alg op = HasEnv (alg (handlePure runHasEnv op))
 
 instance (Carrier sig carrier, Effect sig) => Effectful sig (HasEnv env carrier)
+
+
+reinterpretReader :: (Effectful (State r :+: sig) m, Effect sig) => Eff (ReinterpretReaderH r m) a -> m a
+reinterpretReader = runReinterpretReaderH . interpret
+
+newtype ReinterpretReaderH r m a = ReinterpretReaderH { runReinterpretReaderH :: m a }
+
+instance (Effectful (State r :+: sig) m, Effect sig) => Carrier (Reader r :+: sig) (ReinterpretReaderH r m) where
+  gen = ReinterpretReaderH . gen
+  alg = algR \/ (ReinterpretReaderH . alg . R . handlePure runReinterpretReaderH)
+    where algR (Ask       k) = ReinterpretReaderH (get >>= runReinterpretReaderH . k)
+          algR (Local f m k) = ReinterpretReaderH $ do
+            a <- get
+            put (f a)
+            v <- runReinterpretReaderH m
+            put a
+            runReinterpretReaderH (k v)
