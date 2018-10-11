f23e43d89743c7d96232127fea0be641ed8e9f61
Rob Rix <rob.rix@me.com>
2018-10-11 09:44:11 -0400

    Define an HFunctor instance for Fresh.

--- src/Control/Effect/Fresh.hs
+++ src/Control/Effect/Fresh.hs
@@ -1,8 +1,14 @@
 {-# LANGUAGE DeriveFunctor, ExistentialQuantification, PolyKinds, StandaloneDeriving #-}
 module Control.Effect.Fresh where
 
+import Control.Effect.Handler
+
 data Fresh m k
   = Fresh (Int -> k)
   | forall b . Reset Int (m b) (b -> k)
 
 deriving instance Functor (Fresh m)
+
+instance HFunctor Fresh where
+  hfmap _ (Fresh     k) = Fresh k
+  hfmap f (Reset i m k) = Reset i (f m) k
