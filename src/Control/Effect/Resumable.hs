c0d13e3f8fbfb5bd980ae26dcc12d827f57ba7ce
Rob Rix <rob.rix@me.com>
2018-10-12 08:52:27 -0400

    Resumable is an HFunctor.

--- src/Control/Effect/Resumable.hs
+++ src/Control/Effect/Resumable.hs
@@ -3,7 +3,12 @@ module Control.Effect.Resumable
 ( Resumable(..)
 ) where
 
+import Control.Effect.Handler
+
 data Resumable exc m k
   = forall a . Resumable (exc a) (a -> k)
 
 deriving instance Functor (Resumable exc m)
+
+instance HFunctor (Resumable exc) where
+  hfmap _ (Resumable exc k) = Resumable exc k
