66f816299f77fb2a2b622c164a5645f8f39cb64a
Rob Rix <rob.rix@me.com>
2018-10-12 08:50:09 -0400

    Derive a Functor instance for Resumable.

--- src/Control/Effect/Resumable.hs
+++ src/Control/Effect/Resumable.hs
@@ -1,5 +1,7 @@
-{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, StandaloneDeriving #-}
 module Control.Effect.Resumable where
 
 data Resumable exc k
   = forall a . Resumable (exc a) (a -> k)
+
+deriving instance Functor (Resumable exc)
