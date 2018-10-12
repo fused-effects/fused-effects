3fa899d5d8335633f2ff75e06d0fea6c67df184b
Rob Rix <rob.rix@me.com>
2018-10-12 08:50:33 -0400

    Re-export the Resumable module.

--- src/Control/Effect.hs
+++ src/Control/Effect.hs
@@ -2,16 +2,17 @@ module Control.Effect
 ( module X
 ) where
 
-import Control.Effect.Error    as X
-import Control.Effect.Fail     as X
-import Control.Effect.Fresh    as X
-import Control.Effect.Handler  as X
-import Control.Effect.Internal as X
-import Control.Effect.Lift     as X
-import Control.Effect.NonDet   as X
-import Control.Effect.Reader   as X
-import Control.Effect.State    as X
-import Control.Effect.Sum      as X
-import Control.Effect.Trace    as X
-import Control.Effect.Void     as X
-import Control.Effect.Writer   as X
+import Control.Effect.Error     as X
+import Control.Effect.Fail      as X
+import Control.Effect.Fresh     as X
+import Control.Effect.Handler   as X
+import Control.Effect.Internal  as X
+import Control.Effect.Lift      as X
+import Control.Effect.NonDet    as X
+import Control.Effect.Reader    as X
+import Control.Effect.Resumable as X
+import Control.Effect.State     as X
+import Control.Effect.Sum       as X
+import Control.Effect.Trace     as X
+import Control.Effect.Void      as X
+import Control.Effect.Writer    as X
