63f700a8df20cc577dedc704493dc8964c331a42
Rob Rix <rob.rix@me.com>
2018-10-12 09:19:23 -0400

    Rename exc to err.

--- src/Control/Effect/Resumable.hs
+++ src/Control/Effect/Resumable.hs
@@ -12,46 +12,46 @@ import Control.Effect.Internal
 import Control.Effect.Sum
 import Data.Functor.Classes
 
-data Resumable exc m k
-  = forall a . Resumable (exc a) (a -> k)
+data Resumable err m k
+  = forall a . Resumable (err a) (a -> k)
 
-deriving instance Functor (Resumable exc m)
+deriving instance Functor (Resumable err m)
 
-instance HFunctor (Resumable exc) where
-  hfmap _ (Resumable exc k) = Resumable exc k
+instance HFunctor (Resumable err) where
+  hfmap _ (Resumable err k) = Resumable err k
 
-instance Effect (Resumable exc) where
-  handle state handler (Resumable exc k) = Resumable exc (handler . (<$ state) . k)
+instance Effect (Resumable err) where
+  handle state handler (Resumable err k) = Resumable err (handler . (<$ state) . k)
 
 -- | Throw an exception which can be resumed with a value of its result type.
 --
 --   prop> run (runResumable (throwResumable (Identity a))) == Left (SomeError (Identity a))
-throwResumable :: (Member (Resumable exc) sig, Carrier sig m) => exc a -> m a
-throwResumable exc = send (Resumable exc gen)
+throwResumable :: (Member (Resumable err) sig, Carrier sig m) => err a -> m a
+throwResumable err = send (Resumable err gen)
 
 
-data SomeError (exc :: * -> *)
-  = forall a . SomeError (exc a)
+data SomeError (err :: * -> *)
+  = forall a . SomeError (err a)
 
-instance Eq1 exc => Eq (SomeError exc) where
+instance Eq1 err => Eq (SomeError err) where
   SomeError exc1 == SomeError exc2 = liftEq (const (const True)) exc1 exc2
 
-instance Ord1 exc => Ord (SomeError exc) where
+instance Ord1 err => Ord (SomeError err) where
   SomeError exc1 `compare` SomeError exc2 = liftCompare (const (const EQ)) exc1 exc2
 
-instance (Show1 exc) => Show (SomeError exc) where
-  showsPrec num (SomeError exc) = liftShowsPrec (const (const id)) (const id) num exc
+instance (Show1 err) => Show (SomeError err) where
+  showsPrec num (SomeError err) = liftShowsPrec (const (const id)) (const id) num err
 
 
-runResumable :: Effectful sig m => Eff (ResumableH exc m) a -> m (Either (SomeError exc) a)
+runResumable :: Effectful sig m => Eff (ResumableH err m) a -> m (Either (SomeError err) a)
 runResumable = runResumableH . interpret
 
-newtype ResumableH exc m a = ResumableH { runResumableH :: m (Either (SomeError exc) a) }
+newtype ResumableH err m a = ResumableH { runResumableH :: m (Either (SomeError err) a) }
 
-instance Effectful sig m => Carrier (Resumable exc :+: sig) (ResumableH exc m) where
+instance Effectful sig m => Carrier (Resumable err :+: sig) (ResumableH err m) where
   gen a = ResumableH (gen (Right a))
   alg = algE \/ (ResumableH . alg . handle (Right ()) (either (gen . Left) runResumableH))
-    where algE (Resumable exc _) = ResumableH (gen (Left (SomeError exc)))
+    where algE (Resumable err _) = ResumableH (gen (Left (SomeError err)))
 
 
 -- $setup
