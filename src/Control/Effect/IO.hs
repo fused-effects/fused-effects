358b5322c2f94798289b38bb2203a96c064c7f72
Rob Rix <rob.rix@me.com>
2018-10-13 12:56:02 -0400

    Define a rethrowing action.

--- src/Control/Effect/IO.hs
+++ src/Control/Effect/IO.hs
@@ -1,1 +1,20 @@
-module Control.Effect.IO where
+{-# LANGUAGE FlexibleContexts, TypeApplications #-}
+module Control.Effect.IO
+( rethrowing
+) where
+
+import Control.Effect.Error
+import Control.Effect.Handler
+import Control.Effect.Sum
+import qualified Control.Exception as Exc
+import Control.Monad.IO.Class
+
+-- | Lift an 'IO' action into 'Eff', catching and rethrowing any exceptions it throws into an 'Error' effect.
+-- If you need more granular control over the types of exceptions caught, use 'catchIO' and rethrow in the handler.
+rethrowing :: ( Member (Error Exc.SomeException) sig
+              , MonadIO m
+              , Carrier sig m
+              )
+           => IO a
+           -> m a
+rethrowing m = liftIO (Exc.try m) >>= either (throwError . Exc.toException @Exc.SomeException) pure
