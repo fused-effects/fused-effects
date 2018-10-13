d6482d33daa9d3e4b726e434e44c766b41ebde03
Rob Rix <rob.rix@me.com>
2018-10-13 13:24:11 -0400

    Test that rethrowing rethrows.

--- test/Control/Effect/IO/Spec.hs
+++ test/Control/Effect/IO/Spec.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE GADTs, ScopedTypeVariables, TypeOperators #-}
 module Control.Effect.IO.Spec where
 
+import Control.Effect
+import qualified Control.Exception as Exc
+import Data.Bifunctor (first)
+import Data.Typeable
 import Test.Hspec
 
 spec :: Spec
-spec = pure ()
+spec = do
+  describe "rethrowing" $ do
+    it "bridges exceptions into Error effects" $ do
+      exc <- runM (runError (rethrowing (error "error") *> pure ()))
+      first extract exc `shouldBe` Left (Just "error")
+
+
+extract :: Exc.SomeException -> Maybe String
+extract (Exc.SomeException (exc :: e)) = case eqT :: Maybe (e :~: Exc.ErrorCall) of
+  Just Refl | Exc.ErrorCall str <- exc -> Just str
+  _                                    -> Nothing
