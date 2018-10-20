4d1b089bc8563f33983fe9a9797a4d6fbe14670f
Rob Rix <rob.rix@me.com>
2018-10-20 14:06:01 -0400

    Factor AltC out of the algebras.

--- src/Control/Effect/NonDet.hs
+++ src/Control/Effect/NonDet.hs
@@ -26,9 +26,9 @@ newtype AltC f m a = AltC { runAltC :: m (f a) }
 
 instance (Alternative f, Monad f, Traversable f, Carrier sig m, Effect sig, Applicative m) => Carrier (NonDet :+: sig) (AltC f m) where
   ret a = AltC (ret (pure a))
-  eff = algND \/ (AltC . eff . handle (pure ()) (fmap join . traverse runAltC))
-    where algND Empty      = AltC (ret empty)
-          algND (Choose k) = AltC (liftA2 (<|>) (runAltC (k True)) (runAltC (k False)))
+  eff = AltC . (algND \/ eff . handle (pure ()) (fmap join . traverse runAltC))
+    where algND Empty      = ret empty
+          algND (Choose k) = liftA2 (<|>) (runAltC (k True)) (runAltC (k False))
 
 
 -- $setup
