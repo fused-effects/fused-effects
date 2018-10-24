57d922ec181872429c2e60b5dcc11df62764b134
Rob Rix <rob.rix@me.com>
2018-10-24 16:56:47 -0400

    Define an HFunctor instance for Intercept.

--- src/Control/Effect/Intercept.hs
+++ src/Control/Effect/Intercept.hs
@@ -1,7 +1,12 @@
 {-# LANGUAGE DeriveFunctor, ExistentialQuantification, RankNTypes, StandaloneDeriving #-}
 module Control.Effect.Intercept where
 
+import Control.Effect.Carrier
+
 data Intercept eff m k
   = forall a . Intercept (m a) (forall n x . eff n (n x) -> m a) (a -> k)
 
 deriving instance Functor (Intercept eff m)
+
+instance HFunctor (Intercept eff) where
+  hmap f (Intercept m h k) = Intercept (f m) (f . h) k
