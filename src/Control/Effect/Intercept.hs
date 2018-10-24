fb8125d8fdfa132b4e83ecf40b77be5a9d4d8a7e
Rob Rix <rob.rix@me.com>
2018-10-24 17:06:20 -0400

    Revert "Define an Intercept effect."
    This reverts commit f7e2af7f0e0351b16e79c28af67166f1aabf2fff.

--- src/Control/Effect/Intercept.hs
+++ src/Control/Effect/Intercept.hs
@@ -1,5 +1,1 @@
-{-# LANGUAGE ExistentialQuantification, RankNTypes #-}
 module Control.Effect.Intercept where
-
-data Intercept eff m k
-  = forall a . Intercept (m a) (forall n x . eff n (n x) -> m a) (a -> k)
