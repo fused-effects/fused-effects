c27ed0706c1bd550ea53616445116dcbb5632b06
Rob Rix <rob.rix@me.com>
2018-10-24 16:56:04 -0400

    Derive a Functor instance for Intercept.

--- src/Control/Effect/Intercept.hs
+++ src/Control/Effect/Intercept.hs
@@ -1,5 +1,7 @@
-{-# LANGUAGE ExistentialQuantification, RankNTypes #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, RankNTypes, StandaloneDeriving #-}
 module Control.Effect.Intercept where
 
 data Intercept eff m k
   = forall a . Intercept (m a) (forall n x . eff n (n x) -> m a) (a -> k)
+
+deriving instance Functor (Intercept eff m)
