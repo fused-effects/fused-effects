34a242bdd58ff30f3c19192a41315431f2058f9e
Rob Rix <rob.rix@me.com>
2018-10-24 17:06:07 -0400

    Revert "Define an Effect instance for Intercept."
    This reverts commit 9b2cb59f99b2b33de9ef2346924979e9f6ac5f2c.

--- src/Control/Effect/Intercept.hs
+++ src/Control/Effect/Intercept.hs
@@ -10,6 +10,3 @@ deriving instance Functor (Intercept eff m)
 
 instance HFunctor (Intercept eff) where
   hmap f (Intercept m h k) = Intercept (f m) (f . h) k
-
-instance Effect (Intercept eff) where
-  handle state handler (Intercept m h k) = Intercept (handler (m <$ state)) (handler . (<$ state) . h) (handler . fmap k)
