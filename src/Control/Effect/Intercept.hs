046d22f77914b7473614bd0a7cc1cfebb35dea9e
Rob Rix <rob.rix@me.com>
2018-10-24 17:06:02 -0400

    Revert "Define a smart constructor for Intercept."
    This reverts commit 13258d63bdd6e2317193341d7c76da55b6e812c2.

--- src/Control/Effect/Intercept.hs
+++ src/Control/Effect/Intercept.hs
@@ -1,11 +1,9 @@
-{-# LANGUAGE DeriveFunctor, ExistentialQuantification, FlexibleContexts, RankNTypes, StandaloneDeriving #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, RankNTypes, StandaloneDeriving #-}
 module Control.Effect.Intercept
 ( Intercept(..)
-, intercept
 ) where
 
 import Control.Effect.Carrier
-import Control.Effect.Sum
 
 data Intercept eff m k
   = forall a . Intercept (m a) (forall n x . eff n (n x) -> m a) (a -> k)
@@ -17,9 +15,3 @@ instance HFunctor (Intercept eff) where
 
 instance Effect (Intercept eff) where
   handle state handler (Intercept m h k) = Intercept (handler (m <$ state)) (handler . (<$ state) . h) (handler . fmap k)
-
-intercept :: (Member (Intercept eff) sig, Carrier sig m)
-          => m a
-          -> (forall n x . eff n (n x) -> m a)
-          -> m a
-intercept m f = send (Intercept m f ret)
