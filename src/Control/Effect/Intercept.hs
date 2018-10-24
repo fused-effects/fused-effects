0e6a032eb0eab1bc4049c7a63f68fd18e2c22a12
Rob Rix <rob.rix@me.com>
2018-10-24 17:06:17 -0400

    Revert "Derive a Functor instance for Intercept."
    This reverts commit c27ed0706c1bd550ea53616445116dcbb5632b06.

--- src/Control/Effect/Intercept.hs
+++ src/Control/Effect/Intercept.hs
@@ -1,7 +1,5 @@
-{-# LANGUAGE DeriveFunctor, ExistentialQuantification, RankNTypes, StandaloneDeriving #-}
+{-# LANGUAGE ExistentialQuantification, RankNTypes #-}
 module Control.Effect.Intercept where
 
 data Intercept eff m k
   = forall a . Intercept (m a) (forall n x . eff n (n x) -> m a) (a -> k)
-
-deriving instance Functor (Intercept eff m)
