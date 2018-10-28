1a7a8d0f456b7a517534fc0daa473a6542411d28
Rob Rix <rob.rix@me.com>
2018-10-27 22:32:30 -0400

    Define a Rand effect.

--- src/Control/Effect/Rand.hs
+++ src/Control/Effect/Rand.hs
@@ -1,2 +1,22 @@
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, KindSignatures, StandaloneDeriving #-}
 module Control.Effect.Rand
-() where
+( Rand(..)
+) where
+
+import Control.Effect.Carrier
+import Data.Coerce (coerce)
+import System.Random (Random(..))
+
+data Rand (m :: * -> *) k
+  = forall a . Random a => Uniform (a -> k)
+  | forall a . Random a => UniformR (a, a) (a -> k)
+
+deriving instance Functor (Rand m)
+
+instance HFunctor Rand where
+  hmap _ = coerce
+  {-# INLINE hmap #-}
+
+instance Effect Rand where
+  handle state handler (Uniform k) = Uniform (handler . (<$ state) . k)
+  handle state handler (UniformR r k) = UniformR r (handler . (<$ state) . k)
