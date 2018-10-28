41707ffe3b189ad86147f7da94750953f2ed8761
Rob Rix <rob.rix@me.com>
2018-10-28 18:29:42 -0400

    Define the algebra for TraceByIgnoringC using handleSum.

--- src/Control/Effect/Trace.hs
+++ src/Control/Effect/Trace.hs
@@ -59,7 +59,7 @@ newtype TraceByIgnoringC m a = TraceByIgnoringC { runTraceByIgnoringC :: m a }
 
 instance Carrier sig m => Carrier (Trace :+: sig) (TraceByIgnoringC m) where
   ret = TraceByIgnoringC . ret
-  eff = traceCont \/ TraceByIgnoringC . eff . handlePure runTraceByIgnoringC
+  eff = handleSum (TraceByIgnoringC . eff . handlePure runTraceByIgnoringC) traceCont
 
 
 -- | Run a 'Trace' effect, returning all traces as a list.
