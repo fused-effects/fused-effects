55f37b820dbe0c45ea26cc9713c35a9f1cc73464
Rob Rix <rob.rix@me.com>
2018-10-27 22:40:16 -0400

    Define a handler for Rand.

--- src/Control/Effect/Rand.hs
+++ src/Control/Effect/Rand.hs
@@ -1,5 +1,23 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, TypeOperators, UndecidableInstances #-}
 module Control.Effect.Rand
 ( Rand(..)
+, runRand
+, RandC(..)
 ) where
 
+import Control.Effect.Carrier
+import Control.Effect.Internal
 import Control.Effect.Rand.Internal
+import Control.Effect.Sum
+import System.Random (Random(..), RandomGen(..))
+
+runRand :: (Carrier sig m, Effect sig, RandomGen g) => g -> Eff (RandC g m) a -> m (g, a)
+runRand g = flip runRandC g . interpret
+
+newtype RandC g m a = RandC { runRandC :: g -> m (g, a) }
+
+instance (Carrier sig m, Effect sig, RandomGen g) => Carrier (Rand :+: sig) (RandC g m) where
+  ret a = RandC (\ g -> ret (g, a))
+  eff op = RandC (\ g -> (alg g \/ eff . handleState g runRandC) op)
+    where alg g (Uniform    k) = let (a, g') = random g in runRandC (k a) g'
+          alg g (UniformR r k) = let (a, g') = randomR r g in runRandC (k a) g'
