2feada7bf135bab70185e9b42ef5adde801b56a2
Rob Rix <rob.rix@me.com>
2018-10-27 22:34:23 -0400

    Move the Rand effect into an internal module.

--- src/Control/Effect/Rand.hs
+++ src/Control/Effect/Rand.hs
@@ -1,22 +1,5 @@
-{-# LANGUAGE DeriveFunctor, ExistentialQuantification, KindSignatures, StandaloneDeriving #-}
 module Control.Effect.Rand
 ( Rand(..)
 ) where
 
-import Control.Effect.Carrier
-import Data.Coerce (coerce)
-import System.Random (Random(..))
-
-data Rand (m :: * -> *) k
-  = forall a . Random a => Uniform (a -> k)
-  | forall a . Random a => UniformR (a, a) (a -> k)
-
-deriving instance Functor (Rand m)
-
-instance HFunctor Rand where
-  hmap _ = coerce
-  {-# INLINE hmap #-}
-
-instance Effect Rand where
-  handle state handler (Uniform k) = Uniform (handler . (<$ state) . k)
-  handle state handler (UniformR r k) = UniformR r (handler . (<$ state) . k)
+import Control.Effect.Rand.Internal
