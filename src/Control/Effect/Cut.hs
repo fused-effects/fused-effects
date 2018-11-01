84a2099d6584d5e975d43b7acbc9625db2bada49
Rob Rix <rob.rix@me.com>
2018-11-01 15:02:11 -0400

    Define a Cut effect.

--- src/Control/Effect/Cut.hs
+++ src/Control/Effect/Cut.hs
@@ -1,1 +1,10 @@
-module Control.Effect.Cut where
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, StandaloneDeriving #-}
+module Control.Effect.Cut
+( Cut(..)
+) where
+
+data Cut m k
+  = Cutfail
+  | forall a . Call (m a) (a -> k)
+
+deriving instance Functor (Cut m)
