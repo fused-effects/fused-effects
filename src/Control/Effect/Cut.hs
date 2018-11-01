a6d7b35829fb9477b7bab7239eb109c436bc1774
Rob Rix <rob.rix@me.com>
2018-11-01 15:03:12 -0400

    Define an HFunctor instance for Cut.

--- src/Control/Effect/Cut.hs
+++ src/Control/Effect/Cut.hs
@@ -3,8 +3,14 @@ module Control.Effect.Cut
 ( Cut(..)
 ) where
 
+import Control.Effect.Carrier
+
 data Cut m k
   = Cutfail
   | forall a . Call (m a) (a -> k)
 
 deriving instance Functor (Cut m)
+
+instance HFunctor Cut where
+  hmap _ Cutfail    = Cutfail
+  hmap f (Call m k) = Call (f m) k
