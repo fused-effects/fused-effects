31d415bc3b58fe5621723c347ac571489d09d75f
Rob Rix <rob.rix@me.com>
2018-11-03 13:11:42 -0400

    Define an HFunctor instance for Symbol.

--- examples/Parser.hs
+++ examples/Parser.hs
@@ -1,5 +1,12 @@
 {-# LANGUAGE DeriveFunctor, KindSignatures #-}
 module Parser where
 
+import Control.Effect.Carrier
+import Data.Coerce
+
 data Symbol (m :: * -> *) k = Symbol Char (Char -> k)
   deriving (Functor)
+
+instance HFunctor Symbol where
+  hmap _ = coerce
+  {-# INLINE hmap #-}
