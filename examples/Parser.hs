38c56e7fc6bf884d9ec56badef9c464a49c66f44
Rob Rix <rob.rix@me.com>
2018-11-03 13:16:32 -0400

    Rename Symbol to Satisfy.

--- examples/Parser.hs
+++ examples/Parser.hs
@@ -9,7 +9,7 @@ import Test.Hspec
 spec :: Spec
 spec = describe "parser" $ pure ()
 
-data Symbol (m :: * -> *) k = Symbol (Char -> Bool) (Char -> k)
+data Symbol (m :: * -> *) k = Satisfy (Char -> Bool) (Char -> k)
   deriving (Functor)
 
 instance HFunctor Symbol where
@@ -20,4 +20,4 @@ instance Effect Symbol where
   handle state handler = coerce . fmap (handler . (<$ state))
 
 satisfy :: (Carrier sig m, Member Symbol sig) => (Char -> Bool) -> m Char
-satisfy p = send (Symbol p ret)
+satisfy p = send (Satisfy p ret)
