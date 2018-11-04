83869d63f3551ce5aa4465b55b45e43c0c7b9204
Rob Rix <rob.rix@me.com>
2018-11-04 11:57:52 -0500

    Define a smart constructor for once.

--- src/Control/Effect/Once.hs
+++ src/Control/Effect/Once.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE DeriveFunctor, ExistentialQuantification, StandaloneDeriving #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, FlexibleContexts, StandaloneDeriving #-}
 module Control.Effect.Once where
 
 import Control.Effect.Carrier
+import Control.Effect.Sum
 
 data Once m k
   = forall a . Once (m a) (a -> k)
@@ -15,3 +16,6 @@ instance HFunctor Once where
 instance Effect Once where
   handle state handler (Once m k) = Once (handler (m <$ state)) (handler . fmap k)
   {-# INLINE handle #-}
+
+once :: (Carrier sig m, Member Once sig) => m a -> m a
+once m = send (Once m ret)
