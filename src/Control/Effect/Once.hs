c83f6e6bfbd5868fd706808bafd54c389ece370a
Rob Rix <rob.rix@me.com>
2018-11-04 11:55:47 -0500

    Derive a Functor instance for Once.

--- src/Control/Effect/Once.hs
+++ src/Control/Effect/Once.hs
@@ -1,5 +1,7 @@
-{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, StandaloneDeriving #-}
 module Control.Effect.Once where
 
 data Once m k
   = forall a . Once (m a) (a -> k)
+
+deriving instance Functor (Once m)
