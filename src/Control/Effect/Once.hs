137e41b206f7bec887c52705c8f50945193f5c05
Rob Rix <rob.rix@me.com>
2018-11-04 11:56:34 -0500

    Define an HFunctor instance for Once.

--- src/Control/Effect/Once.hs
+++ src/Control/Effect/Once.hs
@@ -1,7 +1,13 @@
 {-# LANGUAGE DeriveFunctor, ExistentialQuantification, StandaloneDeriving #-}
 module Control.Effect.Once where
 
+import Control.Effect.Carrier
+
 data Once m k
   = forall a . Once (m a) (a -> k)
 
 deriving instance Functor (Once m)
+
+instance HFunctor Once where
+  hmap f (Once m k) = Once (f m) k
+  {-# INLINE hmap #-}
