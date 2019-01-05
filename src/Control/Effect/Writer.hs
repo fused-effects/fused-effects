f0f5113dc0f622dd3e83e32573d5dfe805a75278
Rob Rix <rob.rix@me.com>
2019-01-05 11:47:34 -0500

    Inline all the things!

--- src/Control/Effect/Writer.hs
+++ src/Control/Effect/Writer.hs
@@ -28,6 +28,7 @@ instance Effect (Writer w) where
 --   prop> fst (run (runWriter (mapM_ (tell . Sum) (0 : ws)))) == foldMap Sum ws
 tell :: (Member (Writer w) sig, Carrier sig m) => w -> m ()
 tell w = send (Tell w (ret ()))
+{-# INLINE tell #-}
 
 
 -- | Run a 'Writer' effect with a 'Monoid'al log, producing the final log alongside the result value.
@@ -35,22 +36,27 @@ tell w = send (Tell w (ret ()))
 --   prop> run (runWriter (tell (Sum a) *> pure b)) == (Sum a, b)
 runWriter :: (Carrier sig m, Effect sig, Monoid w) => Eff (WriterC w m) a -> m (w, a)
 runWriter m = runWriterC (interpret m) mempty
+{-# INLINE runWriter #-}
 
 -- | Run a 'Writer' effect with a 'Monoid'al log, producing the final log and discarding the result value.
 --
 --   prop> run (execWriter (tell (Sum a) *> pure b)) == Sum a
 execWriter :: (Carrier sig m, Effect sig, Functor m, Monoid w) => Eff (WriterC w m) a -> m w
 execWriter = fmap fst . runWriter
+{-# INLINE execWriter #-}
 
 
 newtype WriterC w m a = WriterC { runWriterC :: w -> m (w, a) }
 
 instance (Monoid w, Carrier sig m, Effect sig) => Carrier (Writer w :+: sig) (WriterC w m) where
   ret a = WriterC (\ w -> ret (w, a))
+  {-# INLINE ret #-}
+
   eff op = WriterC (\ w -> handleSum
     (eff . handleState w runWriterC)
     (\ (Tell w' k) -> let w'' = mappend w w' in w'' `seq` runWriterC k w'')
     op)
+  {-# INLINE eff #-}
 
 
 -- $setup
