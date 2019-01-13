2a10f9351d8602b6c32b7af8830d58c76dad0655
Kosyrev Serge <kosyrev.serge@protonmail.com>
2019-01-13 16:34:10 +0300

    test/Control/Effect/LiftIO.liftio:  demo the problem
    [2 of 3] Compiling Control.Effect.LiftIO ( test/Control/Effect/LiftIO.hs, dist/build/test/test-tmp/Control/Effect/LiftIO.o )
    
    test/Control/Effect/LiftIO.hs:26:18: error:
        â¢ Could not deduce (Member (Lift IO) (Lift m))
            arising from a use of âliftIOâ
          from the context: MonadIO m
            bound by the type signature for:
                       doliftio :: forall (m :: * -> *). MonadIO m => m ()
            at test/Control/Effect/LiftIO.hs:25:1-29
        â¢ In the first argument of ârunMâ, namely â(liftIO $ putStr "")â
          In the expression: runM (liftIO $ putStr "")
          In an equation for âdoliftioâ: doliftio = runM (liftIO $ putStr "")
       |
    28 | doliftio = runM (liftIO $ putStr "")
       |                  ^^^^^^^^^^^^^^^^^^

---
+++ test/Control/Effect/LiftIO.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, RankNTypes, TypeInType, UndecidableInstances #-}
+module Control.Effect.LiftIO
+  (spec)
+where
+
+import Control.Monad.IO.Class (MonadIO, liftIO)
+
+import Control.Effect
+import Data.Coerce
+import Control.Effect.Lift
+import Control.Effect.Carrier
+import Control.Effect.Reader
+import Prelude hiding (fail)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  liftio
+
+liftio :: Spec
+liftio = describe "liftio" $ do
+  it "can run liftIO with the underlying monad not being IO" $
+    doliftio >>= (`shouldBe` ())
+
+doliftio :: MonadIO m => m ()
+doliftio = runM (liftIO $ putStr "")
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import qualified Control.Effect.Spec
+import qualified Control.Effect.LiftIO
 import qualified Control.Effect.NonDet.Spec
 import Test.Hspec
 
@@ -8,3 +9,4 @@ main :: IO ()
 main = hspec . parallel $ do
   describe "Control.Effect.Spec" Control.Effect.Spec.spec
   describe "Control.Effect.NonDet.Spec" Control.Effect.NonDet.Spec.spec
+  describe "Control.Effect.LiftIO" Control.Effect.LiftIO.spec
