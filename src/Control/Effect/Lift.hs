0b9293012157de396047d3aa6fe06a8e6070c834
Patrick Thomson <patrickt@github.com>
2019-01-24 19:47:33 -0500

    Add a general `lift` combinator.

--- src/Control/Effect/Lift.hs
+++ src/Control/Effect/Lift.hs
@@ -1,17 +1,24 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts, MultiParamTypeClasses #-}
 module Control.Effect.Lift
 ( Lift(..)
+, lift
 , runM
 , LiftC(..)
 ) where
 
 import Control.Effect.Carrier
+import Control.Effect.Sum
 import Control.Effect.Internal
 import Control.Effect.Lift.Internal
 
 -- | Extract a 'Lift'ed 'Monad'ic action from an effectful computation.
 runM :: Monad m => Eff (LiftC m) a -> m a
 runM = runLiftC . interpret
+
+-- | Given a @Lift n@ constraint in a signature carried by @m@, use 'lift' to
+-- promote arbitrary actions of type @n a@ to @m a@.
+lift :: (Member (Lift n) sig, Carrier sig m, Functor n, Applicative m) => n a -> m a
+lift = send . Lift . fmap pure
 
 newtype LiftC m a = LiftC { runLiftC :: m a }
 
