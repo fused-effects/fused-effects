1821a5e92d8b42c70b92c65d2b9a4b321390c4e4
Rob Rix <robrix@github.com>
2019-02-24 13:41:13 -0500

    Define FailC in terms of ErrorC.

--- src/Control/Effect/Fail.hs
+++ src/Control/Effect/Fail.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveFunctor, FlexibleInstances, MultiParamTypeClasses, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE DeriveFunctor, FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, TypeOperators, UndecidableInstances #-}
 module Control.Effect.Fail
 ( Fail(..)
 , MonadFail(..)
@@ -6,8 +6,8 @@ module Control.Effect.Fail
 , FailC(..)
 ) where
 
-import Control.Applicative (liftA2)
 import Control.Effect.Carrier
+import Control.Effect.Error
 import Control.Effect.Fail.Internal
 import Control.Effect.Internal
 import Control.Effect.Sum
@@ -17,28 +17,18 @@ import Control.Monad.IO.Class
 -- | Run a 'Fail' effect, returning failure messages in 'Left' and successful computations’ results in 'Right'.
 --
 --   prop> run (runFail (pure a)) == Right a
-runFail :: (Carrier sig m, Effect sig) => Eff (FailC m) a -> m (Either String a)
-runFail = runFailC . interpret
+runFail :: (Carrier sig m, Effect sig, Monad m) => Eff (FailC m) a -> m (Either String a)
+runFail = runErrorC . runFailC . interpret
 
-newtype FailC m a = FailC { runFailC :: m (Either String a) }
-  deriving (Functor)
-
-instance Applicative m => Applicative (FailC m) where
-  pure a = FailC (pure (Right a))
-  FailC f <*> FailC a = FailC (liftA2 (<*>) f a)
-
-instance Monad m => Monad (FailC m) where
-  FailC a >>= f = FailC (a >>= either (pure . Left) (runFailC . f))
+newtype FailC m a = FailC { runFailC :: ErrorC String m a }
+  deriving (Applicative, Functor, Monad, MonadIO)
 
 instance Monad m => MonadFail (FailC m) where
-  fail s = FailC (pure (Left s))
+  fail s = FailC (ErrorC (pure (Left s)))
 
-instance MonadIO m => MonadIO (FailC m) where
-  liftIO io = FailC (Right <$> liftIO io)
-
-instance (Carrier sig m, Effect sig) => Carrier (Fail :+: sig) (FailC m) where
-  ret a = FailC (ret (Right a))
-  eff = FailC . handleSum (eff . handleEither runFailC) (\ (Fail s) -> ret (Left s))
+instance (Carrier sig m, Effect sig, Monad m) => Carrier (Fail :+: sig) (FailC m) where
+  ret a = FailC (ret a)
+  eff = FailC . handleSum (eff . R . handleCoercible) (\ (Fail s) -> throwError s)
 
 
 -- $setup
