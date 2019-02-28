e961e6a5afae445ecff03bd39e331122a2b2fa60
Rob Rix <robrix@github.com>
2019-02-27 19:25:47 -0500

    Merge Control.Effect.Random.Internal into Control.Effect.Random.

--- src/Control/Effect/Random.hs
+++ src/Control/Effect/Random.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving, LambdaCase, MultiParamTypeClasses, ScopedTypeVariables, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, FlexibleInstances, GeneralizedNewtypeDeriving, LambdaCase, MultiParamTypeClasses, ScopedTypeVariables, StandaloneDeriving, TypeOperators, UndecidableInstances #-}
 module Control.Effect.Random
 ( Random(..)
 , runRandom
@@ -12,7 +12,6 @@ module Control.Effect.Random
 
 import Control.Applicative (Alternative(..))
 import Control.Effect.Carrier
-import Control.Effect.Random.Internal
 import Control.Effect.State
 import Control.Effect.Sum
 import Control.Monad (MonadPlus(..))
@@ -20,6 +19,25 @@ import Control.Monad.Fail
 import Control.Monad.Random.Class (MonadInterleave(..), MonadRandom(..))
 import Control.Monad.IO.Class (MonadIO(..))
 import qualified System.Random as R (Random(..), RandomGen(..), StdGen, newStdGen)
+
+data Random m k
+  = forall a . R.Random a => Random (a -> k)
+  | forall a . R.Random a => RandomR (a, a) (a -> k)
+  | forall a . Interleave (m a) (a -> k)
+
+deriving instance Functor (Random m)
+
+instance HFunctor Random where
+  hmap _ (Random k) = Random k
+  hmap _ (RandomR r k) = RandomR r k
+  hmap f (Interleave m k) = Interleave (f m) k
+  {-# INLINE hmap #-}
+
+instance Effect Random where
+  handle state handler (Random    k) = Random    (handler . (<$ state) . k)
+  handle state handler (RandomR r k) = RandomR r (handler . (<$ state) . k)
+  handle state handler (Interleave m k) = Interleave (handler (m <$ state)) (handler . fmap k)
+
 
 -- | Run a random computation starting from a given generator.
 --
