31a3812bbe72c3e225027bf39d84ebfca8b505fa
Rob Rix <robrix@github.com>
2019-03-20 12:21:53 -0400

    Test runInterpret.

--- src/Control/Effect/Interpret.hs
+++ src/Control/Effect/Interpret.hs
@@ -16,6 +16,8 @@ import Control.Monad.Trans.Class
 -- | Interpret an effect using a higher-order function.
 --
 --   This involves a great deal less boilerplate than defining a custom 'Carrier' instance, at the expense of somewhat less performance. It’s a reasonable starting point for new interpretations, and if more performance or flexibility is required, it’s straightforward to “graduate” by replacing the relevant 'runInterpret' handlers with specialized 'Carrier' instances for the effects.
+--
+--   prop> run (runInterpret (\ op -> case op of { Get k -> k a ; Put _ k -> k }) get) == a
 runInterpret :: (forall x . eff m (m x) -> m x) -> InterpretC eff m a -> m a
 runInterpret handler = runReader (Handler handler) . runInterpretC
 
@@ -35,3 +37,9 @@ instance (HFunctor eff, Carrier sig m) => Carrier (eff :+: sig) (InterpretC eff 
     handler <- InterpretC ask
     lift (runHandler handler op)
   eff (R other) = InterpretC (eff (R (handleCoercible other)))
+
+
+-- $setup
+-- >>> import Test.QuickCheck
+-- >>> import Control.Effect.Pure
+-- >>> import Control.Effect.State
