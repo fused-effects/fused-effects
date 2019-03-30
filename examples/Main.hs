5b388c9fd56c895a054a672dcf78dde47787d70b
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-03-30 15:24:40 -0400

    Add examples/ReinterpretLog.hs

--- examples/Main.hs
+++ examples/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
 import qualified Parser
+import qualified ReinterpretLog
 import qualified Teletype
 import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
   Teletype.spec
+  ReinterpretLog.spec
   Parser.spec
