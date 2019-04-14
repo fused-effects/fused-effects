827eac4a8421c4e297aa5a4951522980e472e512
Patrick Thomson <patrickt@github.com>
2019-04-14 14:32:04 -0400

    Use cabal-doctest to get doctests working on 8.2.2.

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,6 @@
-import Distribution.Simple
-main = defaultMain
+module Main where
+
+import Distribution.Extra.Doctest (defaultMainWithDoctests)
+
+main :: IO ()
+main = defaultMainWithDoctests "doctest"
--- test/Doctest.hs
+++ test/Doctest.hs
@@ -2,10 +2,14 @@ module Main
 ( main
 ) where
 
+import Control.Monad.Base
 import System.Environment
 import Test.DocTest
+import Build_doctests (Component (..), components)
+import Data.Foldable
 
 main :: IO ()
-main = do
+main = for_ components $ \(Component name flags pkgs sources) -> do
   args <- getArgs
-  doctest ("-isrc" : "--fast" : if null args then ["src"] else args)
+  let args = "--fast" : flags ++ pkgs ++ sources
+  doctest args
