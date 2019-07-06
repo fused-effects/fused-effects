fe90124bfe5d4dca704924ff86e3cccb97ec5ace
Rob Rix <rob.rix@me.com>
2019-07-06 01:08:11 -0400

    Derive the Effect instance for Fail.

--- src/Control/Effect/Fail.hs
+++ src/Control/Effect/Fail.hs
@@ -14,16 +14,12 @@ import Control.Monad (MonadPlus(..))
 import Control.Monad.Fail
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
-import Data.Coerce
 import GHC.Generics (Generic1)
 import Prelude hiding (fail)
 
 newtype Fail (m :: * -> *) k = Fail String
   deriving stock (Functor, Generic1)
-  deriving anyclass (HFunctor)
-
-instance Effect Fail where
-  handle _ _ = coerce
+  deriving anyclass (HFunctor, Effect)
 
 -- | Run a 'Fail' effect, returning failure messages in 'Left' and successful computations’ results in 'Right'.
 --
