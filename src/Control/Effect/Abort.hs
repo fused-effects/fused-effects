62aa664dbe81771e74104ba3dc69fd91f9f3ccce
Rob Rix <rob.rix@me.com>
2019-08-21 11:52:52 +0200

    Define an AbortC monad.

--- src/Control/Effect/Abort.hs
+++ src/Control/Effect/Abort.hs
@@ -3,12 +3,16 @@ module Control.Effect.Abort
 ( -- * Abort effect
   Abort(..)
 , abort
+  -- * Abort carrier
+, runAbort
+, AbortC(..)
   -- * Re-exports
 , Carrier
 , Member
 , run
 ) where
 
+import Control.Applicative (liftA2)
 import Control.Effect.Carrier
 
 data Abort (m :: * -> *) k = Abort
@@ -17,3 +21,17 @@ data Abort (m :: * -> *) k = Abort
 -- | Abort the computation.
 abort :: (Carrier sig m, Member Abort sig) => m a
 abort = send Abort
+
+
+runAbort :: AbortC m a -> m (Maybe a)
+runAbort = runAbortC
+
+newtype AbortC m a = AbortC { runAbortC :: m (Maybe a) }
+  deriving (Functor)
+
+instance Applicative m => Applicative (AbortC m) where
+  pure = AbortC . pure . Just
+  AbortC f <*> AbortC a = AbortC (liftA2 (<*>) f a)
+
+instance Monad m => Monad (AbortC m) where
+  AbortC a >>= f = AbortC (a >>= maybe (pure Nothing) (runAbortC . f))
