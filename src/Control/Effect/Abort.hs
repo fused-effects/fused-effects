b0d33469cad37d234d3ac56bc999361668ca3c22
Rob Rix <rob.rix@me.com>
2019-08-21 11:34:25 +0200

    Define a smart constructor for abort.

--- src/Control/Effect/Abort.hs
+++ src/Control/Effect/Abort.hs
@@ -1,8 +1,14 @@
-{-# LANGUAGE DeriveFunctor, KindSignatures #-}
+{-# LANGUAGE DeriveFunctor, FlexibleContexts, KindSignatures #-}
 module Control.Effect.Abort
 ( -- * Abort effect
   Abort(..)
+, abort
 ) where
+
+import Control.Effect.Carrier
 
 data Abort (m :: * -> *) k = Abort
   deriving (Functor)
+
+abort :: (Carrier sig m, Member Abort sig) => m a
+abort = send Abort
