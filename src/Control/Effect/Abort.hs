6ac6734cef54ebfbd1c1a4ff8a07136f60a6d495
Rob Rix <rob.rix@me.com>
2019-08-21 11:33:27 +0200

    Derive a Functor instance for Abort.

--- src/Control/Effect/Abort.hs
+++ src/Control/Effect/Abort.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DeriveFunctor, KindSignatures #-}
 module Control.Effect.Abort
 ( -- * Abort effect
   Abort(..)
 ) where
 
 data Abort (m :: * -> *) k = Abort
+  deriving (Functor)
