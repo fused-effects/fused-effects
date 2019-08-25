e0eb43739c685d6f2740a6cc87cbeb2c39f0ad32
Rob Rix <rob.rix@me.com>
2019-08-25 11:54:11 +0200

    Define HFunctor & Effect instances for NonDet.

--- src/Control/Effect/NonDet/NonEmpty.hs
+++ src/Control/Effect/NonDet/NonEmpty.hs
@@ -4,8 +4,12 @@ module Control.Effect.NonDet.NonEmpty
   NonDet(..)
 ) where
 
+import Control.Effect.Carrier
 import GHC.Generics (Generic1)
 
 data NonDet m k
   = Choose (Bool -> m k)
   deriving (Functor, Generic1)
+
+instance HFunctor NonDet
+instance Effect   NonDet
