22cde3ffd57a8586885c914b0b9ca361ead0d21e
Rob Rix <rob.rix@me.com>
2019-08-25 11:15:54 +0200

    Derive a Generic1 instance for NonDet.

--- src/Control/Effect/NonDet/NonEmpty.hs
+++ src/Control/Effect/NonDet/NonEmpty.hs
@@ -1,9 +1,11 @@
-{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFunctor, DeriveGeneric #-}
 module Control.Effect.NonDet.NonEmpty
 ( -- * NonDet effect
   NonDet(..)
 ) where
 
+import GHC.Generics (Generic1)
+
 data NonDet m k
   = Choose (Bool -> m k)
-  deriving (Functor)
+  deriving (Functor, Generic1)
