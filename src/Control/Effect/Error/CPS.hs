208af69e0dae1cb5118d82c1ec48f84e6e8874c5
Rob Rix <rob.rix@me.com>
2019-08-25 17:14:37 -0400

    Derive a Functor instance for ErrorC.

--- src/Control/Effect/Error/CPS.hs
+++ src/Control/Effect/Error/CPS.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DeriveFunctor, RankNTypes #-}
 module Control.Effect.Error.CPS
 ( -- * Error effect
   module Control.Effect.Error
@@ -9,3 +9,4 @@ module Control.Effect.Error.CPS
 import Control.Effect.Error (Error, throwError, catchError)
 
 newtype ErrorC e m a = ErrorC { runErrorC :: forall b . (e -> m b) -> (a -> m b) -> m b }
+  deriving (Functor)
