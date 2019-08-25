32de1d326ad8ce10e9a9749f82fcb5f30ac7616d
Rob Rix <rob.rix@me.com>
2019-08-25 12:01:04 +0200

    Improve the docs for runNonDetC.

--- src/Control/Effect/NonDet/NonEmpty.hs
+++ src/Control/Effect/NonDet/NonEmpty.hs
@@ -24,7 +24,7 @@ choose a b = send (Choose (bool b a))
 
 -- | A carrier for 'NonDet' effects based on Ralf Hinze’s design described in [Deriving Backtracking Monad Transformers](https://www.cs.ox.ac.uk/ralf.hinze/publications/#P12).
 newtype NonDetC m a = NonDetC
-  { -- | A higher-order function receiving three continuations, respectively implementing '<|>' and 'pure'.
+  { -- | A higher-order function receiving three continuations, respectively implementing choice and 'pure'.
     runNonDetC :: forall b . (m b -> m b -> m b) -> (a -> m b) -> m b
   }
   deriving (Functor)
