a04c5ec5c3bebfaa84eaca1d5e179041a0bd277a
Rob Rix <rob.rix@me.com>
2019-08-25 13:20:46 +0200

    Re-export Choose/ChooseC.

--- src/Control/Effect.hs
+++ src/Control/Effect.hs
@@ -4,6 +4,7 @@ module Control.Effect
 
 import Control.Effect.Abort     as X (Abort, AbortC)
 import Control.Effect.Carrier   as X ((:+:), Carrier, Effect, Member)
+import Control.Effect.Choose    as X (Choose, ChooseC)
 import Control.Effect.Cull      as X (Cull, CullC, OnceC)
 import Control.Effect.Cut       as X (Cut, CutC)
 import Control.Effect.Error     as X (Error, ErrorC)
