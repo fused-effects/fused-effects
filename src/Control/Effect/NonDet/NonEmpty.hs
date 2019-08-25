40646e64cba6523d267de7ac02822aec9a92e449
Rob Rix <rob.rix@me.com>
2019-08-25 12:06:33 +0200

    Define a handler for NonDetC.

--- src/Control/Effect/NonDet/NonEmpty.hs
+++ src/Control/Effect/NonDet/NonEmpty.hs
@@ -4,6 +4,7 @@ module Control.Effect.NonDet.NonEmpty
   NonDet(..)
 , choose
   -- * NonDet carrier
+, runNonDet
 , NonDetC(..)
 ) where
 
@@ -25,6 +26,9 @@ instance Effect   NonDet
 choose :: (Carrier sig m, Member NonDet sig) => m a -> m a -> m a
 choose a b = send (Choose (bool b a))
 
+
+runNonDet :: (m b -> m b -> m b) -> (a -> m b) -> NonDetC m a -> m b
+runNonDet fork leaf m = runNonDetC m fork leaf
 
 -- | A carrier for 'NonDet' effects based on Ralf Hinze’s design described in [Deriving Backtracking Monad Transformers](https://www.cs.ox.ac.uk/ralf.hinze/publications/#P12).
 newtype NonDetC m a = NonDetC
