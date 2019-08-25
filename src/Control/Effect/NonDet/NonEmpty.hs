98cd78071aae67b2dd118579b2a2b53154376b77
Rob Rix <rob.rix@me.com>
2019-08-25 11:59:00 +0200

    Define a carrier for non-empty nondeterminism.

--- src/Control/Effect/NonDet/NonEmpty.hs
+++ src/Control/Effect/NonDet/NonEmpty.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE DeriveFunctor, DeriveGeneric #-}
+{-# LANGUAGE DeriveFunctor, DeriveGeneric, RankNTypes #-}
 module Control.Effect.NonDet.NonEmpty
 ( -- * NonDet effect
   NonDet(..)
+  -- * NonDet carrier
+, NonDetC(..)
 ) where
 
 import Control.Effect.Carrier
@@ -13,3 +15,11 @@ data NonDet m k
 
 instance HFunctor NonDet
 instance Effect   NonDet
+
+
+-- | A carrier for 'NonDet' effects based on Ralf Hinze’s design described in [Deriving Backtracking Monad Transformers](https://www.cs.ox.ac.uk/ralf.hinze/publications/#P12).
+newtype NonDetC m a = NonDetC
+  { -- | A higher-order function receiving three continuations, respectively implementing '<|>' and 'pure'.
+    runNonDetC :: forall b . (m b -> m b -> m b) -> (a -> m b) -> m b
+  }
+  deriving (Functor)
