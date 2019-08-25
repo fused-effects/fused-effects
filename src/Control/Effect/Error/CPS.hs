e45d5f40467436c11dfcc09d0401f67a21c11390
Rob Rix <rob.rix@me.com>
2019-08-25 17:19:27 -0400

    Define a Monad instance for ErrorC.

--- src/Control/Effect/Error/CPS.hs
+++ src/Control/Effect/Error/CPS.hs
@@ -14,3 +14,6 @@ newtype ErrorC e m a = ErrorC { runErrorC :: forall b . (e -> m b) -> (a -> m b)
 instance Applicative (ErrorC e m) where
   pure a = ErrorC $ \ _ k -> k a
   ErrorC f <*> ErrorC a = ErrorC $ \ h k -> f h (\ f' -> a h (k . f'))
+
+instance Monad (ErrorC e m) where
+  ErrorC a >>= f = ErrorC $ \ h k -> a h (\ a' -> runErrorC (f a') h k)
