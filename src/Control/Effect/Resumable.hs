027c7f88e9ab004616b77711d73f8bf2995fdf89
Rob Rix <rob.rix@me.com>
2019-08-31 18:26:31 -0400

    Move SomeError into Control.Carrier.Resumable.Either.

--- src/Control/Effect/Resumable.hs
+++ src/Control/Effect/Resumable.hs
@@ -3,7 +3,6 @@ module Control.Effect.Resumable
 ( -- * Resumable effect
   Resumable(..)
 , throwResumable
-, SomeError(..)
   -- * Resumable carriers
 , runResumableWith
 , ResumableWithC(..)
@@ -14,7 +13,6 @@ module Control.Effect.Resumable
 ) where
 
 import Control.Applicative (Alternative(..))
-import Control.DeepSeq
 import Control.Carrier.Class
 import Control.Carrier.Reader
 import Control.Monad (MonadPlus(..))
@@ -22,7 +20,6 @@ import Control.Monad.Fail
 import Control.Monad.Fix
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
-import Data.Functor.Classes
 
 -- | Errors which can be resumed with values of some existentially-quantified type.
 data Resumable err m k
@@ -41,45 +38,6 @@ instance Effect (Resumable err) where
 --   prop> run (runResumable (throwResumable (Identity a))) === Left (SomeError (Identity a))
 throwResumable :: (Member (Resumable err) sig, Carrier sig m) => err a -> m a
 throwResumable err = send (Resumable err pure)
-
-
--- | An error at some existentially-quantified type index.
-data SomeError err
-  = forall a . SomeError (err a)
-
--- | Equality for 'SomeError' is determined by an 'Eq1' instance for the error type.
---
---   Note that since we can’t tell whether the type indices are equal, let alone what 'Eq' instance to use for them, the comparator passed to 'liftEq' always returns 'True'. Thus, 'SomeError' is best used with type-indexed GADTs for the error type.
---
---   prop> SomeError (Identity a) === SomeError (Identity b)
---   prop> (SomeError (Const a) === SomeError (Const b)) == (a == b)
-instance Eq1 err => Eq (SomeError err) where
-  SomeError exc1 == SomeError exc2 = liftEq (const (const True)) exc1 exc2
-
--- | Ordering for 'SomeError' is determined by an 'Ord1' instance for the error type.
---
---   Note that since we can’t tell whether the type indices are equal, let alone what 'Ord' instance to use for them, the comparator passed to 'liftCompare' always returns 'EQ'. Thus, 'SomeError' is best used with type-indexed GADTs for the error type.
---
---   prop> (SomeError (Identity a) `compare` SomeError (Identity b)) === EQ
---   prop> (SomeError (Const a) `compare` SomeError (Const b)) === (a `compare` b)
-instance Ord1 err => Ord (SomeError err) where
-  SomeError exc1 `compare` SomeError exc2 = liftCompare (const (const EQ)) exc1 exc2
-
--- | Showing for 'SomeError' is determined by a 'Show1' instance for the error type.
---
---   Note that since we can’t tell what 'Show' instance to use for the type index, the functions passed to 'liftShowsPrec' always return the empty 'ShowS'. Thus, 'SomeError' is best used with type-indexed GADTs for the error type.
---
---   prop> show (SomeError (Identity a)) === "SomeError (Identity )"
---   prop> show (SomeError (Const a)) === ("SomeError (Const " ++ showsPrec 11 a ")")
-instance Show1 err => Show (SomeError err) where
-  showsPrec d (SomeError err) = showsUnaryWith (liftShowsPrec (const (const id)) (const id)) "SomeError" d err
-
-
--- | Evaluation of 'SomeError' to normal forms is determined by a 'NFData1' instance for the error type.
---
---   prop> pure (rnf (SomeError (Identity (error "error"))) :: SomeError Identity) `shouldThrow` errorCall "error"
-instance NFData1 err => NFData (SomeError err) where
-  rnf (SomeError err) = liftRnf (\a -> seq a ()) err
 
 
 -- | Run a 'Resumable' effect, resuming uncaught errors with a given handler.
