d4132f9287ca209a04d6e16a3381739a00f94dcf
Rob Rix <rob.rix@me.com>
2019-09-03 11:40:15 -0400

    Correct a doc comment.

--- src/Control/Effect/NonDet/NonEmpty.hs
+++ src/Control/Effect/NonDet/NonEmpty.hs
@@ -36,7 +36,7 @@ runNonDet fork leaf m = runNonDetC m fork leaf
 
 -- | A carrier for 'NonDet' effects based on Ralf Hinze’s design described in [Deriving Backtracking Monad Transformers](https://www.cs.ox.ac.uk/ralf.hinze/publications/#P12).
 newtype NonDetC m a = NonDetC
-  { -- | A higher-order function receiving three continuations, respectively implementing choice and 'pure'.
+  { -- | A higher-order function receiving two continuations, respectively implementing choice and 'pure'.
     runNonDetC :: forall b . (m b -> m b -> m b) -> (a -> m b) -> m b
   }
   deriving (Functor)
