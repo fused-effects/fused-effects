82ef7a5e1579153c7e17e7a0df3f0e75d49b7411
Rob Rix <rob.rix@me.com>
2019-09-06 13:58:53 -0400

    :memo: Empty.

--- src/Control/Effect/Empty.hs
+++ src/Control/Effect/Empty.hs
@@ -21,6 +21,9 @@ import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 import GHC.Generics (Generic1)
 
+-- | An effect modelling nondeterminism without choice.
+--
+--   This can be seen as similar to 'Control.Effect.Fail', but without an error message.
 data Empty (m :: * -> *) k = Empty
   deriving (Functor, Generic1)
 
