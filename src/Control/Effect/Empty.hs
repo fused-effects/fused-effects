e12279fc6ef5c20e64d5356cd9df716bf11251d1
Rob Rix <rob.rix@me.com>
2019-09-06 14:00:04 -0400

    Inline all the things.

--- src/Control/Effect/Empty.hs
+++ src/Control/Effect/Empty.hs
@@ -49,32 +49,42 @@ newtype EmptyC m a = EmptyC { runEmptyC :: m (Maybe a) }
 
 instance Applicative m => Applicative (EmptyC m) where
   pure = EmptyC . pure . Just
+  {-# INLINE pure #-}
   EmptyC f <*> EmptyC a = EmptyC (liftA2 (<*>) f a)
+  {-# INLINE (<*>) #-}
 
 instance Applicative m => Alternative (EmptyC m) where
   empty = EmptyC (pure Nothing)
+  {-# INLINE empty #-}
   EmptyC a <|> EmptyC b = EmptyC (liftA2 (<|>) a b)
+  {-# INLINE (<|>) #-}
 
 instance Monad m => Monad (EmptyC m) where
   EmptyC a >>= f = EmptyC (a >>= maybe (pure Nothing) (runEmptyC . f))
+  {-# INLINE (>>=) #-}
 
 instance Fail.MonadFail m => Fail.MonadFail (EmptyC m) where
   fail = lift . Fail.fail
+  {-# INLINE fail #-}
 
 instance MonadFix m => MonadFix (EmptyC m) where
   mfix f = EmptyC (mfix (runEmpty . maybe (error "mfix (EmptyC): function returned failure") f))
+  {-# INLINE mfix #-}
 
 instance MonadIO m => MonadIO (EmptyC m) where
   liftIO = lift . liftIO
+  {-# INLINE liftIO #-}
 
 instance (Alternative m, Monad m) => MonadPlus (EmptyC m)
 
 instance MonadTrans EmptyC where
   lift = EmptyC . fmap Just
+  {-# INLINE lift #-}
 
 instance (Carrier sig m, Effect sig) => Carrier (Empty :+: sig) (EmptyC m) where
   eff (L Empty) = EmptyC (pure Nothing)
   eff (R other) = EmptyC (eff (handle (Just ()) (maybe (pure Nothing) runEmptyC) other))
+  {-# INLINE eff #-}
 
 
 -- $setup
