1bb52990c7463fba9e1cbc96d3bea066be4f2670
Rob Rix <rob.rix@me.com>
2019-09-09 16:33:05 -0400

    INLINE all the things.

--- src/Control/Effect/NonDet.hs
+++ src/Control/Effect/NonDet.hs
@@ -106,14 +106,19 @@ data BinaryTree a = Nil | Leaf a | Fork (BinaryTree a) (BinaryTree a)
 
 instance Applicative BinaryTree where
   pure = Leaf
+  {-# INLINE pure #-}
   f <*> a = fold Fork (<$> a) Nil f
+  {-# INLINE (<*>) #-}
 
 instance Alternative BinaryTree where
   empty = Nil
+  {-# INLINE empty #-}
   (<|>) = Fork
+  {-# INLINE (<|>) #-}
 
 instance Monad BinaryTree where
   a >>= f = fold Fork f Nil a
+  {-# INLINE (>>=) #-}
 
 
 fold :: (b -> b -> b) -> (a -> b) -> b -> BinaryTree a -> b
@@ -121,6 +126,7 @@ fold fork leaf nil = go where
   go Nil        = nil
   go (Leaf a)   = leaf a
   go (Fork a b) = fork (go a) (go b)
+{-# INLINE fold #-}
 
 
 -- $setup
