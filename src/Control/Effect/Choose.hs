bc99fa6b46d6ab8bb2e8c6344203cae63be0af73
Rob Rix <rob.rix@me.com>
2019-09-10 21:47:28 -0400

    Inline all the things!

--- src/Control/Effect/Choose.hs
+++ src/Control/Effect/Choose.hs
@@ -83,13 +83,17 @@ data BinaryTree a = Leaf a | Fork (BinaryTree a) (BinaryTree a)
 
 instance Applicative BinaryTree where
   pure = Leaf
+  {-# INLINE pure #-}
   f <*> a = fold Fork (<$> a) f
+  {-# INLINE (<*>) #-}
 
 instance Monad BinaryTree where
   a >>= f = fold Fork f a
+  {-# INLINE (>>=) #-}
 
 
 fold :: (b -> b -> b) -> (a -> b) -> BinaryTree a -> b
 fold fork leaf = go where
   go (Leaf a)   = leaf a
   go (Fork a b) = fork (go a) (go b)
+{-# INLINE fold #-}
