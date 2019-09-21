41bc7c348943e4e55fcdf93768b42d13e97a8865
Rob Rix <rob.rix@me.com>
2019-09-21 01:47:46 -0400

    Reintroduce Control.Effect.NonDet, re-exporting Choose, Empty, & Alternative.

---
+++ src/Control/Effect/NonDet.hs
@@ -0,0 +1,12 @@
+module Control.Effect.NonDet
+( -- * Choose effect
+  module Control.Effect.Choose
+  -- * Empty effect
+, module Control.Effect.Empty
+  -- * Re-exports
+, Alternative(..)
+) where
+
+import Control.Applicative (Alternative(..))
+import Control.Effect.Choose hiding (many, some)
+import Control.Effect.Empty
