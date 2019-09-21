61e047abc4aa3aa8c0927fed80398050cb928252
Rob Rix <rob.rix@me.com>
2019-09-21 01:10:25 -0400

    :fire: a redundant import.

--- src/Control/Carrier/Fail/Either.hs
+++ src/Control/Carrier/Fail/Either.hs
@@ -43,4 +43,3 @@ instance (Carrier sig m, Effect sig) => Carrier (Fail :+: sig) (FailC m) where
 -- $setup
 -- >>> :seti -XFlexibleContexts
 -- >>> import Test.QuickCheck
--- >>> import Control.Effect.Pure
