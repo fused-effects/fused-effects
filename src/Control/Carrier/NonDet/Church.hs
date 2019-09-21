1f3f8878740d48423a7b2e4365ade856b48ffcdb
Rob Rix <rob.rix@me.com>
2019-09-21 01:55:44 -0400

    Move oneOf into Control.Effect.NonDet.

--- src/Control/Carrier/NonDet/Church.hs
+++ src/Control/Carrier/NonDet/Church.hs
@@ -5,7 +5,6 @@ module Control.Carrier.NonDet.Church
   -- * NonDet carrier
 , runNonDet
 , NonDetC(..)
-, oneOf
   -- * Re-exports
 , Carrier
 , Member
@@ -21,7 +20,6 @@ import Control.Monad.Fix
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 import Data.Maybe (fromJust)
-import Data.Monoid
 
 -- | Run a 'NonDet' effect, collecting all branches’ results into an 'Alternative' functor.
 --
@@ -31,20 +29,6 @@ import Data.Monoid
 --   prop> run (runNonDet (pure a)) === Just a
 runNonDet :: (Alternative f, Applicative m) => NonDetC m a -> m (f a)
 runNonDet (NonDetC m) = m (liftA2 (<|>)) (pure . pure) (pure empty)
-
--- | Nondeterministically choose an element from a 'Foldable' collection.
--- This can be used to emulate the style of nondeterminism associated with
--- programming in the list monad:
--- @
---   pythagoreanTriples = do
---     a <- oneOf [1..10]
---     b <- oneOf [1..10]
---     c <- oneOf [1..10]
---     guard (a^2 + b^2 == c^2)
---     pure (a, b, c)
--- @
-oneOf :: (Foldable t, Alternative m) => t a -> m a
-oneOf = getAlt . foldMap (Alt . pure)
 
 -- | A carrier for 'NonDet' effects based on Ralf Hinze’s design described in [Deriving Backtracking Monad Transformers](https://www.cs.ox.ac.uk/ralf.hinze/publications/#P12).
 newtype NonDetC m a = NonDetC
--- src/Control/Effect/NonDet.hs
+++ src/Control/Effect/NonDet.hs
@@ -2,6 +2,7 @@ module Control.Effect.NonDet
 ( -- * NonDet effects
   module Control.Effect.Choose
 , module Control.Effect.Empty
+, oneOf
   -- * Re-exports
 , Alternative(..)
 ) where
@@ -9,3 +10,18 @@ module Control.Effect.NonDet
 import Control.Applicative (Alternative(..))
 import Control.Effect.Choose hiding ((<|>), many, some)
 import Control.Effect.Empty hiding (empty)
+import Data.Monoid (Alt(..))
+
+-- | Nondeterministically choose an element from a 'Foldable' collection.
+-- This can be used to emulate the style of nondeterminism associated with
+-- programming in the list monad:
+-- @
+--   pythagoreanTriples = do
+--     a <- oneOf [1..10]
+--     b <- oneOf [1..10]
+--     c <- oneOf [1..10]
+--     guard (a^2 + b^2 == c^2)
+--     pure (a, b, c)
+-- @
+oneOf :: (Foldable t, Alternative m) => t a -> m a
+oneOf = getAlt . foldMap (Alt . pure)
