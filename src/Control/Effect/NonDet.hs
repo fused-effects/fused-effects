f65b114105936e8899c32c1d7dc6902dcff7b99f
Rob Rix <rob.rix@me.com>
2019-09-21 02:31:24 -0400

    Revert "Define oneOf & foldMapA via the Alternative interfaces."
    This reverts commit 358c54db9a7671e2e3b798a3651061af6843187e.

--- src/Control/Effect/NonDet.hs
+++ src/Control/Effect/NonDet.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts, GeneralizedNewtypeDeriving, UndecidableInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
 -- | 'Choose' & 'Empty'-based nondeterminism interfaces.
 module Control.Effect.NonDet
 ( -- * NonDet effects
@@ -9,7 +9,6 @@ module Control.Effect.NonDet
 ) where
 
 import Control.Carrier
-import qualified Control.Effect.Alternative as Alt
 import Control.Effect.Choose
 import Control.Effect.Empty
 import Data.Coerce
@@ -26,11 +25,11 @@ import Data.Coerce
 --     pure (a, b, c)
 -- @
 oneOf :: (Foldable t, Carrier sig m, Member Choose sig, Member Empty sig) => t a -> m a
-oneOf = runViaEffects . Alt.oneOf
+oneOf = foldMapA pure
 
 -- | Map a 'Foldable' collection of values into a nondeterministic computation using the supplied action.
 foldMapA :: (Foldable t, Carrier sig m, Member Choose sig, Member Empty sig) => (a -> m b) -> t a -> m b
-foldMapA f = runViaEffects #. Alt.foldMapA (ViaEffects #. f)
+foldMapA f = getChoosing #. foldMap (Choosing #. f)
 
 
 -- | Compose a function operationally equivalent to 'id' on the left.
@@ -39,11 +38,3 @@ foldMapA f = runViaEffects #. Alt.foldMapA (ViaEffects #. f)
 (#.) :: Coercible b c => (b -> c) -> (a -> b) -> (a -> c)
 (#.) _ = coerce
 {-# INLINE (#.) #-}
-
-
-newtype ViaEffects m a = ViaEffects { runViaEffects :: m a }
-  deriving (Applicative, Functor, Monad)
-
-instance (Carrier sig m, Member Choose sig, Member Empty sig) => Alt.Alternative (ViaEffects m) where
-  empty = ViaEffects empty
-  ViaEffects m1 <|> ViaEffects m2 = ViaEffects (m1 <|> m2)
