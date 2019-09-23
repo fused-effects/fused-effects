5f66a05205180b8c8d219961331b510929f3a3fc
Rob Rix <rob.rix@me.com>
2019-09-22 20:47:34 -0400

    Define a synonym for Inject + Carrier.

--- src/Control/Carrier.hs
+++ src/Control/Carrier.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE ConstraintKinds #-}
 module Control.Carrier
 ( -- * Re-exports
   module Control.Carrier.Class
 , module Control.Carrier.Pure
 , module Control.Effect.Class
 , module Control.Effect.Sum
+, Has
 , send
 ) where
 
@@ -11,6 +13,8 @@ import Control.Carrier.Class
 import Control.Carrier.Pure
 import Control.Effect.Class
 import Control.Effect.Sum
+
+type Has eff sig m = (Inject eff sig, Carrier sig m)
 
 -- | Construct a request for an effect to be interpreted by some handler later on.
 send :: (Inject effect sig, Carrier sig m) => effect m a -> m a
