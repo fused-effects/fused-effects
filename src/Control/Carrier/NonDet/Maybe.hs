c3adcf5d80509d904abba6907a5917be5ee837a2
Rob Rix <rob.rix@me.com>
2019-09-28 17:05:00 -0400

    Stub in a NonDetC type.

--- src/Control/Carrier/NonDet/Maybe.hs
+++ src/Control/Carrier/NonDet/Maybe.hs
@@ -1,6 +1,9 @@
 module Control.Carrier.NonDet.Maybe
 ( -- * NonDet effects
   module Control.Effect.NonDet
+  -- * NonDet carrier
+, runNonDet
+, NonDetC(..)
   -- * Re-exports
 , Carrier
 , Has
@@ -9,3 +12,8 @@ module Control.Carrier.NonDet.Maybe
 
 import Control.Carrier
 import Control.Effect.NonDet
+
+runNonDet :: NonDetC m a -> m (Maybe a)
+runNonDet = runNonDetC
+
+newtype NonDetC m a = NonDetC { runNonDetC :: m (Maybe a) }
