e51dbaf76fd714d832867b785e0c4e239e19bc9a
Rob Rix <rob.rix@me.com>
2019-09-29 16:18:30 -0400

    :fire: some unnecessary wrapping.

--- examples/Inference.hs
+++ examples/Inference.hs
@@ -12,13 +12,13 @@ example :: TestTree
 example = testGroup "inference"
   [ testProperty "type applications instantiate types" $
   -- Without @-XTypeApplications@ or some other constraint on the type, 'ask' would error: all @ghc@ would be able to prove about type of the 'Reader' effect, and thus the return type of 'ask', is that it’s a list of some kind. The type application allows us to specify it.
-    \ x -> run (runHasEnv (runEnv [x] ((++) <$> ask @String <*> ask @String)))
+    \ x -> run (runEnv [x] ((++) <$> ask @String <*> ask @String))
     === [x, x]
   -- However, when the type is polymorphic, this can require contortions: @-XScopedTypeVariables@ and @forall@ annotations just to bring the type variables into scope, etc., and can be especially inconvenient in @ghci.
   --
   -- Sometimes we would like to be able to constrain the type by context instead. In these cases, we can use a @newtype@ with a phantom type parameter, plus a wrapper around 'ask' which uses that type parameter to constrain its return type, to provide enough context for the types to be inferred without annotation or @-XTypeApplications@.
   , testProperty "phantom type parameters constrain inference" $
-    \ x -> run (runHasEnv (runEnv [x] ((++) <$> askEnv <*> askEnv)))
+    \ x -> run (runEnv [x] ((++) <$> askEnv <*> askEnv))
     === [x, x :: Integer]
   ]
 
@@ -31,8 +31,8 @@ example = testGroup "inference"
 askEnv :: Has (Reader env) sig m => HasEnv env m env
 askEnv = ask
 
-runEnv :: env -> HasEnv env (ReaderC env m) a -> HasEnv env m a
-runEnv r = HasEnv . runReader r . runHasEnv
+runEnv :: env -> HasEnv env (ReaderC env m) a -> m a
+runEnv r = runReader r . runHasEnv
 
 
 newtype HasEnv env m a = HasEnv { runHasEnv :: m a }
