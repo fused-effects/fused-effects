9ab766fecd48f64adcba98ea24ba64dbba37e1a1
Rob Rix <rob.rix@me.com>
2019-09-29 16:31:53 -0400

    :fire: hspec.

--- examples/Main.hs
+++ examples/Main.hs
@@ -6,15 +6,12 @@ import qualified Inference
 import qualified Parser
 import qualified ReinterpretLog
 import qualified Teletype
-import Test.Hspec
 import Test.Tasty
 
 main :: IO ()
-main = do
-  hspec $ pure ()
-  defaultMain $ testGroup "examples"
-    [ Inference.example
-    , Parser.example
-    , ReinterpretLog.example
-    , Teletype.example
-    ]
+main = defaultMain $ testGroup "examples"
+  [ Inference.example
+  , Parser.example
+  , ReinterpretLog.example
+  , Teletype.example
+  ]
