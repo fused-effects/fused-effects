5a48ddd7f205539d5014938ac0b292a55cd7f54c
Rob Rix <rob.rix@me.com>
2019-09-29 14:39:54 -0400

    Factor the error action out.

--- test/NonDet.hs
+++ test/NonDet.hs
@@ -6,6 +6,7 @@ module NonDet
 import Control.Carrier.Error.Either
 import Control.Carrier.NonDet.Church
 import Control.Carrier.State.Strict
+import Prelude hiding (error)
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -18,12 +19,15 @@ tests = testGroup "NonDet"
     run (runState 'a' (runNonDet state))
     @?= ('b', "zbb")
   , testCase "collects results from higher-order effects run inside it" $
-    run (runNonDet (runError ((pure 'z' <|> throwError 'a') `catchError` pure)))
+    run (runNonDet (runError error))
     @?= [Right 'z', Right 'a' :: Either Char Char]
   , testCase "collapses results of higher-order effects run outside it" $
-    run (runError (runNonDet ((pure 'z' <|> throwError 'a') `catchError` pure)))
+    run (runError (runNonDet error))
     @?= (Right "a" :: Either Char String)
   ]
 
 state :: (Alternative m, Has (State Char) sig m) => m Char
 state = pure 'z' <|> put 'b' *> get <|> get
+
+error :: (Alternative m, Has (Error Char) sig m) => m Char
+error = (pure 'z' <|> throwError 'a') `catchError` pure
