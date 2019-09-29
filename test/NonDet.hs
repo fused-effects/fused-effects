25f6d6fb8ff35455c61a9423df13bbdf79c359cb
Rob Rix <rob.rix@me.com>
2019-09-29 14:35:05 -0400

    Rename before/after to inside/outside.

--- test/NonDet.hs
+++ test/NonDet.hs
@@ -10,16 +10,16 @@ import Test.Tasty.HUnit
 
 tests :: TestTree
 tests = testGroup "NonDet"
-  [ testCase "collects results of effects run before it" $
+  [ testCase "collects results of effects run inside it" $
     run (runNonDet (runState 'a' (pure 'z' <|> put 'b' *> get <|> get)))
     @?= [('a', 'z'), ('b', 'b'), ('a', 'a')]
-  , testCase "collapses results of effects run after it" $
+  , testCase "collapses results of effects run outside it" $
     run (runState 'a' (runNonDet (pure 'z' <|> put 'b' *> get <|> get)))
     @?= ('b', "zbb")
-  , testCase "collects results from higher-order effects run before it" $
+  , testCase "collects results from higher-order effects run inside it" $
     run (runNonDet (runError ((pure 'z' <|> throwError 'a') `catchError` pure)))
     @?= [Right 'z', Right 'a' :: Either Char Char]
-  , testCase "collapses results of higher-order effects run after it" $
+  , testCase "collapses results of higher-order effects run outside it" $
     run (runError (runNonDet ((pure 'z' <|> throwError 'a') `catchError` pure)))
     @?= (Right "a" :: Either Char String)
   ]
