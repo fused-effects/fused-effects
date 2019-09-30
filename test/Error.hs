964c95b746bc1e800c88a65068d5d76df8f54c41
Rob Rix <rob.rix@me.com>
2019-09-30 09:42:41 -0400

    Rename the annihilation law’s continuation.

--- test/Error.hs
+++ test/Error.hs
@@ -9,7 +9,7 @@ import Control.Effect.Error
 import Test.Tasty.QuickCheck
 
 throwError_annihilation :: (Eq (m b), Has (Error e) sig m, Show (m b)) => e -> (a -> m b) -> Property
-throwError_annihilation e f = (throwError e >>= f) === throwError e
+throwError_annihilation e k = (throwError e >>= k) === throwError e
 
 catchError_substitution :: (Eq (m a), Has (Error e) sig m, Show (m a)) => e -> (e -> m a) -> Property
 catchError_substitution e f = (throwError e `catchError` f) === f e
