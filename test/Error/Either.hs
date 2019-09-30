b6b6d9323a72395622844e96366c3f2b668caab6
Rob Rix <rob.rix@me.com>
2019-09-30 09:07:53 -0400

    Define an Arbitrary instance for Error.Either.

--- test/Error/Either.hs
+++ test/Error/Either.hs
@@ -6,6 +6,10 @@ import Control.Carrier.Error.Either
 import Control.Monad.Trans.Except
 import Test.QuickCheck
 
+instance (Arbitrary e, Arbitrary1 m, Arbitrary a) => Arbitrary (ErrorC e m a) where
+  arbitrary = arbitrary1
+  shrink = shrink1
+
 instance (Arbitrary e, Arbitrary1 m) => Arbitrary1 (ErrorC e m) where
   liftArbitrary genA = ErrorC . ExceptT <$> liftArbitrary @m (liftArbitrary2 @Either (arbitrary @e) genA)
   liftShrink shrinkA = map (ErrorC . ExceptT) . liftShrink (liftShrink2 shrink shrinkA) . runError
