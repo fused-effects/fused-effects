fab7dd3d4a9e964410d01e2b97017fc3e6d295d3
Rob Rix <rob.rix@me.com>
2019-09-30 09:31:19 -0400

    Define a property for catchError’s substitution law.

--- test/Error.hs
+++ test/Error.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 module Error
 ( throwError_annihilation
+, catchError_substitution
 ) where
 
 import Control.Carrier
@@ -9,3 +10,6 @@ import Test.QuickCheck
 
 throwError_annihilation :: (Eq (m b), Has (Error e) sig m, Show (m b)) => e -> (a -> m b) -> Property
 throwError_annihilation e f = (throwError e >>= f) === throwError e
+
+catchError_substitution :: (Eq (m a), Has (Error e) sig m, Show (m a)) => e -> (e -> m a) -> Property
+catchError_substitution e f = (throwError e `catchError` f) === f e
