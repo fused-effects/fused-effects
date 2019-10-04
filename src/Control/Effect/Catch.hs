1c5c6a6e5f01fac8cfe23ef282ed77e75f9cc5b4
Rob Rix <rob.rix@me.com>
2019-10-04 17:14:52 -0400

    Define an HFunctor instance for Catch.

--- src/Control/Effect/Catch.hs
+++ src/Control/Effect/Catch.hs
@@ -3,7 +3,12 @@ module Control.Effect.Catch
 ( Catch(..)
 ) where
 
+import Control.Carrier
+
 data Catch e m k
   = forall b . Catch (m b) (e -> m b) (b -> m k)
 
 deriving instance Functor m => Functor (Catch e m)
+
+instance HFunctor (Catch e) where
+  hmap f (Catch m h k) = Catch (f m) (f . h) (f . k)
