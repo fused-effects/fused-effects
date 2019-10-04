3c0ac51be322d0c7de0c287089a30adc0761f15f
Rob Rix <rob.rix@me.com>
2019-10-04 17:15:57 -0400

    Define a smart constructor for Catch.

--- src/Control/Effect/Catch.hs
+++ src/Control/Effect/Catch.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE DeriveFunctor, ExistentialQuantification, StandaloneDeriving #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, FlexibleContexts, StandaloneDeriving #-}
 module Control.Effect.Catch
 ( Catch(..)
+, catchError
 ) where
 
 import Control.Carrier
@@ -15,3 +16,14 @@ instance HFunctor (Catch e) where
 
 instance Effect (Catch e) where
   handle state handler (Catch m h k) = Catch (handler (m <$ state)) (handler . (<$ state) . h) (handler . fmap k)
+
+
+-- | Run a computation which can throw errors with a handler to run on error.
+--
+-- Errors thrown by the handler will escape up to the nearest enclosing 'catchError' (if any).
+-- Note that this effect does /not/ handle errors thrown from impure contexts such as IO,
+-- nor will it handle exceptions thrown from pure code. If you need to handle IO-based errors,
+-- consider if 'Control.Effect.Resource' fits your use case; if not, use 'liftIO' with
+-- 'Control.Exception.try' or use 'Control.Exception.Catch' from outside the effect invocation.
+catchError :: Has (Catch e) sig m => m a -> (e -> m a) -> m a
+catchError m h = send (Catch m h pure)
