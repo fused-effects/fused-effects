c44b8cc3e0604b63ba49d79bf14cfd2e82456727
Rob Rix <rob.rix@me.com>
2019-10-04 17:14:29 -0400

    Derive a Functor instance for Catch.

--- src/Control/Effect/Catch.hs
+++ src/Control/Effect/Catch.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, StandaloneDeriving #-}
 module Control.Effect.Catch
 ( Catch(..)
 ) where
 
 data Catch e m k
   = forall b . Catch (m b) (e -> m b) (b -> m k)
+
+deriving instance Functor m => Functor (Catch e m)
