8912486092c9437a497a32dc56bda52ae34d0a90
Patrick Thomson <patrickt@github.com>
2019-10-06 14:47:17 -0400

    Document Control.Effect.Pure.

--- src/Control/Effect/Pure.hs
+++ src/Control/Effect/Pure.hs
@@ -1,4 +1,15 @@
 {-# LANGUAGE DeriveFunctor, DeriveGeneric, KindSignatures #-}
+-- | Provides a mechanism to kick off the evaluation of an effect stack
+-- in a pure computation.
+--
+-- This is generally the last effect in an effect stack, unless that stack
+-- needs to delegate to a base monad with the 'Control.Effect.Lift.Lift' effect.
+-- Such stacks are invoked with 'Control.Carrier.Pure.run' once all of their
+-- constituent effects have been discharged.
+--
+-- Predefined carriers:
+--
+-- * "Control.Carrier.Pure"
 module Control.Effect.Pure
 ( -- * Pure effect
   Pure
