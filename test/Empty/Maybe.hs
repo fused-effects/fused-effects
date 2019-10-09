8ae0eb5348ab3d8f6eebd1bc924fc3eee481121c
Rob Rix <rob.rix@me.com>
2019-10-09 09:59:13 -0400

    Test Empty.Maybe.EmptyC.

--- test/Empty/Maybe.hs
+++ test/Empty/Maybe.hs
@@ -1,2 +1,29 @@
+{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
 module Empty.Maybe
-() where
+( tests
+) where
+
+import Control.Carrier.Empty.Maybe
+import Control.Monad.Trans.Maybe
+import Pure
+import Test.QuickCheck.Poly
+import Test.Tasty
+import Test.Tasty.QuickCheck
+
+tests :: TestTree
+tests = testGroup "Empty.Maybe"
+  [ testProperty "empty annihilation" $
+    \ k -> empty_annihilation ((~=) @B) (applyFun @A k)
+  ]
+
+(~=) :: (Eq a, Show a) => EmptyC PureC a -> EmptyC PureC a -> Property
+m1 ~= m2 = run (runEmpty m1) === run (runEmpty m2)
+
+
+instance (Arbitrary1 m, Arbitrary a) => Arbitrary (EmptyC m a) where
+  arbitrary = arbitrary1
+  shrink = shrink1
+
+instance Arbitrary1 m => Arbitrary1 (EmptyC m) where
+  liftArbitrary genA = EmptyC . MaybeT <$> liftArbitrary @m (liftArbitrary @Maybe genA)
+  liftShrink shrinkA = map (EmptyC . MaybeT) . liftShrink (liftShrink shrinkA) . runEmpty
--- test/Test.hs
+++ test/Test.hs
@@ -2,6 +2,7 @@ module Main
 ( main
 ) where
 
+import qualified Empty.Maybe
 import qualified Error.Either
 import qualified Fusion
 import qualified NonDet.Church
@@ -9,7 +10,8 @@ import Test.Tasty
 
 main :: IO ()
 main = defaultMain $ testGroup "unit tests"
-  [ Error.Either.tests
+  [ Empty.Maybe.tests
+  , Error.Either.tests
   , Fusion.tests
   , NonDet.Church.tests
   ]
