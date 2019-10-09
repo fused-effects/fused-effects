913aad818d0fe23e0994daa6771c2d8eab57e840
Rob Rix <rob.rix@me.com>
2019-10-08 21:28:08 -0400

    Revert "Try an approach based on a type family to select instances."
    This reverts commit 6a5b5d470e12c661656b8d2f5208aa0f63df19e3.

--- src/Control/Effect/Sum.hs
+++ src/Control/Effect/Sum.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE AllowAmbiguousTypes, DataKinds, DeriveGeneric, DeriveTraversable, FlexibleContexts, FlexibleInstances, KindSignatures, MultiParamTypeClasses, ScopedTypeVariables, TypeApplications, TypeFamilies, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE DeriveGeneric, DeriveTraversable, FlexibleContexts, FlexibleInstances, KindSignatures, MultiParamTypeClasses, TypeOperators, UndecidableInstances #-}
 -- | Operations on /sums/, combining effects into a /signature/.
 module Control.Effect.Sum
 ( -- * Membership
@@ -31,46 +31,26 @@ class Member (sub :: (* -> *) -> (* -> *)) sup where
   -- | Inject a member of a signature into the signature.
   inj :: sub m a -> sup m a
 
-instance {-# OVERLAPPABLE #-} Member sub sub where
-  inj = id
-
-instance (Member' elem sub sup, Elem sub sup ~ elem) => Member sub sup where
-  inj = inj' @elem
-
-
-type family Elem (sub :: (* -> *) -> (* -> *)) sup :: Bool where
-  Elem t t         = 'True
-  Elem t (l :+: r) = Elem t l || Elem t r
-  Elem _ _         = 'False
-
-type family (a :: Bool) || (b :: Bool) where
-  'False || 'False = 'False
-  _      || _      = 'True
-
-
-class Member' (elem :: Bool) (sub :: (* -> *) -> (* -> *)) sup where
-  inj' :: sub m a -> sup m a
-
 -- | Reflexivity: @t@ is a member of itself.
-instance Member' 'True t t where
-  inj' = id
+instance Member t t where
+  inj = id
 
 -- | Left-recursion: if @t@ is a member of @l1 ':+:' l2 ':+:' r@, then we can inject it into @(l1 ':+:' l2) ':+:' r@ by injection into a right-recursive signature, followed by left-association.
 instance {-# OVERLAPPABLE #-}
-         Member' 'True t (l1 :+: l2 :+: r)
-      => Member' 'True t ((l1 :+: l2) :+: r) where
-  inj' = reassoc . inj' @'True where
+         Member t (l1 :+: l2 :+: r)
+      => Member t ((l1 :+: l2) :+: r) where
+  inj = reassoc . inj where
     reassoc (L l)     = L (L l)
     reassoc (R (L l)) = L (R l)
     reassoc (R (R r)) = R r
 
 -- | Left-occurrence: if @t@ is at the head of a signature, we can inject it in O(1).
 instance {-# OVERLAPPABLE #-}
-         Member' 'True l (l :+: r) where
-  inj' = L
+         Member l (l :+: r) where
+  inj = L
 
 -- | Right-recursion: if @t@ is a member of @r@, we can inject it into @r@ in O(n), followed by lifting that into @l ':+:' r@ in O(1).
 instance {-# OVERLAPPABLE #-}
-         Member' 'True l r
-      => Member' 'True l (l' :+: r) where
-  inj' = R . inj' @'True
+         Member l r
+      => Member l (l' :+: r) where
+  inj = R . inj
