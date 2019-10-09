27e9a5a8b023a818a916c13c9118a44d70a791cc
Rob Rix <rob.rix@me.com>
2019-10-08 23:30:08 -0400

    Define a MonadTrans instance.

--- src/Control/Carrier/Throw/Error.hs
+++ src/Control/Carrier/Throw/Error.hs
@@ -17,6 +17,10 @@ import Control.Monad (MonadPlus)
 import qualified Control.Monad.Fail as Fail
 import Control.Monad.Fix
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
 
 newtype ThrowC e m a = ThrowC { runThrow :: m a }
   deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO, MonadPlus)
+
+instance MonadTrans (ThrowC e) where
+  lift = ThrowC
