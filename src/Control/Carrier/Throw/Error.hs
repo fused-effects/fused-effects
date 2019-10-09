1ea716cc568aa6962a810d555938474a1c2ccdae
Rob Rix <rob.rix@me.com>
2019-10-08 23:30:03 -0400

    Derive a bunch of instances.

--- src/Control/Carrier/Throw/Error.hs
+++ src/Control/Carrier/Throw/Error.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Carrier.Throw.Error
 ( -- * Throw effect
   module Control.Effect.Throw
@@ -9,7 +10,13 @@ module Control.Carrier.Throw.Error
 , run
 ) where
 
+import Control.Applicative (Alternative)
 import Control.Carrier
 import Control.Effect.Throw
+import Control.Monad (MonadPlus)
+import qualified Control.Monad.Fail as Fail
+import Control.Monad.Fix
+import Control.Monad.IO.Class
 
 newtype ThrowC e m a = ThrowC { runThrow :: m a }
+  deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO, MonadPlus)
