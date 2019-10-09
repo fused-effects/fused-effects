d5a4c46dc173e58fdf2ed90805ddc005efa25132
Patrick Thomson <patrickt@github.com>
2019-10-09 18:29:15 -0400

    Update src/Control/Effect/NonDet.hs
    Co-Authored-By: Rob Rix <robrix@github.com>

--- src/Control/Effect/NonDet.hs
+++ src/Control/Effect/NonDet.hs
@@ -7,7 +7,7 @@ Nondeterministic operations are encapsulated by the 'Alternative' class, where '
 Predefined carriers:
 
 * "Control.Carrier.NonDet.Church", which collects all branches' results using an @Alternative@ functor.
-* "Control.Carrier.NonDet.Maybe", which terminates upon encountering the first successful result.
+* "Control.Carrier.NonDet.Maybe", which returns at most one result, in `Maybe`.
 * If 'NonDet' is the last effect in a stack, it can be interpreted directly into a @[]@.
 -}
 
