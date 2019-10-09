b64987821dc88b7de4899b32680ae01f413e00f5
Rob Rix <rob.rix@me.com>
2019-10-08 23:28:20 -0400

    Re-export Carrier & run.

--- src/Control/Carrier/Throw/Error.hs
+++ src/Control/Carrier/Throw/Error.hs
@@ -1,6 +1,10 @@
 module Control.Carrier.Throw.Error
 ( -- * Throw effect
   module Control.Effect.Throw
+  -- * Re-exports
+, Carrier
+, run
 ) where
 
+import Control.Carrier
 import Control.Effect.Throw
