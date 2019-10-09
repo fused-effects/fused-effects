536c2e590292548898f88e2d7951ef51255a0211
Rob Rix <rob.rix@me.com>
2019-10-08 23:17:52 -0400

    Define a Throw carrier.

--- src/Control/Carrier/Throw/Either.hs
+++ src/Control/Carrier/Throw/Either.hs
@@ -1,6 +1,9 @@
 module Control.Carrier.Throw.Either
 ( -- * Throw effect
   module Control.Effect.Throw
+  -- * Throw carrier
+, runThrow
+, ThrowC(ThrowC)
   -- * Re-exports
 , Carrier
 , run
@@ -8,3 +11,5 @@ module Control.Carrier.Throw.Either
 
 import Control.Carrier
 import Control.Effect.Throw
+
+newtype ThrowC e m a = ThrowC { runThrow :: m (Either e a) }
