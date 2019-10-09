cc4d28a4a654348ff194361d365066f5825926e6
Rob Rix <rob.rix@me.com>
2019-10-09 09:34:44 -0400

    Define an annihilation law for Empty.

--- test/Empty.hs
+++ test/Empty.hs
@@ -1,2 +1,10 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Empty
-() where
+( empty_annihilation
+) where
+
+import Control.Carrier
+import Control.Effect.Empty
+
+empty_annihilation :: Has Empty sig m => (m b -> m b -> prop) -> (a -> m b) -> prop
+empty_annihilation (===) k = (empty >>= k) === empty
