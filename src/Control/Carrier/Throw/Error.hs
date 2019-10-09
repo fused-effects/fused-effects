92cb3cba1823f81a9f7bce23b697b7a7c68be698
Rob Rix <rob.rix@me.com>
2019-10-08 23:33:08 -0400

    Revert "Define a MonadTrans instance."
    This reverts commit 27e9a5a8b023a818a916c13c9118a44d70a791cc.

--- src/Control/Carrier/Throw/Error.hs
+++ src/Control/Carrier/Throw/Error.hs
@@ -17,10 +17,6 @@ import Control.Monad (MonadPlus)
 import qualified Control.Monad.Fail as Fail
 import Control.Monad.Fix
 import Control.Monad.IO.Class
-import Control.Monad.Trans.Class
 
 newtype ThrowC e m a = ThrowC { runThrow :: m a }
   deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO, MonadPlus)
-
-instance MonadTrans (ThrowC e) where
-  lift = ThrowC
