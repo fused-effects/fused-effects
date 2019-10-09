d85be290042b008cc6ea572746bd1597b867b366
Rob Rix <rob.rix@me.com>
2019-10-08 23:33:17 -0400

    Revert "Re-export Carrier & run."
    This reverts commit b64987821dc88b7de4899b32680ae01f413e00f5.

--- src/Control/Carrier/Throw/Error.hs
+++ src/Control/Carrier/Throw/Error.hs
@@ -1,10 +1,6 @@
 module Control.Carrier.Throw.Error
 ( -- * Throw effect
   module Control.Effect.Throw
-  -- * Re-exports
-, Carrier
-, run
 ) where
 
-import Control.Carrier
 import Control.Effect.Throw
