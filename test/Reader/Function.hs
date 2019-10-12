b09ac11c15e83de796ac6554124af2a1fa0b916a
Rob Rix <rob.rix@me.com>
2019-10-11 20:07:53 -0400

    Define Arbitrary1 & Arbitrary instances for ReaderC.

--- test/Reader/Function.hs
+++ test/Reader/Function.hs
@@ -1,9 +1,22 @@
+{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 module Reader.Function
 ( tests
 ) where
 
+import Control.Carrier.Reader.Function
 import Test.Tasty
+import Test.Tasty.QuickCheck
 
 tests :: TestTree
 tests = testGroup "Reader.Function"
   []
+
+
+instance (Arbitrary1 m, CoArbitrary r) => Arbitrary1 (ReaderC r m) where
+  liftArbitrary genA = ReaderC <$> liftArbitrary @((->) r) (liftArbitrary @m genA)
+  liftShrink shrinkA = map ReaderC . liftShrink (liftShrink shrinkA) . runReaderC
+
+instance (Arbitrary1 m, Arbitrary a, CoArbitrary r) => Arbitrary (ReaderC r m a) where
+  arbitrary = arbitrary1
+  shrink = shrink1
