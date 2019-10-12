9f76a00efe18a51fad3080e4473fab118876c6da
Rob Rix <rob.rix@me.com>
2019-10-11 22:53:52 -0400

    Port the Reader.Function tests to hedgehog.

--- test/Reader/Function.hs
+++ test/Reader/Function.hs
@@ -1,36 +1,28 @@
-{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
-{-# OPTIONS_GHC -Wno-orphans #-}
+{-# LANGUAGE TypeApplications #-}
 module Reader.Function
 ( tests
 , gen
 ) where
 
+import Control.Carrier
 import Control.Carrier.Reader.Function
-import Hedgehog hiding (Property, (===))
+import Hedgehog
+import Hedgehog.Function
 import qualified Hedgehog.Gen as Gen
 import Pure hiding (gen)
 import Test.Tasty
-import Test.Tasty.QuickCheck hiding (Gen)
+import Test.Tasty.Hedgehog
 
 tests :: TestTree
 tests = testGroup "Reader.Function"
-  [ testProperty "ask environment" $
-    \ a k -> ask_environment @A ((~=) @B) runReader a (getBlind k)
-  , testProperty "local modification" $
-    \ a f m -> local_modification @A ((~=) @B) runReader a (applyFun f) (getBlind m)
+  [ testProperty "ask environment" . forall (genA :. fn @A (Blind <$> gen genA) :. Nil) $
+    \ a k -> ask_environment (~=) runReader a (getBlind . apply k)
+  , testProperty "local modification" . forall (genA :. fn @A genA :. fmap Blind (gen genA) :. Nil) $
+    \ a f m -> local_modification (~=) runReader a (apply f) (getBlind m)
   ]
 
-(~=) :: (Eq a, Show a) => PureC a -> PureC a -> Property
+(~=) :: (Eq a, Show a) => PureC a -> PureC a -> PropertyT IO ()
 m1 ~= m2 = run m1 === run m2
-
-
-instance (Arbitrary1 m, CoArbitrary r) => Arbitrary1 (ReaderC r m) where
-  liftArbitrary genA = ReaderC <$> liftArbitrary @((->) r) (liftArbitrary @m genA)
-  liftShrink shrinkA = map ReaderC . liftShrink (liftShrink shrinkA) . runReaderC
-
-instance (Arbitrary1 m, Arbitrary a, CoArbitrary r) => Arbitrary (ReaderC r m a) where
-  arbitrary = arbitrary1
-  shrink = shrink1
 
 
 gen :: Carrier sig m => Gen a -> Gen (ReaderC a m a)
