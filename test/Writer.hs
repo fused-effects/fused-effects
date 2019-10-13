0b21333aedeb71648e9cd9627fbc4519d96dcb1c
Rob Rix <rob.rix@me.com>
2019-10-13 15:04:18 -0400

    Parameterize genWriter by a recursive rule.

--- test/Writer.hs
+++ test/Writer.hs
@@ -9,10 +9,10 @@ import Hedgehog
 import Hedgehog.Gen
 
 gen :: Has (Writer a) sig m => Gen a -> Gen (m a)
-gen a = choice [ genWriter a, pure <$> a ]
+gen a = choice [ genWriter a (gen a), pure <$> a ]
 
-genWriter :: Has (Writer a) sig m => Gen a -> Gen (m a)
-genWriter a = choice
+genWriter :: Has (Writer a) sig m => Gen a -> Gen (m a) -> Gen (m a)
+genWriter a _ = choice
   [ tell' <$> a
   ] where
   tell' a = a <$ tell a
