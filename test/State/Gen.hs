1c862913a37dda74637f78ef597dc91dc3690de6
Rob Rix <rob.rix@me.com>
2019-10-13 16:29:59 -0400

    Define a generalized test group for State.

--- test/State/Gen.hs
+++ test/State/Gen.hs
@@ -1,12 +1,26 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts, RankNTypes #-}
 module State.Gen
 ( genState
+, testState
 ) where
 
 import Control.Effect.State
 import Hedgehog
+import Hedgehog.Function
 import Hedgehog.Gen
+import Pure
+import Test.Tasty
+import Test.Tasty.Hedgehog
 
 genState :: Has (State a) sig m => Gen a -> Gen (m a) -> Gen (m a)
 genState a _ = choice [ pure get, put' <$> a ] where
   put' a = a <$ put a
+
+
+testState :: (Has (State s) sig m, Arg s, Eq s, Show s, Vary s) => String -> (forall a . (s -> m a -> PureC (s, a))) -> Gen s -> TestTree
+testState name runState gen = testGroup name
+  [ testProperty "get state" . forall (gen :. fn (Blind <$> genM genState gen) :. Nil) $
+    \ a k -> get_state (~=) runState a (getBlind . apply k)
+  , testProperty "put update" . forall (gen :. gen :. fmap Blind (genM genState gen) :. Nil) $
+    \ a b m -> put_update (~=) runState a b (getBlind m)
+  ]
