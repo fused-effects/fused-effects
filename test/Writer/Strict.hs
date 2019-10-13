e8ddecf29c9b25855a98bdef3d34257e02012eb9
Rob Rix <rob.rix@me.com>
2019-10-13 16:06:40 -0400

    Use the generalized tests for the Writer carriers.

--- test/Writer/Strict.hs
+++ test/Writer/Strict.hs
@@ -4,21 +4,12 @@ module Writer.Strict
 ) where
 
 import Control.Carrier.Writer.Strict
-import Hedgehog.Function
 import Hedgehog.Gen
 import Hedgehog.Range
 import Pure
 import Test.Tasty
-import Test.Tasty.Hedgehog
 import Writer.Gen
 
 tests :: TestTree
-tests = testGroup "Writer.Strict.WriterC"
-  [ testProperty "tell append" . forall (genAs :. fmap Blind (genM genWriter genAs) :. Nil) $
-    \ w m -> tell_append (~=) runWriter w (getBlind m)
-  , testProperty "listen eavesdrop" . forall (fmap Blind (genM genWriter genAs) :. Nil) $
-    \ m -> listen_eavesdrop (~=) (runWriter @[A]) (getBlind m)
-  , testProperty "censor revision" . forall (fn genAs :. fmap Blind (genM genWriter genAs) :. Nil) $
-    \ f m -> censor_revision (~=) runWriter (apply f) (getBlind m)
-  ] where
-  genAs = list (linear 0 10) genA
+tests = testGroup "Writer.Strict.WriterC" (writerTests runWriter genW) where
+  genW = list (linear 0 10) genA
--- test/Writer/WriterT/Lazy.hs
+++ test/Writer/WriterT/Lazy.hs
@@ -3,25 +3,15 @@ module Writer.WriterT.Lazy
 ( tests
 ) where
 
-import Control.Effect.Writer
 import qualified Control.Monad.Trans.Writer.Lazy as WriterT
 import Data.Tuple
-import Hedgehog.Function
 import Hedgehog.Gen
 import Hedgehog.Range
 import Pure
 import Test.Tasty
-import Test.Tasty.Hedgehog
 import Writer.Gen
 
 tests :: TestTree
-tests = testGroup "Writer.WriterT.Lazy"
-  [ testProperty "tell append" . forall (genAs :. fmap Blind (genM genWriter genAs) :. Nil) $
-    \ w m -> tell_append (~=) runWriter w (getBlind m)
-  , testProperty "listen eavesdrop" . forall (fmap Blind (genM genWriter genAs) :. Nil) $
-    \ m -> listen_eavesdrop (~=) runWriter (getBlind m)
-  , testProperty "censor revision" . forall (fn genAs :. fmap Blind (genM genWriter genAs) :. Nil) $
-    \ f m -> censor_revision (~=) runWriter (apply f) (getBlind m)
-  ] where
-  genAs = list (linear 0 10) genA
-  runWriter = fmap swap . WriterT.runWriterT @[A]
+tests = testGroup "Writer.WriterT.Lazy" (writerTests runWriter genW) where
+  genW = list (linear 0 10) genA
+  runWriter = fmap swap . WriterT.runWriterT
