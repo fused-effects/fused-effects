b6566cd7ef5c9c5dde5e1e1b28ae1dfb3381b046
Rob Rix <rob.rix@me.com>
2019-10-13 12:01:39 -0400

    Define a generator for writer.

--- test/Writer/Strict.hs
+++ test/Writer/Strict.hs
@@ -1,9 +1,20 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Writer.Strict
 ( tests
+, gen
 ) where
 
+import Control.Carrier
+import Control.Carrier.Writer.Strict
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
 import Test.Tasty
 
 tests :: TestTree
 tests = testGroup "Writer.Strict.WriterC"
   []
+
+
+gen :: (Carrier sig m, Effect sig, Monoid a) => Gen a -> Gen (WriterC a m a)
+gen a = Gen.choice [ tell' <$> a, pure <$> a ] where
+  tell' a = a <$ tell a
