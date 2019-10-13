9e82ea5669aa83353a30937f3efb987ebd1a388f
Rob Rix <rob.rix@me.com>
2019-10-13 16:57:28 -0400

    Move genState into its own module.

--- test/State.hs
+++ test/State.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts, RankNTypes #-}
+{-# LANGUAGE RankNTypes #-}
 module State
 ( tests
 ) where
@@ -9,9 +9,9 @@ import Control.Effect.State
 import qualified Control.Monad.Trans.State.Lazy as LazyStateT
 import qualified Control.Monad.Trans.State.Strict as StrictStateT
 import Data.Tuple (swap)
+import Gen.State
 import Hedgehog
 import Hedgehog.Function
-import Hedgehog.Gen
 import Pure
 import Test.Tasty
 import Test.Tasty.Hedgehog
@@ -23,11 +23,6 @@ tests = testGroup "State"
   , testState "StateT (Lazy)"   (fmap (fmap swap) . flip LazyStateT.runStateT)   genA
   , testState "StateT (Strict)" (fmap (fmap swap) . flip StrictStateT.runStateT) genA
   ]
-
-
-genState :: Has (State a) sig m => Gen a -> Gen (m a) -> Gen (m a)
-genState a _ = choice [ pure get, put' <$> a ] where
-  put' a = a <$ put a
 
 
 testState :: (Has (State s) sig m, Arg s, Eq s, Show s, Vary s) => String -> (forall a . (s -> m a -> PureC (s, a))) -> Gen s -> TestTree
