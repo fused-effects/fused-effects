93fe70725b1f255af37eeeb02d368a2224108fee
Rob Rix <rob.rix@me.com>
2019-10-13 15:10:56 -0400

    Generate censor actions.

--- test/Writer.hs
+++ test/Writer.hs
@@ -6,14 +6,16 @@ module Writer
 
 import Control.Effect.Writer
 import Hedgehog
+import Hedgehog.Function
 import Hedgehog.Gen
 
-gen :: Has (Writer a) sig m => Gen a -> Gen (m a)
+gen :: (Has (Writer a) sig m, Arg a, Vary a) => Gen a -> Gen (m a)
 gen a = choice [ genWriter a (gen a), pure <$> a ]
 
-genWriter :: forall a m sig . Has (Writer a) sig m => Gen a -> Gen (m a) -> Gen (m a)
+genWriter :: forall a m sig . (Has (Writer a) sig m, Arg a, Vary a) => Gen a -> Gen (m a) -> Gen (m a)
 genWriter a ma = choice
   [ tell' <$> a
   , subtermM ma (\ m -> element [fst <$> listen @a m, snd <$> listen @a m])
+  , fn a >>= subterm ma . censor . apply
   ] where
   tell' a = a <$ tell a
