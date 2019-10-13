e1d8d54fa31c08a6810d62d20974e8fd620431b3
Rob Rix <rob.rix@me.com>
2019-10-13 14:06:06 -0400

    Rename genThrowError to genThrow.

--- test/Throw.hs
+++ test/Throw.hs
@@ -1,6 +1,6 @@
 module Throw
 ( gen
-, genThrowError
+, genThrow
 ) where
 
 import Control.Effect.Throw
@@ -8,7 +8,7 @@ import Hedgehog
 import Hedgehog.Gen as Gen
 
 gen :: Has (Throw e) sig m => Gen e -> Gen a -> Gen (m a)
-gen e a = Gen.choice [ genThrowError e, pure <$> a ]
+gen e a = Gen.choice [ genThrow e, pure <$> a ]
 
-genThrowError :: Has (Throw e) sig m => Gen e -> Gen (m a)
-genThrowError e = throwError <$> e
+genThrow :: Has (Throw e) sig m => Gen e -> Gen (m a)
+genThrow e = throwError <$> e
