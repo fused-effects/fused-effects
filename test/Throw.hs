0d647486afc41abb305ea8f952b556b38c547607
Rob Rix <rob.rix@me.com>
2019-10-13 13:42:45 -0400

    Define a generator for Throw.

--- test/Throw.hs
+++ test/Throw.hs
@@ -1,2 +1,10 @@
 module Throw
-() where
+( gen
+) where
+
+import Control.Effect.Throw
+import Hedgehog
+import Hedgehog.Gen as Gen
+
+gen :: Has (Throw e) sig m => Gen e -> Gen a -> Gen (m a)
+gen e a = Gen.choice [ throwError <$> e, pure <$> a ]
