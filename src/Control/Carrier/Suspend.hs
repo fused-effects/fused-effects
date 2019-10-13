02247892744d9099d21d6a63c9becc1c07413bb4
Rob Rix <rob.rix@me.com>
2019-10-13 19:40:03 -0400

    Define a MonadTrans instance for SuspendC.

--- src/Control/Carrier/Suspend.hs
+++ src/Control/Carrier/Suspend.hs
@@ -10,9 +10,13 @@ import Control.Monad (MonadPlus)
 import qualified Control.Monad.Fail as Fail
 import Control.Monad.Fix
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
 
 newtype SuspendC eff m a = SuspendC { runSuspendC :: ErrorC (SomeEffect (eff m)) m a }
   deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO, MonadPlus)
+
+instance MonadTrans (SuspendC eff) where
+  lift m = SuspendC (lift m)
 
 data SomeEffect eff
   = forall a . SomeError (eff a)
