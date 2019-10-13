a202b3c545f76f1b81965a8e07d4658cc8e0b5ea
Rob Rix <rob.rix@me.com>
2019-10-13 18:27:57 -0400

    :fire: Empty.gen.

--- test/Empty.hs
+++ test/Empty.hs
@@ -1,14 +1,9 @@
 module Empty
-( gen
-, genEmpty
+( genEmpty
 ) where
 
 import Control.Effect.Empty
 import Hedgehog
-import Hedgehog.Gen
-
-gen :: Has Empty sig m => Gen a -> Gen (m a)
-gen a = choice [ genEmpty a (gen a), pure <$> a ]
 
 genEmpty :: Has Empty sig m => Gen a -> Gen (m a) -> Gen (m a)
 genEmpty _ _ = pure empty
