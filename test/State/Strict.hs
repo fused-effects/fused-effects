0e9d1b30b8d4d37a05a4bd9dc0b280499b16ae67
Rob Rix <rob.rix@me.com>
2019-10-12 23:00:48 -0400

    Test the get state property.

--- test/State/Strict.hs
+++ test/State/Strict.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts, TypeApplications #-}
 module State.Strict
 ( tests
 , gen
@@ -7,12 +7,20 @@ module State.Strict
 import Control.Carrier
 import Control.Carrier.State.Strict
 import Hedgehog
+import Hedgehog.Function
 import qualified Hedgehog.Gen as Gen
+import Pure hiding (gen)
 import Test.Tasty
+import Test.Tasty.Hedgehog
 
 tests :: TestTree
 tests = testGroup "State.Strict.StateC"
-  []
+  [ testProperty "get state" . forall (genA :. fn @A (Blind <$> gen genA) :. Nil) $
+    \ a k -> get_state (~=) runState a (getBlind . apply k)
+  ]
+
+(~=) :: (Eq a, Show a) => PureC a -> PureC a -> PropertyT IO ()
+m1 ~= m2 = run m1 === run m2
 
 
 gen :: (Carrier sig m, Effect sig) => Gen a -> Gen (StateC a m a)
