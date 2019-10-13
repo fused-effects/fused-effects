4828f31ffecf81f1330a84dada9ec91aac18d77b
Rob Rix <rob.rix@me.com>
2019-10-13 16:48:42 -0400

    Rephrase gen as genReader.

--- test/Reader.hs
+++ test/Reader.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE RankNTypes #-}
 module Reader
-( gen
+( genReader
 , testReader
 ) where
 
@@ -12,17 +12,16 @@ import Hedgehog.Gen
 import Test.Tasty
 import Test.Tasty.Hedgehog
 
-gen :: (Has (Reader a) sig m, Arg a, Vary a) => Gen a -> Gen (m a)
-gen a = choice
+genReader :: (Has (Reader a) sig m, Arg a, Vary a) => Gen a -> Gen (m a) -> Gen (m a)
+genReader a ma = choice
   [ pure ask
-  , fn a >>= subterm (gen a) . local . apply
-  , pure <$> a
+  , fn a >>= subterm ma . local . apply
   ]
 
 testReader :: (Has (Reader r) sig m, Arg r, Eq r, Show r, Vary r) => String -> (forall a . r -> m a -> PureC a) -> Gen r -> TestTree
 testReader name runReader genA = testGroup name
-  [ testProperty "ask environment" . forall (genA :. fn (Blind <$> gen genA) :. Nil) $
+  [ testProperty "ask environment" . forall (genA :. fn (Blind <$> genM genReader genA) :. Nil) $
     \ a k -> ask_environment (~=) runReader a (getBlind . apply k)
-  , testProperty "local modification" . forall (genA :. fn genA :. fmap Blind (gen genA) :. Nil) $
+  , testProperty "local modification" . forall (genA :. fn genA :. fmap Blind (genM genReader genA) :. Nil) $
     \ a f m -> local_modification (~=) runReader a (apply f) (getBlind m)
   ]
