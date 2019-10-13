6e04a866f2f9fc66924eb1ed9b4917577b97f625
Rob Rix <rob.rix@me.com>
2019-10-13 19:38:59 -0400

    Derive a bunch of instances.

--- src/Control/Carrier/Suspend.hs
+++ src/Control/Carrier/Suspend.hs
@@ -1,12 +1,18 @@
-{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE ExistentialQuantification, GeneralizedNewtypeDeriving #-}
 module Control.Carrier.Suspend
 ( SuspendC(..)
 , SomeEffect(..)
 ) where
 
+import Control.Applicative (Alternative)
 import Control.Carrier.Error.Either
+import Control.Monad (MonadPlus)
+import qualified Control.Monad.Fail as Fail
+import Control.Monad.Fix
+import Control.Monad.IO.Class
 
 newtype SuspendC eff m a = SuspendC { runSuspendC :: ErrorC (SomeEffect (eff m)) m a }
+  deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO, MonadPlus)
 
 data SomeEffect eff
   = forall a . SomeError (eff a)
