f3030cc53f5077c3abba9000dd90460c96b5c9b0
Rob Rix <rob.rix@me.com>
2019-10-13 14:59:21 -0400

    Wrap the tell generator in a choice.

--- test/Writer.hs
+++ test/Writer.hs
@@ -12,5 +12,7 @@ gen :: Has (Writer a) sig m => Gen a -> Gen (m a)
 gen a = choice [ genWriter a, pure <$> a ]
 
 genWriter :: Has (Writer a) sig m => Gen a -> Gen (m a)
-genWriter a = tell' <$> a where
+genWriter a = choice
+  [ tell' <$> a
+  ] where
   tell' a = a <$ tell a
