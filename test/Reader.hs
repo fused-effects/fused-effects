85054e87be013dfaf109527ff0e5ea27702b5799
Rob Rix <rob.rix@me.com>
2019-10-13 13:40:17 -0400

    Produce local actions using subterm.

--- test/Reader.hs
+++ test/Reader.hs
@@ -8,4 +8,4 @@ import Hedgehog.Function
 import Hedgehog.Gen as Gen
 
 gen :: (Has (Reader a) sig m, Arg a, Vary a) => Gen a -> Gen (m a)
-gen a = Gen.choice [ pure ask, local . apply <$> fn a <*> gen a, pure <$> a ]
+gen a = Gen.choice [ pure ask, fn a >>= \ f -> subterm (gen a) (local (apply f)), pure <$> a ]
