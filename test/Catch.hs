c93fdef6a982fc7feff467fec3dd652fbd40e968
Rob Rix <rob.rix@me.com>
2019-10-13 17:53:28 -0400

    Specialize genCatch to Gen e.

--- test/Catch.hs
+++ test/Catch.hs
@@ -8,5 +8,5 @@ import Hedgehog
 import Hedgehog.Function
 import Hedgehog.Gen
 
-genCatch :: forall e m a proxy sig . (Has (Catch e) sig m, Arg e, Vary e) => proxy e -> Gen (m a) -> Gen (m a)
+genCatch :: forall e m a sig . (Has (Catch e) sig m, Arg e, Vary e) => Gen e -> Gen (m a) -> Gen (m a)
 genCatch _ ma = choice [ fn @e ma >>= subterm ma . flip catchError . apply ]
