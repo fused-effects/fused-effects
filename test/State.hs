4714b9b8165cff5c0b61fff6c98f29d301f608af
Rob Rix <rob.rix@me.com>
2019-10-13 16:39:40 -0400

    Combine all the state tests.

--- test/State.hs
+++ test/State.hs
@@ -1,17 +1,39 @@
+{-# LANGUAGE FlexibleContexts, RankNTypes #-}
 module State
 ( tests
 ) where
 
-import qualified State.Lazy
-import qualified State.StateT.Lazy
-import qualified State.StateT.Strict
-import qualified State.Strict
+import qualified Control.Carrier.State.Lazy as LazyStateC
+import qualified Control.Carrier.State.Strict as StrictStateC
+import Control.Effect.State
+import qualified Control.Monad.Trans.State.Lazy as LazyStateT
+import qualified Control.Monad.Trans.State.Strict as StrictStateT
+import Data.Tuple (swap)
+import Hedgehog
+import Hedgehog.Function
+import Hedgehog.Gen
+import Pure
 import Test.Tasty
+import Test.Tasty.Hedgehog
 
 tests :: TestTree
 tests = testGroup "State"
-  [ State.Lazy.tests
-  , State.StateT.Lazy.tests
-  , State.StateT.Strict.tests
-  , State.Strict.tests
+  [ testState "StateC (Lazy)"   LazyStateC.runState   genA
+  , testState "StateC (Strict)" StrictStateC.runState genA
+  , testState "StateT (Lazy)"   (fmap (fmap swap) . flip LazyStateT.runStateT)   genA
+  , testState "StateT (Strict)" (fmap (fmap swap) . flip StrictStateT.runStateT) genA
+  ]
+
+
+genState :: Has (State a) sig m => Gen a -> Gen (m a) -> Gen (m a)
+genState a _ = choice [ pure get, put' <$> a ] where
+  put' a = a <$ put a
+
+
+testState :: (Has (State s) sig m, Arg s, Eq s, Show s, Vary s) => String -> (forall a . (s -> m a -> PureC (s, a))) -> Gen s -> TestTree
+testState name runState gen = testGroup name
+  [ testProperty "get state" . forall (gen :. fn (Blind <$> genM genState gen) :. Nil) $
+    \ a k -> get_state (~=) runState a (getBlind . apply k)
+  , testProperty "put update" . forall (gen :. gen :. fmap Blind (genM genState gen) :. Nil) $
+    \ a b m -> put_update (~=) runState a b (getBlind m)
   ]
