0a68eb9267263234f41b55c2e4510e58e9d89705
Rob Rix <rob.rix@me.com>
2019-10-13 15:29:27 -0400

    Test the listen property for Strict.WriterC.

--- test/Writer/Strict.hs
+++ test/Writer/Strict.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeApplications #-}
 module Writer.Strict
 ( tests
 ) where
@@ -14,5 +15,7 @@ tests :: TestTree
 tests = testGroup "Writer.Strict.WriterC"
   [ testProperty "tell append" . forall (genAs :. fmap Blind (Writer.gen genAs) :. Nil) $
     \ w m -> tell_append (~=) runWriter w (getBlind m)
+  , testProperty "listen eavesdrop" . forall (fmap Blind (Writer.gen genAs) :. Nil) $
+    \ m -> listen_eavesdrop (~=) (runWriter @[A]) (getBlind m)
   ] where
   genAs = list (linear 0 10) genA
