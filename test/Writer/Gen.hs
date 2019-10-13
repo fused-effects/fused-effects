78e6cdff3cb3ee269adfecc789a7faf24eb4c42e
Rob Rix <rob.rix@me.com>
2019-10-13 16:05:07 -0400

    Define a set of generalized writer tests.

--- test/Writer/Gen.hs
+++ test/Writer/Gen.hs
@@ -1,12 +1,16 @@
-{-# LANGUAGE FlexibleContexts, ScopedTypeVariables, TypeApplications #-}
+{-# LANGUAGE FlexibleContexts, RankNTypes, ScopedTypeVariables, TypeApplications #-}
 module Writer.Gen
 ( genWriter
+, writerTests
 ) where
 
 import Control.Effect.Writer
 import Hedgehog
 import Hedgehog.Function
 import Hedgehog.Gen
+import Pure
+import Test.Tasty
+import Test.Tasty.Hedgehog
 
 genWriter :: forall a m sig . (Has (Writer a) sig m, Arg a, Vary a) => Gen a -> Gen (m a) -> Gen (m a)
 genWriter a ma = choice
@@ -15,3 +19,13 @@ genWriter a ma = choice
   , fn a >>= subterm ma . censor . apply
   ] where
   tell' a = a <$ tell a
+
+writerTests :: (Has (Writer w) sig m, Arg w, Eq w, Monoid w, Show w, Vary w) => (forall a . (m a -> PureC (w, a))) -> Gen w -> [TestTree]
+writerTests runWriter genW =
+  [ testProperty "tell append" . forall (genW :. fmap Blind (genM genWriter genW) :. Nil) $
+    \ w m -> tell_append (~=) runWriter w (getBlind m)
+  , testProperty "listen eavesdrop" . forall (fmap Blind (genM genWriter genW) :. Nil) $
+    \ m -> listen_eavesdrop (~=) runWriter (getBlind m)
+  , testProperty "censor revision" . forall (fn genW :. fmap Blind (genM genWriter genW) :. Nil) $
+    \ f m -> censor_revision (~=) runWriter (apply f) (getBlind m)
+  ]
