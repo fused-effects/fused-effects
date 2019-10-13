4cd5944cd38026c5a08d6b4e0481724fdb068bef
Rob Rix <rob.rix@me.com>
2019-10-13 19:56:55 -0400

    Define a Carrier instance for SuspendC.

--- src/Control/Carrier/Suspend.hs
+++ src/Control/Carrier/Suspend.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ExistentialQuantification, GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ExistentialQuantification, FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, TypeOperators, UndecidableInstances #-}
 module Control.Carrier.Suspend
 ( runSuspend
 , SuspendC(..)
@@ -6,17 +6,19 @@ module Control.Carrier.Suspend
 ) where
 
 import Control.Applicative (Alternative)
+import Control.Carrier
 import Control.Carrier.Error.Either
 import Control.Monad (MonadPlus)
 import qualified Control.Monad.Fail as Fail
 import Control.Monad.Fix
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
+import Control.Monad.Trans.Except (throwE)
 
-runSuspend :: SuspendC eff m a -> m (Either (SomeEffect (eff m)) a)
+runSuspend :: SuspendC eff m a -> m (Either (SomeEffect (eff (SuspendC eff m))) a)
 runSuspend = runError . runSuspendC
 
-newtype SuspendC eff m a = SuspendC { runSuspendC :: ErrorC (SomeEffect (eff m)) m a }
+newtype SuspendC eff m a = SuspendC { runSuspendC :: ErrorC (SomeEffect (eff (SuspendC eff m))) m a }
   deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO, MonadPlus)
 
 instance MonadTrans (SuspendC eff) where
@@ -24,3 +26,7 @@ instance MonadTrans (SuspendC eff) where
 
 data SomeEffect eff
   = forall a . SomeEffect (eff a)
+
+instance (Carrier sig m, Effect sig, HFunctor eff) => Carrier (eff :+: sig) (SuspendC eff m) where
+  eff (L op) = SuspendC (ErrorC (throwE (SomeEffect op)))
+  eff (R op) = SuspendC (eff (R (handleCoercible op)))
