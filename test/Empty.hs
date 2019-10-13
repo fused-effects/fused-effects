294b8dd85709bffd6910fa04d8e943061e21b07a
Rob Rix <rob.rix@me.com>
2019-10-13 18:26:34 -0400

    Give genEmpty parameters for pure values and actions.

--- test/Empty.hs
+++ test/Empty.hs
@@ -8,7 +8,7 @@ import Hedgehog
 import Hedgehog.Gen
 
 gen :: Has Empty sig m => Gen a -> Gen (m a)
-gen a = choice [ genEmpty, pure <$> a ]
+gen a = choice [ genEmpty a (gen a), pure <$> a ]
 
-genEmpty :: Has Empty sig m => Gen (m a)
-genEmpty = pure empty
+genEmpty :: Has Empty sig m => Gen a -> Gen (m a) -> Gen (m a)
+genEmpty _ _ = pure empty
