afa28a024750dae0bb9a779f377cd5d9b5dfbe7c
Rob Rix <rob.rix@me.com>
2019-10-13 18:42:53 -0400

    Factor the test group back out.

--- test/Throw.hs
+++ test/Throw.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE RankNTypes, ScopedTypeVariables, TypeApplications #-}
 module Throw
 ( genThrow
-, testThrow
+, throwTests
 ) where
 
 import Control.Effect.Throw
@@ -15,8 +15,8 @@ genThrow :: Has (Throw e) sig m => Gen e -> Gen a -> Gen (m a) -> Gen (m a)
 genThrow e _ _ = throwError <$> e
 
 
-testThrow :: forall e m a b sig . (Has (Throw e) sig m, Arg a, Eq b, Eq e, Show a, Show b, Show e, Vary a) => String -> (forall a . m a -> PureC (Either e a)) -> Gen e -> Gen a -> Gen b -> TestTree
-testThrow name runThrow e _ b = testGroup name
+throwTests :: forall e m a b sig . (Has (Throw e) sig m, Arg a, Eq b, Eq e, Show a, Show b, Show e, Vary a) => (forall a . m a -> PureC (Either e a)) -> Gen e -> Gen a -> Gen b -> [TestTree]
+throwTests runThrow e _ b =
   [ testProperty "throwError annihilation" . forall (e :. fn @a (Blind <$> genM [genThrow e] b) :. Nil) $
     \ e k -> throwError_annihilation (~=) runThrow e (getBlind . apply k)
   ]
