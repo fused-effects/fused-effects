a93b12f1c5faa134309f0252b353c2de0e346c55
Rob Rix <rob.rix@me.com>
2019-10-13 16:24:49 -0400

    :fire: State.Gen.gen.

--- test/State/Gen.hs
+++ test/State/Gen.hs
@@ -1,15 +1,11 @@
 {-# LANGUAGE FlexibleContexts #-}
 module State.Gen
-( gen
-, genState
+( genState
 ) where
 
 import Control.Effect.State
 import Hedgehog
 import Hedgehog.Gen
-
-gen :: Has (State a) sig m => Gen a -> Gen (m a)
-gen a = choice [ genState a (gen a) , pure <$> a ]
 
 genState :: Has (State a) sig m => Gen a -> Gen (m a) -> Gen (m a)
 genState a _ = choice [ pure get, put' <$> a ] where
