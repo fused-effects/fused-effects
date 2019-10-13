e9ea3f4ceeb3158183ceeb554bdd2cea376d0ce5
Rob Rix <rob.rix@me.com>
2019-10-13 14:32:11 -0400

    Use the generalized generator for the Empty tests.

--- test/Empty/Base.hs
+++ test/Empty/Base.hs
@@ -1,23 +1,18 @@
 {-# LANGUAGE TypeApplications #-}
 module Empty.Base
 ( tests
-, gen
 ) where
 
 import Control.Effect.Empty
+import qualified Empty
 import Hedgehog
 import Hedgehog.Function
-import qualified Hedgehog.Gen as Gen
-import Pure hiding (gen)
+import Pure
 import Test.Tasty
 import Test.Tasty.Hedgehog
 
 tests :: TestTree
 tests = testGroup "Empty.Maybe"
-  [ testProperty "empty annihilation" . forall (fn @A (gen genB) :. Nil) $
-    \ k -> empty_annihilation (===) id (apply k)
+  [ testProperty "empty annihilation" . forall (fn @A (Empty.gen genB) :. Nil) $
+    \ k -> empty_annihilation (===) (id @(Maybe _)) (apply k)
   ]
-
-
-gen :: Gen a -> Gen (Maybe a)
-gen a = Gen.choice [ pure empty, pure <$> a ]
--- test/Empty/Maybe.hs
+++ test/Empty/Maybe.hs
@@ -1,27 +1,22 @@
 {-# LANGUAGE TypeApplications #-}
 module Empty.Maybe
 ( tests
-, gen
 ) where
 
 import Control.Carrier
 import Control.Carrier.Empty.Maybe
+import qualified Empty
 import Hedgehog
 import Hedgehog.Function
-import qualified Hedgehog.Gen as Gen
-import Pure hiding (gen)
+import Pure
 import Test.Tasty
 import Test.Tasty.Hedgehog
 
 tests :: TestTree
 tests = testGroup "Empty.Maybe.EmptyC"
-  [ testProperty "empty annihilation" . forall (fn @A (gen genB) :. Nil) $
+  [ testProperty "empty annihilation" . forall (fn @A (Empty.gen genB) :. Nil) $
     \ k -> empty_annihilation (~=) runEmpty (apply k)
   ]
 
 (~=) :: (Eq a, Show a) => PureC a -> PureC a -> PropertyT IO ()
 m1 ~= m2 = run m1 === run m2
-
-
-gen :: (Carrier sig m, Effect sig) => Gen a -> Gen (EmptyC m a)
-gen a = Gen.choice [ pure empty, pure <$> a ]
