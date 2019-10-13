8100d452af747a3a1199ea5c4beb6f5ca0d2ccc3
Rob Rix <rob.rix@me.com>
2019-10-13 17:55:58 -0400

    :fire: Throw.gen.

--- test/Throw.hs
+++ test/Throw.hs
@@ -1,14 +1,9 @@
 module Throw
-( gen
-, genThrow
+( genThrow
 ) where
 
 import Control.Effect.Throw
 import Hedgehog
-import Hedgehog.Gen
-
-gen :: Has (Throw e) sig m => Gen e -> Gen a -> Gen (m a)
-gen e a = choice [ genThrow e, pure <$> a ]
 
 genThrow :: Has (Throw e) sig m => Gen e -> Gen (m a)
 genThrow e = throwError <$> e
