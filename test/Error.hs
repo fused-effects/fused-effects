5ebaf907f25df125d6e5c5554af5f9346dbef29e
Rob Rix <rob.rix@me.com>
2019-10-13 14:10:51 -0400

    Define a generalized generator for Error.

--- test/Error.hs
+++ test/Error.hs
@@ -1,2 +1,17 @@
 module Error
-() where
+( gen
+) where
+
+import Control.Effect.Error
+import qualified Catch
+import Hedgehog
+import Hedgehog.Function
+import Hedgehog.Gen
+import qualified Throw
+
+gen :: (Has (Error e) sig m, Arg e, Vary e) => Gen e -> Gen a -> Gen (m a)
+gen e a = choice
+  [ Throw.genThrow e
+  , Catch.genCatch e (gen e a)
+  , pure <$> a
+  ]
