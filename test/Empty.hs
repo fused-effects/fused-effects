e5d69ec25c4ddc7dbd32a4df74be9ef46778e4a6
Rob Rix <rob.rix@me.com>
2019-10-13 18:32:40 -0400

    Define a generalized test tree for Empty.

--- test/Empty.hs
+++ test/Empty.hs
@@ -1,9 +1,22 @@
+{-# LANGUAGE RankNTypes, ScopedTypeVariables, TypeApplications #-}
 module Empty
 ( genEmpty
+, testEmpty
 ) where
 
 import Control.Effect.Empty
 import Hedgehog
+import Hedgehog.Function
+import Pure
+import Test.Tasty
+import Test.Tasty.Hedgehog
 
 genEmpty :: Has Empty sig m => Gen a -> Gen (m a) -> Gen (m a)
 genEmpty _ _ = pure empty
+
+
+testEmpty :: forall a b m sig . (Has Empty sig m, Arg a, Eq b, Show a, Show b, Vary a) => String -> (forall a . m a -> PureC (Maybe a)) -> Gen a -> Gen b -> TestTree
+testEmpty name runEmpty _ genB = testGroup name
+  [ testProperty "empty annihilation" . forall (fn @a (Blind <$> genM [genEmpty] genB) :. Nil) $
+    \ k -> empty_annihilation (~=) runEmpty (getBlind . apply k)
+  ]
