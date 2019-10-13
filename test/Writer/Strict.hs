923be5fd02da80a1e9171a777157924a8094a086
Rob Rix <rob.rix@me.com>
2019-10-13 15:32:01 -0400

    Test the censor property for Strict.WriterC.

--- test/Writer/Strict.hs
+++ test/Writer/Strict.hs
@@ -4,6 +4,7 @@ module Writer.Strict
 ) where
 
 import Control.Carrier.Writer.Strict
+import Hedgehog.Function
 import Hedgehog.Gen
 import Hedgehog.Range
 import Pure
@@ -17,5 +18,7 @@ tests = testGroup "Writer.Strict.WriterC"
     \ w m -> tell_append (~=) runWriter w (getBlind m)
   , testProperty "listen eavesdrop" . forall (fmap Blind (Writer.gen genAs) :. Nil) $
     \ m -> listen_eavesdrop (~=) (runWriter @[A]) (getBlind m)
+  , testProperty "censor revision" . forall (fn genAs :. fmap Blind (Writer.gen genAs) :. Nil) $
+    \ f m -> censor_revision (~=) runWriter (apply f) (getBlind m)
   ] where
   genAs = list (linear 0 10) genA
