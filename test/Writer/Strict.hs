0af19f235a6ed33c39c190dd59c7249f674f230a
Rob Rix <rob.rix@me.com>
2019-10-13 15:22:47 -0400

    Test the tell law against Strict.WriterC

--- test/Writer/Strict.hs
+++ test/Writer/Strict.hs
@@ -2,8 +2,17 @@ module Writer.Strict
 ( tests
 ) where
 
+import Control.Carrier.Writer.Strict
+import Hedgehog.Gen
+import Hedgehog.Range
+import Pure
 import Test.Tasty
+import Test.Tasty.Hedgehog
+import qualified Writer
 
 tests :: TestTree
 tests = testGroup "Writer.Strict.WriterC"
-  []
+  [ testProperty "tell append" . forall (genAs :. fmap Blind (Writer.gen genAs) :. Nil) $
+    \ w m -> tell_append (~=) runWriter w (getBlind m)
+  ] where
+  genAs = list (linear 0 10) genA
