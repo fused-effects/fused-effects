5b892b1d794c7d8b01769d5fc12c3e0ada781a78
Rob Rix <rob.rix@me.com>
2019-10-12 22:51:16 -0400

    Define a generator for StateC.

--- test/State/Strict.hs
+++ test/State/Strict.hs
@@ -1,9 +1,20 @@
+{-# LANGUAGE FlexibleContexts #-}
 module State.Strict
 ( tests
+, gen
 ) where
 
+import Control.Carrier
+import Control.Carrier.State.Strict
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
 import Test.Tasty
 
 tests :: TestTree
-tests = testGroup "State.Strict"
+tests = testGroup "State.Strict.StateC"
   []
+
+
+gen :: (Carrier sig m, Effect sig) => Gen a -> Gen (StateC a m a)
+gen a = Gen.choice [ pure get, put' <$> a, pure <$> a ] where
+  put' a = a <$ put a
