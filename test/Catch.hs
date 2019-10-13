a81403fc3e430e6041076337e41e4452b480d6e3
Rob Rix <rob.rix@me.com>
2019-10-13 14:03:01 -0400

    Define a generator for catchError.

--- test/Catch.hs
+++ test/Catch.hs
@@ -1,2 +1,13 @@
+{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
 module Catch
-() where
+( genCatchError
+) where
+
+import Control.Effect.Catch
+import Hedgehog
+import Hedgehog.Function
+import Hedgehog.Gen
+
+genCatchError :: forall e m a proxy sig . (Has (Catch e) sig m, Arg e, Vary e) => proxy e -> Gen (m a)
+genCatchError _ = go where
+  go = fn @e go >>= subterm go . flip catchError . apply
