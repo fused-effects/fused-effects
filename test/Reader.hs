270e200a208a66a4c8b698920eaf4d660d984dea
Rob Rix <rob.rix@me.com>
2019-10-13 13:47:40 -0400

    Reformat the Reader generator.

--- test/Reader.hs
+++ test/Reader.hs
@@ -8,4 +8,8 @@ import Hedgehog.Function
 import Hedgehog.Gen as Gen
 
 gen :: (Has (Reader a) sig m, Arg a, Vary a) => Gen a -> Gen (m a)
-gen a = Gen.choice [ pure ask, fn a >>= \ f -> subterm (gen a) (local (apply f)), pure <$> a ]
+gen a = Gen.choice
+  [ pure ask
+  , fn a >>= \ f -> subterm (gen a) (local (apply f))
+  , pure <$> a
+  ]
