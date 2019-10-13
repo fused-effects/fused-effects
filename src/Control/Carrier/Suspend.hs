ea1b137d464b018767f47dd3dc29a49c01e44ccf
Rob Rix <rob.rix@me.com>
2019-10-13 19:37:46 -0400

    Stub in a suspend carrier.

--- src/Control/Carrier/Suspend.hs
+++ src/Control/Carrier/Suspend.hs
@@ -1,2 +1,12 @@
+{-# LANGUAGE ExistentialQuantification #-}
 module Control.Carrier.Suspend
-() where
+( SuspendC(..)
+, SomeEffect(..)
+) where
+
+import Control.Carrier.Error.Either
+
+newtype SuspendC eff m a = SuspendC { runSuspendC :: ErrorC (SomeEffect (eff m)) m a }
+
+data SomeEffect eff
+  = forall a . SomeError (eff a)
