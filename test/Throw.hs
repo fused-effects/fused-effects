52cfc80b09a721a9694ecb93e3f092292da549d0
Rob Rix <rob.rix@me.com>
2019-10-13 18:25:34 -0400

    Define a generalized test tree for Throw.

--- test/Throw.hs
+++ test/Throw.hs
@@ -1,9 +1,22 @@
+{-# LANGUAGE RankNTypes, ScopedTypeVariables, TypeApplications #-}
 module Throw
 ( genThrow
+, testThrow
 ) where
 
 import Control.Effect.Throw
 import Hedgehog
+import Hedgehog.Function
+import Pure
+import Test.Tasty
+import Test.Tasty.Hedgehog
 
 genThrow :: Has (Throw e) sig m => Gen e -> Gen a -> Gen (m a) -> Gen (m a)
 genThrow e _ _ = throwError <$> e
+
+
+testThrow :: forall e m a b sig . (Has (Throw e) sig m, Arg a, Eq b, Eq e, Show a, Show b, Show e, Vary a) => String -> (forall a . m a -> PureC (Either e a)) -> Gen e -> Gen a -> Gen b -> TestTree
+testThrow name runThrow e _ b = testGroup name
+  [ testProperty "throwError annihilation" . forall (e :. fn @a (Blind <$> genM [genThrow e] b) :. Nil) $
+    \ e k -> throwError_annihilation (~=) runThrow e (getBlind . apply k)
+  ]
