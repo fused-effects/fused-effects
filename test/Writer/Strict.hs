22a7582d00bf0cce8593eb6fc9492ff657e0d0f2
Rob Rix <rob.rix@me.com>
2019-10-13 15:48:53 -0400

    Don’t qualify the imports of Writer.

--- test/Writer/Strict.hs
+++ test/Writer/Strict.hs
@@ -10,15 +10,15 @@ import Hedgehog.Range
 import Pure
 import Test.Tasty
 import Test.Tasty.Hedgehog
-import qualified Writer
+import Writer
 
 tests :: TestTree
 tests = testGroup "Writer.Strict.WriterC"
-  [ testProperty "tell append" . forall (genAs :. fmap Blind (Writer.gen genAs) :. Nil) $
+  [ testProperty "tell append" . forall (genAs :. fmap Blind (genM genWriter genAs) :. Nil) $
     \ w m -> tell_append (~=) runWriter w (getBlind m)
-  , testProperty "listen eavesdrop" . forall (fmap Blind (Writer.gen genAs) :. Nil) $
+  , testProperty "listen eavesdrop" . forall (fmap Blind (genM genWriter genAs) :. Nil) $
     \ m -> listen_eavesdrop (~=) (runWriter @[A]) (getBlind m)
-  , testProperty "censor revision" . forall (fn genAs :. fmap Blind (Writer.gen genAs) :. Nil) $
+  , testProperty "censor revision" . forall (fn genAs :. fmap Blind (genM genWriter genAs) :. Nil) $
     \ f m -> censor_revision (~=) runWriter (apply f) (getBlind m)
   ] where
   genAs = list (linear 0 10) genA
