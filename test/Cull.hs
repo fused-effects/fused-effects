8076121e9529da4b70d5a969a59c1f423d5b2caa
Rob Rix <rob.rix@me.com>
2019-10-13 23:17:45 -0400

    Stub in tests for Cull.

--- test/Cull.hs
+++ test/Cull.hs
@@ -2,13 +2,16 @@
 module Cull
 ( tests
 , gen
+, cullTests
 ) where
 
 import Control.Effect.Cull
 import Control.Effect.NonDet (NonDet)
 import Hedgehog
+import Hedgehog.Function
 import Hedgehog.Gen
 import qualified NonDet
+import Pure
 import Test.Tasty
 
 tests :: TestTree
@@ -21,3 +24,7 @@ gen m a = choice
   [ subterm (m a) cull
   , NonDet.gen m a
   ]
+
+
+cullTests :: forall a b m sig . (Has NonDet sig m, Arg a, Eq a, Eq b, Show a, Show b, Vary a) => (forall a . m a -> PureC [a]) -> (forall a. Gen a -> Gen (Blind (m a))) -> Gen a -> Gen b -> [TestTree]
+cullTests runCull m a b = NonDet.nonDetTests runCull m a b
