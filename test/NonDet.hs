ead2cfd8280022453277794a624521bedb8fd325
Rob Rix <rob.rix@me.com>
2019-10-13 22:05:52 -0400

    Define a generator for NonDet actions.

--- test/NonDet.hs
+++ test/NonDet.hs
@@ -1,9 +1,20 @@
 module NonDet
 ( tests
+, genNonDet
 ) where
 
+import qualified Choose
+import Control.Carrier
+import Control.Effect.NonDet (NonDet)
+import qualified Empty
+import Hedgehog
+import Hedgehog.Gen
 import Test.Tasty
 
 tests :: TestTree
 tests = testGroup "NonDet"
   []
+
+
+genNonDet :: Has NonDet sig m => Gen a -> Gen (m a) -> Gen (m a)
+genNonDet a m = choice [ Empty.genEmpty a m, Choose.genChoose a m ]
