eb4eb08131784b5887422affd262b478a8c71275
Rob Rix <rob.rix@me.com>
2019-10-13 23:16:43 -0400

    Define a generator for cull actions.

--- test/Cull.hs
+++ test/Cull.hs
@@ -1,9 +1,23 @@
+{-# LANGUAGE RankNTypes #-}
 module Cull
 ( tests
+, gen
 ) where
 
+import Control.Effect.Cull
+import Control.Effect.NonDet (NonDet)
+import Hedgehog
+import Hedgehog.Gen
+import qualified NonDet
 import Test.Tasty
 
 tests :: TestTree
 tests = testGroup "Cull"
   []
+
+
+gen :: (Has Cull sig m, Has NonDet sig m) => (forall a . Gen a -> Gen (m a)) -> Gen a -> Gen (m a)
+gen m a = choice
+  [ subterm (m a) cull
+  , NonDet.gen m a
+  ]
