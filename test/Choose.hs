4e16e5586dc9b0df925fd55e742d5e09b81a03dd
Rob Rix <rob.rix@me.com>
2019-10-14 12:35:45 -0400

    Inline the definition of the <|>/distributivity property.

--- test/Choose.hs
+++ test/Choose.hs
@@ -31,7 +31,7 @@ gen m a = subterm2 (m a) (m a) (liftWith2 "(<|>)" (<|>))
 chooseTests :: (Has Choose sig m, Arg a, Eq a, Eq b, Show a, Show b, Vary a) => (forall a . m a -> PureC [a]) -> (forall a . Show a => Gen a -> Gen (With (m a))) -> Gen a -> Gen b -> [TestTree]
 chooseTests runChoose m a b =
   [ testProperty ">>= distributes over <|>" . forall (m a :. m a :. fn (m b) :. Nil) $
-    \ (With m) (With n) (FnWith k) -> choose_distributivity (===) runChoose m n k
+    \ (With m) (With n) (FnWith k) -> runChoose ((m <|> n) >>= k) === runChoose ((m >>= k) <|> (n >>= k))
   , testProperty "<|> is associative" . forall (m a :. m a :. m a :. Nil) $
     \ (With m) (With n) (With o) -> choose_associativity (===) runChoose m n o
   ]
