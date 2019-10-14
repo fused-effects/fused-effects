4ce7523708cd7a956eef2e753dc60a83e40bb21a
Rob Rix <rob.rix@me.com>
2019-10-13 21:57:42 -0400

    Rename testChoose back to chooseTests.

--- test/Choose.hs
+++ test/Choose.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE RankNTypes, ScopedTypeVariables, TypeApplications #-}
 module Choose
 ( genChoose
-, testChoose
+, chooseTests
 , tests
 ) where
 
@@ -17,19 +17,19 @@ import Test.Tasty.Hedgehog
 
 tests :: TestTree
 tests = testGroup "Choose"
-  [ testGroup "ChooseC"  $ testChoose (ChooseC.runChooseS (pure . pure))
-  , testGroup "NonEmpty" $ testChoose (pure . toList)
+  [ testGroup "ChooseC"  $ chooseTests (ChooseC.runChooseS (pure . pure))
+  , testGroup "NonEmpty" $ chooseTests (pure . toList)
   ] where
-  testChoose :: Has Choose sig m => (forall a . m a -> PureC [a]) -> [TestTree]
-  testChoose run = Choose.testChoose run (fmap Blind . genM [genChoose]) genA genB
+  chooseTests :: Has Choose sig m => (forall a . m a -> PureC [a]) -> [TestTree]
+  chooseTests run = Choose.chooseTests run (fmap Blind . genM [genChoose]) genA genB
 
 
 genChoose :: Has Choose sig m => Gen a -> Gen (m a) -> Gen (m a)
 genChoose _ m = subterm2 m m (<|>)
 
 
-testChoose :: forall a b m sig . (Has Choose sig m, Arg a, Eq a, Eq b, Show a, Show b, Vary a) => (forall a . m a -> PureC [a]) -> (forall a . Gen a -> Gen (Blind (m a))) -> Gen a -> Gen b -> [TestTree]
-testChoose runChoose m a b =
+chooseTests :: forall a b m sig . (Has Choose sig m, Arg a, Eq a, Eq b, Show a, Show b, Vary a) => (forall a . m a -> PureC [a]) -> (forall a . Gen a -> Gen (Blind (m a))) -> Gen a -> Gen b -> [TestTree]
+chooseTests runChoose m a b =
   [ testProperty "<|> distributivity" . forall (m a :. m a :. fn @a (m b) :. Nil) $
     \ m n k -> choose_distributivity (~=) runChoose (getBlind m) (getBlind n) (getBlind . apply k)
   , testProperty "<|> associativity" . forall (m a :. m a :. m a :. Nil) $
