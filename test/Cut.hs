2e9e80d7a4d43ac215d4fdcf90ebec1056888cd1
Rob Rix <rob.rix@me.com>
2019-10-14 08:25:41 -0400

    Define a generator for Cut effects.

--- test/Cut.hs
+++ test/Cut.hs
@@ -1,9 +1,24 @@
+{-# LANGUAGE RankNTypes #-}
 module Cut
 ( tests
+, gen
 ) where
 
+import Control.Effect.Cut
+import Control.Effect.NonDet
+import Hedgehog
+import Hedgehog.Gen
+import qualified NonDet
 import Test.Tasty
 
 tests :: TestTree
 tests = testGroup "Cut"
   []
+
+
+gen :: (Has Cut sig m, Has NonDet sig m) => (forall a . Gen a -> Gen (m a)) -> Gen a -> Gen (m a)
+gen m a = choice
+  [ subterm (m a) call
+  , pure cutfail
+  , NonDet.gen m a
+  ]
