d4c44ab03ad2706fde1a14cbacbc7e00990f7585
Rob Rix <rob.rix@me.com>
2019-10-13 22:18:54 -0400

    Define nonDetTests as the combination of emptyTests & chooseTests.

--- test/NonDet.hs
+++ test/NonDet.hs
@@ -1,14 +1,19 @@
+{-# LANGUAGE RankNTypes #-}
 module NonDet
 ( tests
 , genNonDet
+, nonDetTests
 ) where
 
 import qualified Choose
 import Control.Carrier
 import Control.Effect.NonDet (NonDet)
+import Data.Maybe (listToMaybe)
 import qualified Empty
 import Hedgehog
+import Hedgehog.Function
 import Hedgehog.Gen
+import Pure
 import Test.Tasty
 
 tests :: TestTree
@@ -18,3 +23,9 @@ tests = testGroup "NonDet"
 
 genNonDet :: Has NonDet sig m => Gen a -> Gen (m a) -> Gen (m a)
 genNonDet a m = choice [ Empty.gen a m, Choose.gen a m ]
+
+
+nonDetTests :: forall a b m sig . (Has NonDet sig m, Arg a, Eq a, Eq b, Show a, Show b, Vary a) => (forall a . m a -> PureC [a]) -> (forall a. Gen a -> Gen (Blind (m a))) -> Gen a -> Gen b -> [TestTree]
+nonDetTests runNonDet m a b
+  =  Empty.emptyTests   (fmap listToMaybe . runNonDet) m a b
+  ++ Choose.chooseTests runNonDet m a b
