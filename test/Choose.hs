1e0d517d4845822e71a46dd065779077d1d50b3a
Rob Rix <rob.rix@me.com>
2019-10-13 21:00:06 -0400

    Define a generalized test for the choose/distributivity law.

--- test/Choose.hs
+++ test/Choose.hs
@@ -1,10 +1,24 @@
+{-# LANGUAGE RankNTypes, ScopedTypeVariables, TypeApplications #-}
 module Choose
 ( genChoose
+, chooseTests
 ) where
 
 import Control.Effect.Choose
 import Hedgehog
+import Hedgehog.Function
 import Hedgehog.Gen
+import Pure
+import Test.Tasty
+import Test.Tasty.Hedgehog
 
 genChoose :: Has Choose sig m => Gen a -> Gen (m a) -> Gen (m a)
 genChoose _ m = subterm2 m m (<|>)
+
+
+chooseTests :: forall a b m sig . (Has Choose sig m, Arg a, Eq b, Show a, Show b, Vary a) => (forall a . m a -> PureC [a]) -> Gen a -> Gen b -> [TestTree]
+chooseTests runChoose a b =
+  [ testProperty "choose distributivity" . forall (op :. op :. fn @a (Blind <$> genM [genChoose] b) :. Nil) $
+    \ m n k -> choose_distributivity (~=) runChoose (getBlind m) (getBlind n) (getBlind . apply k)
+  ] where
+  op = Blind <$> genM [genChoose] a
