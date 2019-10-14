848d2586404c1f9880fec4b8b7ff88cedc1cbceb
Rob Rix <rob.rix@me.com>
2019-10-13 22:11:22 -0400

    Pass throwTests a higher-order generator.

--- test/Throw.hs
+++ test/Throw.hs
@@ -15,8 +15,8 @@ gen :: Has (Throw e) sig m => Gen e -> Gen a -> Gen (m a) -> Gen (m a)
 gen e _ _ = throwError <$> e
 
 
-throwTests :: forall e m a b sig . (Has (Throw e) sig m, Arg a, Eq b, Eq e, Show a, Show b, Show e, Vary a) => (forall a . m a -> PureC (Either e a)) -> Gen e -> Gen a -> Gen b -> [TestTree]
-throwTests runThrow e _ b =
-  [ testProperty "throwError annihilation" . forall (e :. fn @a (genM [gen e] b) :. Nil) $
+throwTests :: forall e m a b sig . (Has (Throw e) sig m, Arg a, Eq b, Eq e, Show a, Show b, Show e, Vary a) => (forall a . m a -> PureC (Either e a)) -> (forall a . Gen a -> Gen (Blind (m a))) -> Gen e -> Gen a -> Gen b -> [TestTree]
+throwTests runThrow m e _ b =
+  [ testProperty "throwError annihilation" . forall (e :. fn @a (m b) :. Nil) $
     \ e k -> throwError_annihilation (~=) runThrow e (getBlind . apply k)
   ]
