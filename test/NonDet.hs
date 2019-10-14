aee3da9a95761e735f77bb81db139624a5c974d6
Rob Rix <rob.rix@me.com>
2019-10-13 22:25:39 -0400

    Test the <|> left identity law.

--- test/NonDet.hs
+++ test/NonDet.hs
@@ -7,6 +7,7 @@ module NonDet
 
 import qualified Choose
 import Control.Carrier
+import Control.Effect.Choose
 import Control.Effect.NonDet (NonDet)
 import Data.Maybe (listToMaybe)
 import qualified Empty
@@ -15,6 +16,7 @@ import Hedgehog.Function
 import Hedgehog.Gen
 import Pure
 import Test.Tasty
+import Test.Tasty.Hedgehog
 
 tests :: TestTree
 tests = testGroup "NonDet"
@@ -27,5 +29,7 @@ genNonDet a m = choice [ Empty.gen a m, Choose.gen a m ]
 
 nonDetTests :: forall a b m sig . (Has NonDet sig m, Arg a, Eq a, Eq b, Show a, Show b, Vary a) => (forall a . m a -> PureC [a]) -> (forall a. Gen a -> Gen (Blind (m a))) -> Gen a -> Gen b -> [TestTree]
 nonDetTests runNonDet m a b
-  =  Empty.emptyTests   (fmap listToMaybe . runNonDet) m a b
+  =  testProperty "<|> left identity" (forall (m a :. Nil)
+    (\ m -> choose_leftIdentity (~=) runNonDet (getBlind m)))
+  :  Empty.emptyTests   (fmap listToMaybe . runNonDet) m a b
   ++ Choose.chooseTests runNonDet m a b
