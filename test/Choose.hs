9d429ef9c4a53684feceaa435436fccc10893bb2
Rob Rix <rob.rix@me.com>
2019-10-13 21:45:40 -0400

    Parameterize testChoose by the type of actions.

--- test/Choose.hs
+++ test/Choose.hs
@@ -19,18 +19,17 @@ tests = testGroup "Choose"
   [ testGroup "ChooseC" $ testChoose (ChooseC.runChooseS (pure . pure))
   ] where
   testChoose :: Has Choose sig m => (forall a . m a -> PureC [a]) -> [TestTree]
-  testChoose run = Choose.testChoose run genA genB
+  testChoose run = Choose.testChoose run (fmap Blind . genM [genChoose]) genA genB
 
 
 genChoose :: Has Choose sig m => Gen a -> Gen (m a) -> Gen (m a)
 genChoose _ m = subterm2 m m (<|>)
 
 
-testChoose :: forall a b m sig . (Has Choose sig m, Arg a, Eq a, Eq b, Show a, Show b, Vary a) => (forall a . m a -> PureC [a]) -> Gen a -> Gen b -> [TestTree]
-testChoose runChoose a b =
-  [ testProperty "<|> distributivity" . forall (op :. op :. fn @a (Blind <$> genM [genChoose] b) :. Nil) $
+testChoose :: forall a b m sig . (Has Choose sig m, Arg a, Eq a, Eq b, Show a, Show b, Vary a) => (forall a . m a -> PureC [a]) -> (forall a . Gen a -> Gen (Blind (m a))) -> Gen a -> Gen b -> [TestTree]
+testChoose runChoose m a b =
+  [ testProperty "<|> distributivity" . forall (m a :. m a :. fn @a (m b) :. Nil) $
     \ m n k -> choose_distributivity (~=) runChoose (getBlind m) (getBlind n) (getBlind . apply k)
-  , testProperty "<|> associativity" . forall (op :. op :. op :. Nil) $
+  , testProperty "<|> associativity" . forall (m a :. m a :. m a :. Nil) $
     \ m n o -> choose_associativity (~=) runChoose (getBlind m) (getBlind n) (getBlind o)
-  ] where
-  op = Blind <$> genM [genChoose] a
+  ]
