075c15f414522d427d7c3db0c69e6712b302615b
Rob Rix <rob.rix@me.com>
2019-10-14 09:35:02 -0400

    Stub in tests for the Cut properties.

--- test/Cut.hs
+++ test/Cut.hs
@@ -2,13 +2,16 @@
 module Cut
 ( tests
 , gen
+, cutTests
 ) where
 
 import Control.Effect.Cut
 import Control.Effect.NonDet
 import Hedgehog
+import Hedgehog.Function
 import Hedgehog.Gen
 import qualified NonDet
+import Pure
 import Test.Tasty
 
 tests :: TestTree
@@ -22,3 +25,8 @@ gen m a = choice
   , pure cutfail
   , NonDet.gen m a
   ]
+
+
+cutTests :: forall a b m sig . (Has Cut sig m, Has NonDet sig m, Arg a, Eq a, Eq b, Show a, Show b, Vary a) => (forall a . m a -> PureC [a]) -> (forall a. Gen a -> Gen (Blind (m a))) -> Gen a -> Gen b -> [TestTree]
+cutTests runCut m a b
+  = NonDet.nonDetTests runCut m a b
