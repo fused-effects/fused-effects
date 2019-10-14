497169d948e0079ebb7c449aafdd287af963c53a
Rob Rix <rob.rix@me.com>
2019-10-13 21:13:21 -0400

    Rename the operator.

--- test/Choose.hs
+++ test/Choose.hs
@@ -24,9 +24,9 @@ genChoose _ m = subterm2 m m (<|>)
 
 testChoose :: forall a b m sig . (Has Choose sig m, Arg a, Eq a, Eq b, Show a, Show b, Vary a) => (forall a . m a -> PureC [a]) -> Gen a -> Gen b -> [TestTree]
 testChoose runChoose a b =
-  [ testProperty "choose distributivity" . forall (op :. op :. fn @a (Blind <$> genM [genChoose] b) :. Nil) $
+  [ testProperty "<|> distributivity" . forall (op :. op :. fn @a (Blind <$> genM [genChoose] b) :. Nil) $
     \ m n k -> choose_distributivity (~=) runChoose (getBlind m) (getBlind n) (getBlind . apply k)
-  , testProperty "choose associativity" . forall (op :. op :. op :. Nil) $
+  , testProperty "<|> associativity" . forall (op :. op :. op :. Nil) $
     \ m n o -> choose_associativity (~=) runChoose (getBlind m) (getBlind n) (getBlind o)
   ] where
   op = Blind <$> genM [genChoose] a
