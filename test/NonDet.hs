ffdcaf419a948408c3a9fc1bde3645806d315bde
Rob Rix <rob.rix@me.com>
2019-10-14 12:38:02 -0400

    Inline the definition of the <|>/left-identity property.

--- test/NonDet.hs
+++ test/NonDet.hs
@@ -10,6 +10,7 @@ import Control.Carrier
 import qualified Control.Carrier.NonDet.Church as Church.NonDetC
 import qualified Control.Carrier.NonDet.Maybe as Maybe.NonDetC
 import Control.Effect.Choose
+import Control.Effect.Empty
 import Control.Effect.NonDet (NonDet)
 import qualified Control.Monad.Trans.Maybe as MaybeT
 import Data.Maybe (listToMaybe, maybeToList)
@@ -39,7 +40,7 @@ gen m a = choice [ Empty.gen m a, Choose.gen m a ]
 nonDetTests :: (Has NonDet sig m, Arg a, Eq a, Eq b, Show a, Show b, Vary a) => (forall a . m a -> PureC [a]) -> (forall a . Show a => Gen a -> Gen (With (m a))) -> Gen a -> Gen b -> [TestTree]
 nonDetTests runNonDet m a b
   =  testProperty "empty is the left identity of <|>"  (forall (m a :. Nil)
-    (\ (With m) -> choose_leftIdentity  (===) runNonDet m))
+    (\ (With m) -> runNonDet (empty <|> m) === runNonDet m))
   :  testProperty "empty is the right identity of <|>" (forall (m a :. Nil)
     (\ (With m) -> choose_rightIdentity (===) runNonDet m))
   :  Empty.emptyTests   (fmap listToMaybe . runNonDet) m a b
