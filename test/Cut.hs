6ab4eb5aeedbddd2ae44552246bd09f4ca7d4c6f
Rob Rix <rob.rix@me.com>
2019-10-14 21:35:04 -0400

    Rearrange the Cut tests.

--- test/Cut.hs
+++ test/Cut.hs
@@ -2,14 +2,14 @@
 module Cut
 ( tests
 , gen
-, cutTests
+, test
 ) where
 
 import qualified Control.Carrier.Cut.Church as CutC
 import Control.Effect.Choose
 import Control.Effect.Cut (Cut, call, cutfail)
 import Control.Effect.NonDet (NonDet)
-import Hedgehog
+import Hedgehog (Gen, (===))
 import Hedgehog.Function
 import Hedgehog.Gen
 import qualified NonDet
@@ -19,10 +19,8 @@ import Test.Tasty.Hedgehog
 
 tests :: TestTree
 tests = testGroup "Cut"
-  [ testGroup "CutC" $ cutTests CutC.runCutA
-  ] where
-  cutTests :: (Has Cut sig m, Has NonDet sig m) => (forall a . m a -> PureC [a]) -> [TestTree]
-  cutTests run = Cut.cutTests run (m gen) a b
+  [ testGroup "CutC" $ test (m gen) a b CutC.runCutA
+  ]
 
 
 gen :: (Has Cut sig m, Has NonDet sig m, Show a) => (forall a . Show a => Gen a -> Gen (With (m a))) -> Gen a -> Gen (With (m a))
@@ -33,15 +31,15 @@ gen m a = choice
   ]
 
 
-cutTests
+test
   :: forall a b m sig
   .  (Has Cut sig m, Has NonDet sig m, Arg a, Eq a, Eq b, Show a, Show b, Vary a)
-  => (forall a . m a -> PureC [a])
-  -> (forall a . Show a => Gen a -> Gen (With (m a)))
+  => (forall a . Show a => Gen a -> Gen (With (m a)))
   -> Gen a
   -> Gen b
+  -> (forall a . m a -> PureC [a])
   -> [TestTree]
-cutTests runCut m a b
+test m a b runCut
   = testProperty "cutfail annihilates >>=" (forall (fn @a (m a) :. Nil)
     (\ (FnWith k) -> runCut (cutfail >>= k) === runCut cutfail))
   : testProperty "cutfail annihilates <|>" (forall (m a :. Nil)
