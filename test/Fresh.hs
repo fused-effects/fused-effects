ca9b77c2c8ae63c42ddf4facd8ab4af291a0a012
Rob Rix <rob.rix@me.com>
2019-10-14 21:00:35 -0400

    Define tests for Fresh.

--- test/Fresh.hs
+++ test/Fresh.hs
@@ -2,6 +2,7 @@
 module Fresh
 ( tests
 , gen
+, freshTests
 ) where
 
 import Control.Effect.Fresh
@@ -9,6 +10,7 @@ import Hedgehog
 import Hedgehog.Function
 import Pure
 import Test.Tasty
+import Test.Tasty.Hedgehog
 
 tests :: TestTree
 tests = testGroup "Fresh"
@@ -19,3 +21,10 @@ gen :: (Has Fresh sig m, Show a) => (forall a . Show a => Gen a -> Gen (With (m 
 gen _ a = do
   f <- fn a
   pure (liftWith2 "fmap" fmap (showingFn f) (atom "fresh" fresh))
+
+
+freshTests :: (Has Fresh sig m, Show a) => (forall a . m a -> PureC a) -> (forall a . Show a => Gen a -> Gen (With (m a))) -> Gen a -> [TestTree]
+freshTests runFresh m a =
+  [ testProperty "fresh yields unique values" . forall (m a :. Nil) $
+    \ (With m) -> runFresh (m >> fresh) /== runFresh (m >> fresh >> fresh)
+  ]
