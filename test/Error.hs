76e4a711c2e67fc7e55720086dd193fee9a2b424
Rob Rix <rob.rix@me.com>
2019-10-14 21:30:46 -0400

    Rearrange the Error tests.

--- test/Error.hs
+++ test/Error.hs
@@ -2,14 +2,14 @@
 module Error
 ( tests
 , gen
-, errorTests
+, test
 ) where
 
 import qualified Control.Carrier.Error.Either as ErrorC
 import Control.Effect.Error
 import qualified Control.Monad.Trans.Except as ExceptT
 import qualified Catch
-import Hedgehog
+import Hedgehog (Gen)
 import Hedgehog.Function
 import Hedgehog.Gen
 import Pure
@@ -18,12 +18,10 @@ import Test.Tasty
 
 tests :: TestTree
 tests = testGroup "Error" $
-  [ testGroup "ErrorC"  $ errorTests ErrorC.runError
-  , testGroup "Either"  $ errorTests pure
-  , testGroup "ExceptT" $ errorTests ExceptT.runExceptT
-  ] where
-  errorTests :: Has (Error E) sig m => (forall a . m a -> PureC (Either E a)) -> [TestTree]
-  errorTests run = Error.errorTests run (m (gen e)) e a b
+  [ testGroup "ErrorC"  $ test e (m (gen e)) a b ErrorC.runError
+  , testGroup "Either"  $ test e (m (gen e)) a b pure
+  , testGroup "ExceptT" $ test e (m (gen e)) a b ExceptT.runExceptT
+  ]
 
 
 gen :: (Has (Error e) sig m, Arg e, Show a, Show e, Vary e) => Gen e -> (forall a . Show a => Gen a -> Gen (With (m a))) -> Gen a -> Gen (With (m a))
@@ -33,14 +31,14 @@ gen e m a = choice
   ]
 
 
-errorTests
+test
   :: (Has (Error e) sig m, Arg a, Arg e, Eq a, Eq b, Eq e, Show a, Show b, Show e, Vary a, Vary e)
-  => (forall a . m a -> PureC (Either e a))
+  => Gen e
   -> (forall a . Show a => Gen a -> Gen (With (m a)))
-  -> Gen e
   -> Gen a
   -> Gen b
+  -> (forall a . m a -> PureC (Either e a))
   -> [TestTree]
-errorTests runError m e a b
+test e m a b runError
   =  Throw.test e m a b runError
   ++ Catch.test e m a b runError
