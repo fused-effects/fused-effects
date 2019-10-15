a6a00909257f554f02eb3be1a7dbf9ef72b126c2
Rob Rix <rob.rix@me.com>
2019-10-14 21:32:47 -0400

    Rearrange the Choose tests.

--- test/Choose.hs
+++ test/Choose.hs
@@ -2,13 +2,13 @@
 module Choose
 ( tests
 , gen
-, chooseTests
+, test
 ) where
 
 import qualified Control.Carrier.Choose.Church as ChooseC
 import Control.Effect.Choose
 import Data.List.NonEmpty
-import Hedgehog
+import Hedgehog (Gen, (===))
 import Hedgehog.Function
 import Hedgehog.Gen
 import Pure
@@ -17,25 +17,23 @@ import Test.Tasty.Hedgehog
 
 tests :: TestTree
 tests = testGroup "Choose"
-  [ testGroup "ChooseC"  $ chooseTests (ChooseC.runChooseS (pure . pure))
-  , testGroup "NonEmpty" $ chooseTests (pure . toList)
-  ] where
-  chooseTests :: Has Choose sig m => (forall a . m a -> PureC [a]) -> [TestTree]
-  chooseTests run = Choose.chooseTests run (m gen) a b
+  [ testGroup "ChooseC"  $ test (m gen) a b (ChooseC.runChooseS (pure . pure))
+  , testGroup "NonEmpty" $ test (m gen) a b (pure . toList)
+  ]
 
 
 gen :: (Has Choose sig m, Show a) => (forall a . Show a => Gen a -> Gen (With (m a))) -> Gen a -> Gen (With (m a))
 gen m a = subterm2 (m a) (m a) (liftWith2 "(<|>)" (<|>))
 
 
-chooseTests
+test
   :: (Has Choose sig m, Arg a, Eq a, Eq b, Show a, Show b, Vary a)
-  => (forall a . m a -> PureC [a])
-  -> (forall a . Show a => Gen a -> Gen (With (m a)))
+  => (forall a . Show a => Gen a -> Gen (With (m a)))
   -> Gen a
   -> Gen b
+  -> (forall a . m a -> PureC [a])
   -> [TestTree]
-chooseTests runChoose m a b =
+test m a b runChoose =
   [ testProperty ">>= distributes over <|>" . forall (m a :. m a :. fn (m b) :. Nil) $
     \ (With m) (With n) (FnWith k) -> runChoose ((m <|> n) >>= k) === runChoose ((m >>= k) <|> (n >>= k))
   , testProperty "<|> is associative" . forall (m a :. m a :. m a :. Nil) $
