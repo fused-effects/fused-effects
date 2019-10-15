a9c1a53822ac3ce66de6460d633fcd91ae064fdb
Rob Rix <rob.rix@me.com>
2019-10-14 20:53:35 -0400

    Define a generator for Fresh effects.

--- test/Fresh.hs
+++ test/Fresh.hs
@@ -1,9 +1,21 @@
+{-# LANGUAGE RankNTypes #-}
 module Fresh
 ( tests
+, gen
 ) where
 
+import Control.Effect.Fresh
+import Hedgehog
+import Hedgehog.Function
+import Pure
 import Test.Tasty
 
 tests :: TestTree
 tests = testGroup "Fresh"
   []
+
+
+gen :: (Has Fresh sig m, Show a) => (forall a . Show a => Gen a -> Gen (With (m a))) -> Gen a -> Gen (With (m a))
+gen _ a = do
+  f <- fn a
+  pure (liftWith2 "fmap" fmap (showingFn f) (atom "fresh" fresh))
