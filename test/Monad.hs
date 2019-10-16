94aad2eb1698e884ec154a510cd260fb65435600
Rob Rix <rob.rix@me.com>
2019-10-15 23:36:40 -0400

    Define a property for the >>=/right-identity law.

--- test/Monad.hs
+++ test/Monad.hs
@@ -8,7 +8,7 @@ import Test.Tasty
 import Test.Tasty.Hedgehog
 
 test
-  :: (Monad m, Arg a, Eq b, Show a, Show b, Vary a)
+  :: (Monad m, Arg a, Eq a, Eq b, Show a, Show b, Vary a)
   => (forall a . Show a => Gen a -> Gen (With (m a)))
   -> Gen a
   -> Gen b
@@ -17,4 +17,6 @@ test
 test m a b run =
   [ testProperty "return is the left-identity of >>=" . forall (a :. fn (m b) :. Nil) $
     \ a (FnWith k) -> run (return a >>= k) === run (k a)
+  , testProperty "return is the right-identity of >>=" . forall (m a :. Nil) $
+    \ (With m) -> run (m >>= return) === run m
   ]
