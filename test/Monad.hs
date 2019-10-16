70e4b6e54c9638420364afb6aefdf193a63d54a5
Rob Rix <rob.rix@me.com>
2019-10-15 23:35:47 -0400

    Define a test for the >>=/left-identity law.

--- test/Monad.hs
+++ test/Monad.hs
@@ -1,2 +1,20 @@
+{-# LANGUAGE RankNTypes #-}
 module Monad
-() where
+( test
+) where
+
+import Gen
+import Test.Tasty
+import Test.Tasty.Hedgehog
+
+test
+  :: (Monad m, Arg a, Eq b, Show a, Show b, Vary a)
+  => (forall a . Show a => Gen a -> Gen (With (m a)))
+  -> Gen a
+  -> Gen b
+  -> (forall a . m a -> PureC a)
+  -> [TestTree]
+test m a b run =
+  [ testProperty "return is the left-identity of >>=" . forall (a :. fn (m b) :. Nil) $
+    \ a (FnWith k) -> run (return a >>= k) === run (k a)
+  ]
