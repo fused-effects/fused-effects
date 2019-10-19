c43412881ae5b0791bb89856a4b6901701d4c6e7
Rob Rix <rob.rix@me.com>
2019-10-19 08:47:47 -0400

    Hide Throw constructors from re-export.

--- src/Control/Carrier/Throw/Either.hs
+++ src/Control/Carrier/Throw/Either.hs
@@ -4,13 +4,15 @@ module Control.Carrier.Throw.Either
   runThrow
 , ThrowC(..)
   -- * Throw effect
-, module Control.Effect.Throw
+, module X
 ) where
 
 import Control.Applicative (Alternative)
 import Control.Carrier
 import Control.Carrier.Error.Either
 import Control.Effect.Throw
+import Control.Effect.Throw as X (Throw)
+import Control.Effect.Throw as X hiding (Throw)
 import Control.Monad (MonadPlus)
 import qualified Control.Monad.Fail as Fail
 import Control.Monad.Fix
