86d095e1c36cdf0820b2ece623962b3c609331f0
Rob Rix <rob.rix@me.com>
2019-10-19 09:34:50 -0400

    :fire: runNonDetC.

--- src/Control/Carrier/NonDet/Church.hs
+++ src/Control/Carrier/NonDet/Church.hs
@@ -61,10 +61,7 @@ runNonDetM leaf = runNonDet (liftA2 mappend) (pure . leaf) (pure mempty)
 -- | A carrier for 'NonDet' effects based on Ralf Hinze’s design described in [Deriving Backtracking Monad Transformers](https://www.cs.ox.ac.uk/ralf.hinze/publications/#P12).
 --
 -- @since 1.0.0.0
-newtype NonDetC m a = NonDetC
-  { -- | A handler receiving continuations respectively interpreting '<|>', 'pure', and 'empty'.
-    runNonDetC :: forall b . (m b -> m b -> m b) -> (a -> m b) -> m b -> m b
-  }
+newtype NonDetC m a = NonDetC (forall b . (m b -> m b -> m b) -> (a -> m b) -> m b -> m b)
   deriving (Functor)
 
 instance Applicative (NonDetC m) where
