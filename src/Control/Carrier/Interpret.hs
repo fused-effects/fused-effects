f448a21f05f373b5edfc6e5063a549b3b674337a
Rob Rix <rob.rix@me.com>
2019-10-19 10:30:24 -0400

    Reformat a bunch of stuff.

--- src/Control/Carrier/Interpret.hs
+++ src/Control/Carrier/Interpret.hs
@@ -26,12 +26,11 @@ import Control.Monad.Trans.Class
 import Unsafe.Coerce (unsafeCoerce)
 
 -- | A @Handler@ is a function that interprets effects described by @sig@ into the carrier monad @m@.
-newtype Handler sig m =
-  Handler { runHandler :: forall s x . sig (InterpretC s sig m) x -> InterpretC s sig m x }
+newtype Handler sig m = Handler
+  { runHandler :: forall s x . sig (InterpretC s sig m) x -> InterpretC s sig m x }
 
 
-newtype Tagged a b =
-  Tagged { unTag :: b }
+newtype Tagged a b = Tagged { unTag :: b }
 
 class Reifies s a | s -> a where
   reflect :: Tagged s a
@@ -40,15 +39,13 @@ class Reifies s a | s -> a where
 data Skolem
 
 -- | @Magic@ captures the GHC implementation detail of how single method type classes are implemented.
-newtype Magic a r =
-  Magic (Reifies Skolem a => Tagged Skolem r)
+newtype Magic a r = Magic (Reifies Skolem a => Tagged Skolem r)
 
 -- For more information on this technique, see the @reflection@ library. We use the formulation described in https://github.com/ekmett/reflection/issues/31 for better inlining.
 --
 -- Essentially we can view @k@ as internally a function of type @Reifies s a -> Tagged s r@, whch we can again view as just @a -> Tagged s r@ through @unsafeCoerce@. After this coercion, we just apply the function to @a@.
 reify :: forall a r . a -> (forall s . Reifies s a => Tagged s r) -> r
-reify a k =
-  unsafeCoerce (Magic @a k) a
+reify a k = unsafeCoerce (Magic @a k) a
 
 
 -- | Interpret an effect using a higher-order function.
@@ -57,26 +54,17 @@ reify a k =
 --
 -- @since 1.0.0.0
 runInterpret
-  :: forall eff m a.
-     (HFunctor eff, Monad m)
+  :: forall eff m a
+  .  (HFunctor eff, Monad m)
   => (forall x . eff m x -> m x)
   -> (forall s . Reifies s (Handler eff m) => InterpretC s eff m a)
   -> m a
-runInterpret f m =
-  reify (Handler handler) (go m)
+runInterpret f m = reify (Handler handler) (go m) where
+  handler :: forall s x . eff (InterpretC s eff m) x -> InterpretC s eff m x
+  handler e = InterpretC (f (handleCoercible e))
 
-  where
-
-    handler :: forall s x . eff (InterpretC s eff m) x -> InterpretC s eff m x
-    handler e =
-      InterpretC (f (handleCoercible e))
-
-    go
-      :: forall x s .
-         InterpretC s eff m x
-      -> Tagged s (m x)
-    go (InterpretC m) =
-      Tagged m
+  go :: forall x s .  InterpretC s eff m x -> Tagged s (m x)
+  go (InterpretC m) = Tagged m
 
 -- | Interpret an effect using a higher-order function with some state variable.
 --
@@ -87,14 +75,11 @@ runInterpretState
   -> s
   -> (forall t. Reifies t (Handler eff (StateC s m)) => InterpretC t eff (StateC s m) a)
   -> m (s, a)
-runInterpretState handler state m =
-  runState state $
-  runInterpret
-    (\e -> StateC (\s -> handler s e))
-    m
+runInterpretState handler state m
+  = runState state
+  $ runInterpret (\e -> StateC (\s -> handler s e)) m
 
-newtype InterpretC s (sig :: (* -> *) -> * -> *) m a =
-  InterpretC (m a)
+newtype InterpretC s (sig :: (* -> *) -> * -> *) m a = InterpretC (m a)
   deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO, MonadPlus)
 
 instance MonadTrans (InterpretC s sig) where
