5bd4a4b11b2d3312b23c07313b47f19a7ccef02f
Rob Rix <rob.rix@me.com>
2019-10-19 08:47:16 -0400

    Hide Lift constructors from re-export.

--- src/Control/Carrier/Lift.hs
+++ src/Control/Carrier/Lift.hs
@@ -6,12 +6,14 @@ module Control.Carrier.Lift
   runM
 , LiftC(..)
   -- * Lift effect
-, module Control.Effect.Lift
+, module X
 ) where
 
 import Control.Applicative (Alternative(..))
 import Control.Carrier
 import Control.Effect.Lift
+import Control.Effect.Lift as X (Lift)
+import Control.Effect.Lift as X hiding (Lift)
 import Control.Monad (MonadPlus(..))
 import qualified Control.Monad.Fail as Fail
 import Control.Monad.Fix
