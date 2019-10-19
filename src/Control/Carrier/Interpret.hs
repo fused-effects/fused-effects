1abfe77e1c5de5bffd9180fc5fde3c5c9b5f8eb2
Rob Rix <rob.rix@me.com>
2019-10-19 10:33:58 -0400

    Revert "Match the type in the pattern."
    This reverts commit f753707b9366e360aa710c83c16f2e6d90e280bf.

--- src/Control/Carrier/Interpret.hs
+++ src/Control/Carrier/Interpret.hs
@@ -44,8 +44,8 @@ newtype Magic a r = Magic (Reifies Skolem a => Tagged Skolem r)
 -- For more information on this technique, see the @reflection@ library. We use the formulation described in https://github.com/ekmett/reflection/issues/31 for better inlining.
 --
 -- Essentially we can view @k@ as internally a function of type @Reifies s a -> Tagged s r@, whch we can again view as just @a -> Tagged s r@ through @unsafeCoerce@. After this coercion, we just apply the function to @a@.
-reify :: a -> (forall s . Reifies s a => Tagged s r) -> r
-reify (a :: a) k = unsafeCoerce (Magic @a k) a
+reify :: forall a r . a -> (forall s . Reifies s a => Tagged s r) -> r
+reify a k = unsafeCoerce (Magic @a k) a
 
 
 -- | Interpret an effect using a higher-order function.
