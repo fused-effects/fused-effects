d14455d8a3052ba468915fda01553cf58ad2f692
Rob Rix <rob.rix@me.com>
2019-10-20 12:41:04 -0400

    Add a property testing the purity law.

--- test/MonadFix.hs
+++ test/MonadFix.hs
@@ -3,14 +3,19 @@ module MonadFix
 ( test
 ) where
 
+import Control.Monad.Fix
 import Gen
 import Test.Tasty
+import Test.Tasty.Hedgehog
 
 test
-  :: GenM m
+  :: (MonadFix m, Arg a, Eq (g a), Functor f, Show a, Show (g a), Vary a)
+  => GenM m
   -> Gen a
   -> Gen (f ())
   -> Run f g m
   -> [TestTree]
-test _ _ _ (Run _) =
-  []
+test _ a s (Run run) =
+  [ testProperty "purity" . forall (s :. fn a :. Nil) $
+    \ s h -> run (mfix (return . h) <$ s) === run (return (fix h) <$ s)
+  ]
