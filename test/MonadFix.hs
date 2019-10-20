aaf46f8951e843be5aca22dca4195f3172337a87
Rob Rix <rob.rix@me.com>
2019-10-20 12:53:50 -0400

    Test the purity property against terminating functions.

--- test/MonadFix.hs
+++ test/MonadFix.hs
@@ -9,13 +9,13 @@ import Test.Tasty
 import Test.Tasty.Hedgehog
 
 test
-  :: (MonadFix m, Arg a, Eq (g a), Functor f, Show a, Show (g a), Vary a)
+  :: (MonadFix m, Eq (g a), Functor f, Show (g a))
   => GenM m
   -> Gen a
   -> Gen (f ())
   -> Run f g m
   -> [TestTree]
 test _ a s (Run run) =
-  [ testProperty "purity" . forall (s :. fn a :. Nil) $
+  [ testProperty "purity" . forall (s :. termFn a :. Nil) $
     \ s h -> run (mfix (return . h) <$ s) === run (return (fix h) <$ s)
   ]
