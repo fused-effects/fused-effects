189f35b301d7e8abc9d1cceb73681af58012964c
Rob Rix <rob.rix@me.com>
2019-10-20 13:14:27 -0400

    Define a property for the left-shrinking/tightening law.

--- test/MonadFix.hs
+++ test/MonadFix.hs
@@ -9,13 +9,16 @@ import Test.Tasty
 import Test.Tasty.Hedgehog
 
 test
-  :: (MonadFix m, Eq (g a), Functor f, Show (g a))
+  :: (MonadFix m, Arg a, Eq (g a), Eq (g b), Functor f, Show a, Show (g a), Show (g b), Vary a)
   => GenM m
   -> Gen a
+  -> Gen b
   -> Gen (f ())
   -> Run f g m
   -> [TestTree]
-test _ a s (Run run) =
+test m a b s (Run run) =
   [ testProperty "purity" . forall (s :. termFn a :. Nil) $
     \ s h -> run (mfix (return . h) <$ s) === run (return (fix h) <$ s)
+  , testProperty "left-shrinking" . forall (s :. m a :. termFn (fn (m b)) :. Nil) $
+    \ s a f -> run (mfix (\ x -> a >>= \ y -> f x y) <$ s) === run ((a >>= \ y -> mfix (\ x -> f x y)) <$ s)
   ]
