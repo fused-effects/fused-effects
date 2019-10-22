3a5e2bfa744db2bb5fbb03f769ac73cd3d13385f
Rob Rix <rob.rix@me.com>
2019-10-21 22:19:59 -0400

    Re-enable the MonadFix tests for CutC.

--- test/Cut.hs
+++ test/Cut.hs
@@ -14,7 +14,7 @@ import Control.Effect.NonDet (NonDet)
 import Data.Semigroup as S ((<>))
 import Gen
 import qualified Monad
--- import qualified MonadFix
+import qualified MonadFix
 import qualified NonDet
 import qualified Reader
 import Test.Tasty
@@ -24,7 +24,7 @@ tests :: TestTree
 tests = testGroup "Cut"
   [ testGroup "CutC" $
     [ testMonad
-    -- , testMonadFix
+    , testMonadFix
     , testCut
     ] >>= ($ runL CutC.runCutA)
   , testGroup "ReaderC · CutC" $
@@ -33,7 +33,7 @@ tests = testGroup "Cut"
   --   Cut.test (local (id @R)) (m (gen0 S.<> Reader.gen0 r) (\ m -> genN m S.<> Reader.genN r m)) a b (pair <*> r <*> unit) (Run (uncurry ((. CutC.runCutA) . runReader)))
   ] where
   testMonad    run = Monad.test    (m gen0 genN) a b c initial run
-  -- testMonadFix run = MonadFix.test (m gen0 genN) a b   initial run
+  testMonadFix run = MonadFix.test (m gen0 genN) a b   initial run
   testCut      run = Cut.test id   (m gen0 genN) a b   initial run
   initial = identity <*> unit
 
