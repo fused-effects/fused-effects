80b3d9dc60493c2401270484051bc3f0a2cfda8b
Rob Rix <rob.rix@me.com>
2019-10-21 20:50:15 -0400

    Derive the Alternative instance for CutC.

--- src/Control/Carrier/Cut/Church.hs
+++ src/Control/Carrier/Cut/Church.hs
@@ -56,13 +56,7 @@ runCutM leaf = runCut (liftA2 mappend) (pure . leaf) (pure mempty)
 
 -- | @since 1.0.0.0
 newtype CutC m a = CutC (NonDetC (StateC Bool m) a)
-  deriving (Applicative, Functor, Monad, Fail.MonadFail, MonadIO)
-
-instance Alternative (CutC m) where
-  empty = CutC empty
-  {-# INLINE empty #-}
-  CutC l <|> CutC r = CutC $ l <|> r
-  {-# INLINE (<|>) #-}
+  deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadIO)
 
 -- | Separate fixpoints are computed for each branch.
 deriving instance MonadFix m => MonadFix (CutC m)
