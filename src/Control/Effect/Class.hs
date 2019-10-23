2995edb826b2a711fd29d4cf576bd650fa779e9f
Rob Rix <rob.rix@me.com>
2019-10-23 18:52:36 -0400

    :fire: GHFunctor.

--- src/Control/Effect/Class.hs
+++ src/Control/Effect/Class.hs
@@ -9,8 +9,7 @@ module Control.Effect.Class
 , hmapDefault
 , handleCoercible
 , Effect(..)
--- * Generic deriving of 'HFunctor' & 'Effect' instances.
-, GHFunctor(..)
+-- * Generic deriving of 'Effect' instances.
 , GEffect(..)
 ) where
 
@@ -69,53 +68,6 @@ class HFunctor sig => Effect sig where
                  -> sig n (f a)
   handle state handler = to1 . ghandle state handler . from1
   {-# INLINE handle #-}
-
-
--- | Generic implementation of 'HFunctor'.
-class GHFunctor m m' rep rep' where
-  -- | Generic implementation of 'hmap'.
-  ghmap :: (Monad m, Functor m') => (forall x . m x -> m' x) -> (rep a -> rep' a)
-
-instance GHFunctor m m' rep rep' => GHFunctor m m' (M1 i c rep) (M1 i c rep') where
-  ghmap f = M1 . ghmap f . unM1
-  {-# INLINE ghmap #-}
-
-instance (GHFunctor m m' l l', GHFunctor m m' r r') => GHFunctor m m' (l :+: r) (l' :+: r') where
-  ghmap f (L1 l) = L1 (ghmap f l)
-  ghmap f (R1 r) = R1 (ghmap f r)
-  {-# INLINE ghmap #-}
-
-instance (GHFunctor m m' l l', GHFunctor m m' r r') => GHFunctor m m' (l :*: r) (l' :*: r') where
-  ghmap f (l :*: r) = ghmap f l :*: ghmap f r
-  {-# INLINE ghmap #-}
-
-instance GHFunctor m m' V1 V1 where
-  ghmap _ v = case v of {}
-  {-# INLINE ghmap #-}
-
-instance GHFunctor m m' U1 U1 where
-  ghmap _ = id
-  {-# INLINE ghmap #-}
-
-instance GHFunctor m m' (K1 R c) (K1 R c) where
-  ghmap _ = coerce
-  {-# INLINE ghmap #-}
-
-instance GHFunctor m m' Par1 Par1 where
-  ghmap _ = coerce
-  {-# INLINE ghmap #-}
-
-instance (Functor f, GHFunctor m m' g g') => GHFunctor m m' (f :.: g) (f :.: g') where
-  ghmap f = Comp1 . fmap (ghmap f) . unComp1
-  {-# INLINE ghmap #-}
-
-instance GHFunctor m m' (Rec1 m) (Rec1 m') where
-  ghmap f = Rec1 . f . unRec1
-  {-# INLINE ghmap #-}
-
-instance HFunctor f => GHFunctor m m' (Rec1 (f m)) (Rec1 (f m')) where
-  ghmap f = Rec1 . hmap f . unRec1
-  {-# INLINE ghmap #-}
 
 
 -- | Generic implementation of 'Effect'.
