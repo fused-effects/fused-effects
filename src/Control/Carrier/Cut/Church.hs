ae71431e302e152adc98374548028ed94aa5821c
Rob Rix <rob.rix@me.com>
2019-10-22 23:19:16 -0400

    Revert "Suspend CutC to BinaryTree instead of to []."
    This reverts commit 5a9a5801e2f826b5523a09869cb067febdf5227b.

--- src/Control/Carrier/Cut/Church.hs
+++ src/Control/Carrier/Cut/Church.hs
@@ -16,7 +16,6 @@ module Control.Carrier.Cut.Church
 ) where
 
 import Control.Algebra
-import Control.Monad (join)
 import Control.Effect.Cut
 import Control.Effect.NonDet
 import qualified Control.Monad.Fail as Fail
@@ -93,7 +92,7 @@ instance (Algebra sig m, Effect sig) => Algebra (Cut :+: NonDet :+: sig) (CutC m
   eff (L (Call m k)) = CutC $ \ cons nil fail -> runCut (\ a as -> runCut cons as fail (k a)) nil nil m
   eff (R (L (L Empty)))      = empty
   eff (R (L (R (Choose k)))) = k True <|> k False
-  eff (R (R other))          = CutC $ \ cons nil _ -> eff (handle (Leaf ()) (fmap join . traverse (runCut (fmap . Fork . Leaf) (pure Nil) (pure Fail))) other) >>= foldr cons nil
+  eff (R (R other))          = CutC $ \ cons nil _ -> eff (handle [()] (fmap concat . traverse runCutA) other) >>= foldr cons nil
   {-# INLINE eff #-}
 
 
