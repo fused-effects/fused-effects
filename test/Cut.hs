049f3ff80aa0e4a5333bd3d4384fdec02c9a72ad
Rob Rix <rob.rix@me.com>
2019-10-22 22:39:28 -0400

    Re-enable the Cut tests.

--- test/Cut.hs
+++ test/Cut.hs
@@ -14,7 +14,7 @@ import Control.Effect.NonDet (NonDet)
 import Data.Semigroup as S ((<>))
 import Gen
 import qualified Monad
--- import qualified MonadFix
+import qualified MonadFix
 import qualified NonDet
 import qualified Reader
 import Test.Tasty
@@ -24,16 +24,16 @@ tests :: TestTree
 tests = testGroup "Cut"
   [ testGroup "CutC" $
     [ testMonad
-    -- , testMonadFix
+    , testMonadFix
     , testCut
     ] >>= ($ runL CutC.runCutA)
   , testGroup "ReaderC · CutC" $
     Cut.test (local (id @R)) (m (gen0 S.<> Reader.gen0 r) (\ m -> genN m S.<> Reader.genN r m)) a b (pair <*> r <*> unit) (Run (CutC.runCutA . uncurry runReader))
-  -- , testGroup "CutC · ReaderC" $
-  --   Cut.test (local (id @R)) (m (gen0 S.<> Reader.gen0 r) (\ m -> genN m S.<> Reader.genN r m)) a b (pair <*> r <*> unit) (Run (uncurry ((. CutC.runCutA) . runReader)))
+  , testGroup "CutC · ReaderC" $
+    Cut.test (local (id @R)) (m (gen0 S.<> Reader.gen0 r) (\ m -> genN m S.<> Reader.genN r m)) a b (pair <*> r <*> unit) (Run (uncurry ((. CutC.runCutA) . runReader)))
   ] where
   testMonad    run = Monad.test    (m gen0 genN) a b c initial run
-  -- testMonadFix run = MonadFix.test (m gen0 genN) a b   initial run
+  testMonadFix run = MonadFix.test (m gen0 genN) a b   initial run
   testCut      run = Cut.test id   (m gen0 genN) a b   initial run
   initial = identity <*> unit
 
