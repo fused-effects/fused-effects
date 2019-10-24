7086045a79d99a0b83b990228d3c4b9be599ef9c
Rob Rix <rob.rix@me.com>
2019-10-24 09:25:46 -0400

    Move Reader into Control.Algebra.Internal.

--- src/Control/Algebra/Internal.hs
+++ src/Control/Algebra/Internal.hs
@@ -6,6 +6,7 @@ module Control.Algebra.Internal
 , Empty(..)
 , Lift(..)
 , NonDet
+, Reader(..)
 ) where
 
 import Control.Effect.Class
@@ -56,3 +57,19 @@ instance Functor m => Effect   (Lift m)
 --
 -- @since 0.1.0.0
 type NonDet = Empty :+: Choose
+
+
+-- | @since 0.1.0.0
+data Reader r m k
+  = Ask (r -> m k)
+  | forall b . Local (r -> r) (m b) (b -> m k)
+
+deriving instance Functor m => Functor (Reader r m)
+
+instance HFunctor (Reader r) where
+  hmap f (Ask k)       = Ask           (f . k)
+  hmap f (Local g m k) = Local g (f m) (f . k)
+
+instance Effect (Reader r) where
+  handle state handler (Ask k)       = Ask (handler . (<$ state) . k)
+  handle state handler (Local f m k) = Local f (handler (m <$ state)) (handler . fmap k)
