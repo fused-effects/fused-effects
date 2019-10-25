e9253ec82e8878f382bdca3e51145cb4a9a0328b
Rob Rix <rob.rix@me.com>
2019-10-24 21:10:05 -0400

    Test how state gets reset with Try.

--- test/Effect.hs
+++ test/Effect.hs
@@ -1,18 +1,34 @@
-{-# LANGUAGE DeriveFunctor, ExistentialQuantification, FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, StandaloneDeriving, TypeFamilies, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, StandaloneDeriving, TypeApplications, TypeFamilies, TypeOperators, UndecidableInstances #-}
 module Effect
 ( tests
 ) where
 
 import Control.Algebra
+import Control.Carrier.State.Strict
 import Control.Effect.Lift
 import Control.Monad.IO.Class
 import qualified Control.Exception as Exc
+import Data.Monoid (Sum(..))
 import Test.Tasty
+import Test.Tasty.HUnit
 
 tests :: TestTree
 tests = testGroup "Effect"
-  []
+  [ testCase "allows monoidal state above effects requiring Applicative" $ do
+    x <- liftIO . runTry @Exc.SomeException . execState (s 0) $ do
+      put (s 1)
+      v <- try @Exc.SomeException (put (s 2) *> liftIO (Exc.throwIO (Err "test")) *> put (s 3))
+      either (pure . Exc.displayException) (pure . show) v
+    x @?= s 0
+  ] where
+  s :: Int -> Sum Int
+  s = Sum
 
+
+newtype Err = Err { err :: String }
+  deriving (Show)
+
+instance Exc.Exception Err
 
 data Try e m k
   = forall a . Try (m a) (Either e a -> m k)
