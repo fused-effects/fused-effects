a040d8834a4344ee03dccf487873027df6a8e799
Rob Rix <rob.rix@me.com>
2019-10-24 20:52:46 -0400

    Define an algebra for Try.

--- test/Effect.hs
+++ test/Effect.hs
@@ -1,9 +1,12 @@
-{-# LANGUAGE DeriveFunctor, ExistentialQuantification, StandaloneDeriving, TypeFamilies #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, StandaloneDeriving, TypeFamilies, TypeOperators, UndecidableInstances #-}
 module Effect
 ( tests
 ) where
 
-import Control.Effect.Class
+import Control.Algebra
+import Control.Effect.Lift
+import Control.Monad.IO.Class
+import qualified Control.Exception as Exc
 import Test.Tasty
 
 tests :: TestTree
@@ -19,3 +22,11 @@ deriving instance Functor m => Functor (Try e m)
 instance Effect (Try e) where
   type Constrain (Try e) f = Applicative f
   handle state handler (Try m k) = Try (handler (m <$ state)) (handler . fmap k . sequenceA)
+
+
+newtype TryC e m a = TryC { runTry :: m a }
+  deriving (Applicative, Functor, Monad, MonadIO)
+
+instance Exc.Exception e => Algebra (Try e :+: Lift IO) (TryC e IO) where
+  alg (L (Try m k)) = liftIO (Exc.try (runTry m)) >>= k
+  alg (R other)     = TryC (handleCoercible other)
