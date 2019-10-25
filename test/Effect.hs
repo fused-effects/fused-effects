1168f489df4a3dd4aced9b1d386af583138b22e6
Rob Rix <rob.rix@me.com>
2019-10-24 21:11:45 -0400

    Make Try polymorphic in the exception type.

--- test/Effect.hs
+++ test/Effect.hs
@@ -15,10 +15,10 @@ import Test.Tasty.HUnit
 tests :: TestTree
 tests = testGroup "Effect"
   [ testCase "allows monoidal state above effects requiring Applicative" $ do
-    x <- liftIO . runTry @Exc.SomeException . execState (s 0) $ do
+    x <- liftIO . runTry . execState (s 0) $ do
       put (s 1)
-      v <- try @Exc.SomeException (put (s 2) *> liftIO (Exc.throwIO (Err "test")) *> put (s 3))
-      either (pure . Exc.displayException) (pure . show) v
+      v <- try (put (s 2) *> liftIO (Exc.throwIO (Err "test")) *> put (s 3))
+      either (pure . Exc.displayException @Err) (pure . show) v
     x @?= s 0
   ] where
   s :: Int -> Sum Int
@@ -30,22 +30,22 @@ newtype Err = Err { err :: String }
 
 instance Exc.Exception Err
 
-data Try e m k
-  = forall a . Try (m a) (Either e a -> m k)
+data Try m k
+  = forall e a . Exc.Exception e => Try (m a) (Either e a -> m k)
 
-deriving instance Functor m => Functor (Try e m)
+deriving instance Functor m => Functor (Try m)
 
-instance Effect (Try e) where
-  type Constrain (Try e) f = Applicative f
+instance Effect Try where
+  type Constrain Try f = Applicative f
   handle state handler (Try m k) = Try (handler (m <$ state)) (handler . fmap k . sequenceA)
 
-try :: Has (Try e) sig m => m a -> m (Either e a)
+try :: (Exc.Exception e, Has Try sig m) => m a -> m (Either e a)
 try m = send (Try m pure)
 
 
-newtype TryC e m a = TryC { runTry :: m a }
+newtype TryC m a = TryC { runTry :: m a }
   deriving (Applicative, Functor, Monad, MonadIO)
 
-instance Exc.Exception e => Algebra (Try e :+: Lift IO) (TryC e IO) where
+instance Algebra (Try :+: Lift IO) (TryC IO) where
   alg (L (Try m k)) = liftIO (Exc.try (runTry m)) >>= k
   alg (R other)     = TryC (handleCoercible other)
