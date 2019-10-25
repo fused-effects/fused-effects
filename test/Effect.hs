122c3979b3bf974cb733d5f1254e131c163d8af3
Rob Rix <rob.rix@me.com>
2019-10-24 20:40:55 -0400

    Stub in a Try effect.

--- test/Effect.hs
+++ test/Effect.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, StandaloneDeriving #-}
 module Effect
 ( tests
 ) where
@@ -7,3 +8,9 @@ import Test.Tasty
 tests :: TestTree
 tests = testGroup "Effect"
   []
+
+
+data Try e m k
+  = forall a . Try (m a) (Either e a -> m k)
+
+deriving instance Functor m => Functor (Try e m)
