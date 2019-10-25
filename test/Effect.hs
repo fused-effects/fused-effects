92974ba8c54ac26bc41bcff624edcee095991dc4
Rob Rix <rob.rix@me.com>
2019-10-24 20:43:51 -0400

    Define an Effect instance for Try.

--- test/Effect.hs
+++ test/Effect.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE DeriveFunctor, ExistentialQuantification, StandaloneDeriving #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, StandaloneDeriving, TypeFamilies #-}
 module Effect
 ( tests
 ) where
 
+import Control.Effect.Class
 import Test.Tasty
 
 tests :: TestTree
@@ -14,3 +15,7 @@ data Try e m k
   = forall a . Try (m a) (Either e a -> m k)
 
 deriving instance Functor m => Functor (Try e m)
+
+instance Effect (Try e) where
+  type Constrain (Try e) f = Applicative f
+  handle state handler (Try m k) = Try (handler (m <$ state)) (handler . fmap k . sequenceA)
