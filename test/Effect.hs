cff35fa27de133d794ae5099baff9cc35d15253c
Rob Rix <rob.rix@me.com>
2019-10-25 18:40:03 -0400

    Revert "There aren’t type operator sections, apparently."
    This reverts commit 916c98b6bcd91c549b1741835ffaf69434ea6053.

--- test/Effect.hs
+++ test/Effect.hs
@@ -64,6 +64,6 @@ data Thread m k
 --
 -- We don’t go as far as to define a carrier because cooperative multithreading is a bit more of an example than a test, and I don’t care to run 'Control.Concurrent.forkIO' in the unit tests. At any rate, the existence of this instance is test enough for what we’re trying to do.
 instance Effect Thread where
-  type CanHandle Thread = (~) Identity
+  type CanHandle Thread = (~ Identity)
   handle ctx hdl (Fork m k) = Fork (runIdentity <$> hdl (m <$ ctx)) (hdl (k <$ ctx))
   handle ctx hdl (Yield  k) = Yield                                 (hdl (k <$ ctx))
