1d2f2d3bf4d24352616c60a42a4784ee86945411
Rob Rix <rob.rix@me.com>
2019-10-26 15:27:20 -0400

    Move Unlift into the internal module.

--- src/Control/Effect/Lift.hs
+++ src/Control/Effect/Lift.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ExistentialQuantification, RankNTypes, TypeFamilies #-}
+{-# LANGUAGE RankNTypes #-}
 {- | Provides a mechanism to kick off the evaluation of an effect stack that takes place in a monadic context.
 
 'Lift' effects are always the last effect in a given effect stack. These stacks are invoked with 'Control.Carrier.Lift.runM' or 'Control.Algebra.run'.
@@ -26,8 +26,7 @@ module Control.Effect.Lift
 ) where
 
 import Control.Algebra
-import Control.Effect.Lift.Internal (Lift(..))
-import Data.Functor.Identity
+import Control.Effect.Lift.Internal (Lift(..), Unlift(..))
 
 -- | Given a @Lift n@ constraint in a signature carried by @m@, 'sendM'
 -- promotes arbitrary actions of type @n a@ to @m a@. It is spiritually
@@ -37,16 +36,6 @@ import Data.Functor.Identity
 sendM :: (Has (Lift n) sig m, Functor n) => n a -> m a
 sendM = send . Lift . fmap pure
 
-
-data Unlift sig m k
-  = forall a . Unlift ((forall a . m a -> sig a) -> m a) (a -> m k)
-
-instance Functor m => Functor (Unlift sig m) where
-  fmap f (Unlift with k) = Unlift with (fmap f . k)
-
-instance Effect (Unlift sig) where
-  type CanHandle (Unlift sig) ctx = ctx ~ Identity
-  handle ctx dst (Unlift with k) = Unlift (\ run -> dst (with (run . fmap runIdentity . dst . (<$ ctx)) <$ ctx)) (dst . fmap k)
 
 withUnlift :: Has (Unlift n) sig m => ((forall a . m a -> n a) -> m a) -> m a
 withUnlift with = send (Unlift with pure)
