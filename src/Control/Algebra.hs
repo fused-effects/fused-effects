220eac9048bed760a32f279a8be2c5a3178855e8
Rob Rix <rob.rix@me.com>
2019-10-26 15:29:30 -0400

    Revert "Unlift IO."
    This reverts commit bfe2408be9fd13b2f164acd91519fd35d7b0715f.

--- src/Control/Algebra.hs
+++ src/Control/Algebra.hs
@@ -102,9 +102,8 @@ handleCoercible = handleIdentity coerce
 
 -- base
 
-instance Algebra (Lift IO :+: Unlift IO) IO where
-  alg (L (Lift m))        = join m
-  alg (R (Unlift with k)) = with id >>= k
+instance Algebra (Lift IO) IO where
+  alg = join . unLift
 
 instance Algebra (Lift Identity) Identity where
   alg = join . unLift
