40122e1ae942d040c7475966cf6c3d65023d71fb
Rob Rix <rob.rix@me.com>
2019-10-27 07:49:43 -0400

    Fix a reference in the docs.

--- src/Control/Effect/Pure.hs
+++ src/Control/Effect/Pure.hs
@@ -2,7 +2,7 @@
 
 {- | Provides a mechanism to kick off the evaluation of an effect stack in a pure computation.
 
-This is generally the last effect in an effect stack, unless that stack needs to delegate to a base monad with the 'Control.Effect.Lift.Lift' effect. Such stacks are invoked with 'Control.Carrier.Pure.run' once all of their constituent effects have been discharged.
+This is generally the last effect in an effect stack, unless that stack needs to delegate to a base monad with the 'Control.Effect.Lift.Lift' effect. Such stacks are invoked with 'Control.Algebra.run' once all of their constituent effects have been discharged.
 
 Predefined carriers:
 
