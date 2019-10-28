0b71c1bbaabc377ee0a45aa1c11c0ea59d5548e7
Rob Rix <rob.rix@me.com>
2019-10-27 20:56:11 -0400

    Add a test of unlifting through State.

--- test/Lift.hs
+++ test/Lift.hs
@@ -1,9 +1,21 @@
+{-# LANGUAGE TypeApplications #-}
 module Lift
 ( tests
 ) where
 
+import Control.Carrier.State.Strict
+import Control.Effect.Lift
+import qualified Control.Exception as E
+import Control.Monad.IO.Class
 import Test.Tasty
+import Test.Tasty.HUnit
 
 tests :: TestTree
 tests = testGroup "Lift"
-  []
+  [ testCase "liftWith" $ do
+    r <- liftIO . runState "yep" $ liftWith $ \ ctx hdl ->
+      E.handle (hdl . (<$ ctx) . put . getMsg) $
+        hdl (liftIO (E.throwIO (E.AssertionFailed "nope")) <$ ctx)
+    r @?= ("nope", ())
+  ] where
+  getMsg (E.AssertionFailed msg) = msg
