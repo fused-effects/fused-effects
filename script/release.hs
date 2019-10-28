7492909650d819a69ebd9b18c0ffb586413a1908
Rob Rix <rob.rix@me.com>
2019-10-28 18:33:57 -0400

    command returns its results in Maybe.

--- script/release.hs
+++ script/release.hs
@@ -6,6 +6,7 @@ module Main (main) where
 
 import Control.Algebra
 import Control.Monad.IO.Class
+import Data.Maybe (fromMaybe)
 import GHC.Generics (Generic1)
 import System.Process
 
@@ -17,15 +18,15 @@ main = runTeletype . runDry $ do
   manual "Change the version of the package in `fused-effects.cabal`."
   manual "Push the branch to GitHub and open a draft PR. Double-check the changes, comparing against a previous release PR, e.g. https://github.com/fused-effects/fused-effects/pull/80. When satisfied, mark the PR as ready for review, and request a review from a collaborator."
   auto "Build and publish candidate?" $ do
-    command "cabal" ["v2-build"] >>= write
-    command "cabal" ["v2-sdist"] >>= write
-    command "cabal" ["v2-haddock", "--haddock-for-hackage"] >>= write
+    command "cabal" ["v2-build"] >>= write . fromMaybe ""
+    command "cabal" ["v2-sdist"] >>= write . fromMaybe ""
+    command "cabal" ["v2-haddock", "--haddock-for-hackage"] >>= write . fromMaybe ""
   manual "Build locally using `cabal v2-build`, then collect the sources and docs with `cabal v2-sdist` and `cabal v2-haddock --haddock-for-hackage`, respectively. Note the paths to the tarballs in the output of these commands."
   manual "Publish a candidate release to Hackage with `cabal upload dist-newstyle/sdist/fused-effects-x.y.z.w.tar.gz` and `cabal upload --documentation dist-newstyle/fused-effects-x.y.z.w-docs.tar.gz`. Add a link to the candidate release in a comment on the PR."
   manual "Once the PR has been approved and you’re satisfied with the candidate release, merge the PR. Publish the release to Hackage by running the above commands with the addition of `--publish`."
   manual "Locally, check out `master` and pull the latest changes to your working copy. Make a new tag, e.g. `git tag x.y.z.w`."
   auto "Push tags to GitHub using `git push --tags`?" $
-    command "git" ["push", "--tags"] >>= write . unlines . map ("> " ++) . lines
+    command "git" ["push", "--tags"] >>= write . unlines . map ("> " ++) . lines . fromMaybe ""
 
 manual :: Has Teletype sig m => String -> m ()
 manual s = write s >> prompt "press enter to continue:" >> write ""
@@ -35,12 +36,12 @@ auto s m = write s >> prompt "press enter to run:" >> m
 
 
 data Command m k
-  = Command String [String] (String -> m k)
+  = Command String [String] (Maybe String -> m k)
   deriving (Functor, Generic1)
 
 instance Effect Command
 
-command :: Has Command sig m => String -> [String] -> m String
+command :: Has Command sig m => String -> [String] -> m (Maybe String)
 command cmd args = send (Command cmd args pure)
 
 
@@ -50,7 +51,7 @@ newtype LiveC m a = LiveC { runLive :: m a }
 instance (Algebra sig m, MonadIO m) => Algebra (Command :+: sig) (LiveC m) where
   alg (L (Command cmd args k)) = do
     stdout <- liftIO (readProcess cmd args "")
-    k stdout
+    k (Just stdout)
   alg (R other) = LiveC (handleCoercible other)
 
 
@@ -60,7 +61,7 @@ newtype DryC m a = DryC { runDry :: m a }
 instance Has Teletype sig m => Algebra (Command :+: sig) (DryC m) where
   alg (L (Command cmd args k)) = do
     write $ "> " ++ cmd ++ " " ++ unwords args
-    k ""
+    k Nothing
   alg (R other) = DryC (handleCoercible other)
 
 
