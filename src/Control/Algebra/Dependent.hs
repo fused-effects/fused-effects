bf9fc95df11e16fd265468194f446f1ad1bb02e1
Rob Rix <rob.rix@me.com>
2020-01-18 20:04:27 -0500

    Copy in a renamed version of the Member class.

--- src/Control/Algebra/Dependent.hs
+++ src/Control/Algebra/Dependent.hs
@@ -1,2 +1,38 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Control.Algebra.Dependent
 () where
+
+import Control.Effect.Sum
+
+class DMember (sub :: (* -> *) -> (* -> *)) sup where
+  -- | Inject a member of a signature into the signature.
+  dinj :: sub m a -> sup m a
+
+-- | Reflexivity: @t@ is a member of itself.
+instance DMember t t where
+  dinj = id
+
+-- | Left-recursion: if @t@ is a member of @l1 ':+:' l2 ':+:' r@, then we can inject it into @(l1 ':+:' l2) ':+:' r@ by injection into a right-recursive signature, followed by left-association.
+instance {-# OVERLAPPABLE #-}
+         DMember t (l1 :+: l2 :+: r)
+      => DMember t ((l1 :+: l2) :+: r) where
+  dinj = reassoc . dinj where
+    reassoc (L l)     = L (L l)
+    reassoc (R (L l)) = L (R l)
+    reassoc (R (R r)) = R r
+
+-- | Left-occurrence: if @t@ is at the head of a signature, we can inject it in O(1).
+instance {-# OVERLAPPABLE #-}
+         DMember l (l :+: r) where
+  dinj = L
+
+-- | Right-recursion: if @t@ is a member of @r@, we can inject it into @r@ in O(n), followed by lifting that into @l ':+:' r@ in O(1).
+instance {-# OVERLAPPABLE #-}
+         DMember l r
+      => DMember l (l' :+: r) where
+  dinj = R . dinj
