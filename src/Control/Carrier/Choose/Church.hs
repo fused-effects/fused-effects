61ee226273a68fd1c0c47efc15a962e47dd90858
Rob Rix <rob.rix@me.com>
2020-03-02 15:02:45 -0500

    Define an Algebra' instance for ChooseC.

--- src/Control/Carrier/Choose/Church.hs
+++ src/Control/Carrier/Choose/Church.hs
@@ -95,3 +95,10 @@ instance (Algebra sig m, Effect sig) => Algebra (Choose :+: sig) (ChooseC m) whe
     dst :: ChooseC Identity (ChooseC m a) -> m (ChooseC Identity a)
     dst = runIdentity . runChoose (liftA2 (liftA2 (<|>))) (pure . runChoose (liftA2 (<|>)) (pure . pure))
   {-# INLINE alg #-}
+
+instance (Algebra' sig m, Effect sig) => Algebra' (Choose :+: sig) (ChooseC m) where
+  alg' hom (L (Choose k)) = ChooseC $ \ fork leaf -> fork (runChoose fork leaf (hom (k True))) (runChoose fork leaf (hom (k False)))
+  alg' (hom :: forall x . n x -> ChooseC m x) (R other)      = ChooseC $ \ fork leaf -> alg' id (thread (pure ()) dst other) >>= runIdentity . runChoose (coerce fork) (coerce leaf) where
+    dst :: ChooseC Identity (n a) -> m (ChooseC Identity a)
+    dst = runIdentity . runChoose (liftA2 (liftA2 (<|>))) (pure . runChoose (liftA2 (<|>)) (pure . pure) . hom)
+  {-# INLINE alg' #-}
