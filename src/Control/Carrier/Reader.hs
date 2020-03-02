ba37eb92a511eda226d96f16a5879d2c86f07bde
Rob Rix <rob.rix@me.com>
2020-03-02 14:42:16 -0500

    Derive the MonadTrans instance.

--- src/Control/Carrier/Reader.hs
+++ src/Control/Carrier/Reader.hs
@@ -45,10 +45,7 @@ runReader r (ReaderC runReaderC) = runReaderC r
 -- | @since 1.0.0.0
 newtype ReaderC r m a = ReaderC (r -> m a)
   deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO, MonadPlus) via R.ReaderT r m
-
-instance MonadTrans (ReaderC r) where
-  lift = ReaderC . const
-  {-# INLINE lift #-}
+  deriving (MonadTrans) via R.ReaderT r
 
 instance Algebra sig m => Algebra (Reader r :+: sig) (ReaderC r m) where
   alg (L (Ask       k)) = ReaderC (\ r -> runReader r (k r))
