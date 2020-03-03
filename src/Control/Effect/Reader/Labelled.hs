fb1ca65dffbd79885f9c8d8ba7c738c56255a824
Rob Rix <rob.rix@me.com>
2020-03-02 22:08:53 -0500

    Redefine ask using runUnderLabel.

--- src/Control/Effect/Reader/Labelled.hs
+++ src/Control/Effect/Reader/Labelled.hs
@@ -13,15 +13,16 @@ module Control.Effect.Reader.Labelled
 , run
 ) where
 
-import Control.Effect.Labelled
-import Control.Effect.Reader.Internal
+import           Control.Effect.Labelled
+import qualified Control.Effect.Reader as R
+import           Control.Effect.Reader.Internal
 
 -- | Retrieve the environment value.
 --
 -- @
--- runReader a ('ask' '>>=' k) = runReader a (k a)
+-- runReader a ('runLabelled' @_ @label ('ask' @label) '>>=' k) = runReader a (k a)
 -- @
 --
 -- @since 0.1.0.0
 ask :: forall label r m sig . HasLabelled label (Reader r) sig m => m r
-ask = sendLabelled @label (Ask pure)
+ask = runUnderLabel @_ @label R.ask
