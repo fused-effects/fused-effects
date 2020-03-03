5adb1ad75c410df3de60aed45250bfacc79be35b
Rob Rix <rob.rix@me.com>
2020-03-02 22:13:49 -0500

    Re-export some stuff.

--- src/Control/Effect/State/Labelled.hs
+++ src/Control/Effect/State/Labelled.hs
@@ -1,2 +1,12 @@
 module Control.Effect.State.Labelled
-() where
+( -- * State effect
+  S.State
+  -- * Re-exports
+, Algebra
+, Effect
+, Has
+, run
+) where
+
+import           Control.Effect.Labelled
+import qualified Control.Effect.State as S
