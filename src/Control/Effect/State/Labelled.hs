06db02cf8c68a8e1130ff353833e0118a869f75d
Rob Rix <rob.rix@me.com>
2020-03-02 22:15:52 -0500

    Define a labelled get operation.

--- src/Control/Effect/State/Labelled.hs
+++ src/Control/Effect/State/Labelled.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 module Control.Effect.State.Labelled
 ( -- * State effect
-  S.State
+  State
+, get
   -- * Re-exports
 , Algebra
 , Effect
@@ -10,3 +15,15 @@ module Control.Effect.State.Labelled
 
 import           Control.Effect.Labelled
 import qualified Control.Effect.State as S
+import           Control.Effect.State.Internal
+
+-- | Get the current state value.
+--
+-- @
+-- runState a ('runLabelled' @_ @label ('get' @label) '>>=' k) = runState a (k a)
+-- @
+--
+-- @since 1.0.2.0
+get :: forall label s m sig . HasLabelled label (State s) sig m => m s
+get = runUnderLabel @_ @label S.get
+{-# INLINEABLE get #-}
