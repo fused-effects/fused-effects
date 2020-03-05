22ecf673ebe08303ef4b3df93188d792b07899c2
Rob Rix <rob.rix@me.com>
2020-03-05 17:18:33 -0500

    Re-export the State effect.

--- src/Control/Carrier/State/Church.hs
+++ src/Control/Carrier/State/Church.hs
@@ -2,6 +2,10 @@
 module Control.Carrier.State.Church
 ( -- * State carrier
   StateC(StateC)
+  -- * State effect
+, module Control.Effect.State
 ) where
+
+import Control.Effect.State
 
 newtype StateC s m a = StateC { runStateC :: forall r . (a -> s -> m r) -> s -> m r }
