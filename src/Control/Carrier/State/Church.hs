c9525a894346f41a5c52f7f971c944d48a29c457
Rob Rix <rob.rix@me.com>
2020-03-05 17:18:27 -0500

    Define a church-encoded state carrier.

--- src/Control/Carrier/State/Church.hs
+++ src/Control/Carrier/State/Church.hs
@@ -1,2 +1,7 @@
+{-# LANGUAGE RankNTypes #-}
 module Control.Carrier.State.Church
-() where
+( -- * State carrier
+  StateC(StateC)
+) where
+
+newtype StateC s m a = StateC { runStateC :: forall r . (a -> s -> m r) -> s -> m r }
