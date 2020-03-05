7047ea528cd3607aca87cd676b27e2ebcb57bb0e
Rob Rix <rob.rix@me.com>
2020-03-05 17:30:38 -0500

    Define a Monad instance for Church.StateC.

--- src/Control/Carrier/State/Church.hs
+++ src/Control/Carrier/State/Church.hs
@@ -9,6 +9,9 @@ module Control.Carrier.State.Church
 
 import Control.Effect.State
 
+runStateK :: (a -> s -> m r) -> StateC s m a -> s -> m r
+runStateK k (StateC m) = m k
+
 newtype StateC s m a = StateC (forall r . (a -> s -> m r) -> s -> m r)
   deriving (Functor)
 
@@ -16,3 +19,6 @@ instance Applicative (StateC s m) where
   pure a = StateC $ \ k s -> k a s
 
   StateC f <*> StateC a = StateC $ \ k -> f (a . (k .))
+
+instance Monad (StateC s m) where
+  StateC a >>= f = StateC $ \ k -> a (runStateK k . f)
