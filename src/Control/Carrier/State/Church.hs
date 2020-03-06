a8aba2a9972d675f2573b2f06b83bd76ff3be639
Rob Rix <rob.rix@me.com>
2020-03-05 19:32:56 -0500

    :memo: evalState.

--- src/Control/Carrier/State/Church.hs
+++ src/Control/Carrier/State/Church.hs
@@ -39,6 +39,11 @@ runState :: forall s m a . Applicative m => s -> StateC s m a -> m (s, a)
 runState s (StateC m) = m (\ a s -> pure (s, a)) s
 {-# INLINE runState #-}
 
+-- | Run a 'State' effect, yielding the result value and discarding the final state.
+--
+-- @
+-- 'evalState' s m = 'fmap' 'snd' ('runState' s m)
+-- @
 evalState :: forall s m a . Applicative m => s -> StateC s m a -> m a
 evalState s (StateC m) = m (const . pure) s
 {-# INLINE evalState #-}
