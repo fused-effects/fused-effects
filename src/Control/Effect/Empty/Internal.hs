b9feeb528ace2edb9b246e644e05b515e246933a
Rob Rix <rob.rix@me.com>
2020-03-12 12:19:05 -0400

    Redefine Empty using GADT syntax.
    This doesn’t change its semantics at all.

--- src/Control/Effect/Empty/Internal.hs
+++ src/Control/Effect/Empty/Internal.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE GADTSyntax #-}
 {-# LANGUAGE KindSignatures #-}
 module Control.Effect.Empty.Internal
 ( Empty(..)
 ) where
 
+import Data.Kind (Type)
+
 -- | @since 1.0.0.0
-data Empty (m :: * -> *) k = Empty
+data Empty (m :: Type -> Type) k where
+  Empty :: Empty m a
