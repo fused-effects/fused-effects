622432eb8aadef2ec933affe8d1700f562e0b0cd
Rob Rix <rob.rix@me.com>
2020-03-12 09:53:27 -0400

    Use Handler in the type of the Lift effect.

--- src/Control/Effect/Lift/Internal.hs
+++ src/Control/Effect/Lift/Internal.hs
@@ -4,10 +4,12 @@ module Control.Effect.Lift.Internal
 ( Lift(..)
 ) where
 
+import Control.Algebra.Internal (Handler)
+
 -- | @since 1.0.0.0
 data Lift sig m k
   = forall a . LiftWith
-    (forall ctx . Functor ctx => (forall a . ctx (m a) -> sig (ctx a)) -> ctx () -> sig (ctx a))
+    (forall ctx . Functor ctx => Handler ctx m sig -> ctx () -> sig (ctx a))
     (a -> m k)
 
 instance Functor m => Functor (Lift sig m) where
