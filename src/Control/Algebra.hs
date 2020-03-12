319a79d92caa1bc4c28b1aa1a6d68f45a0368461
Rob Rix <rob.rix@me.com>
2020-03-12 09:52:45 -0400

    Move Handler into Control.Algebra.Internal.

--- src/Control/Algebra.hs
+++ src/Control/Algebra.hs
@@ -27,6 +27,7 @@ module Control.Algebra
 , (:+:) (..)
 ) where
 
+import           Control.Algebra.Internal
 import           Control.Effect.Catch.Internal
 import           Control.Effect.Choose.Internal
 import           Control.Effect.Empty.Internal
@@ -53,35 +54,6 @@ import           Data.List.NonEmpty (NonEmpty)
 import           Data.Monoid
 import qualified Data.Semigroup as S
 import           Data.Tuple (swap)
-
--- | Handlers take an action in @m@ bundled up with some state in some context functor @ctx@, and return an action in @n@ producing a derived state in @ctx@.
---
--- These are expected to be well-behaved /distributive laws/, and are required to adhere to the following laws:
---
--- @
--- handler '.' 'fmap' 'pure' = 'pure'
--- @
--- @
--- handler '.' 'fmap' (k '=<<') = handler '.' 'fmap' k 'Control.Monad.<=<' handler
--- @
---
--- respectively expressing that the handler does not alter the context of pure computations, and that the handler distributes over monadic composition.
---
--- Handlers compose with handlers, using e.g. "Data.Functor.Compose".'Compose' to ensure that the result is itself well-typed as a 'Handler':
---
--- @
--- 'fmap' 'Compose' '.' handler1 '.' 'fmap' handler2 '.' 'getCompose'
--- @
---
--- with monad homomorphisms on the left and right:
---
--- @
--- hom '.' handler
--- @
--- @
--- handler '.' 'fmap' hom
--- @
-type Handler ctx m n = forall x . ctx (m x) -> n (ctx x)
 
 -- | The class of carriers (results) for algebras (effect handlers) over signatures (effects), whose actions are given by the 'alg' method.
 --
--- src/Control/Algebra/Internal.hs
+++ src/Control/Algebra/Internal.hs
@@ -1,2 +1,33 @@
+{-# LANGUAGE RankNTypes #-}
 module Control.Algebra.Internal
-() where
+( Handler
+) where
+
+-- | Handlers take an action in @m@ bundled up with some state in some context functor @ctx@, and return an action in @n@ producing a derived state in @ctx@.
+--
+-- These are expected to be well-behaved /distributive laws/, and are required to adhere to the following laws:
+--
+-- @
+-- handler '.' 'fmap' 'pure' = 'pure'
+-- @
+-- @
+-- handler '.' 'fmap' (k '=<<') = handler '.' 'fmap' k 'Control.Monad.<=<' handler
+-- @
+--
+-- respectively expressing that the handler does not alter the context of pure computations, and that the handler distributes over monadic composition.
+--
+-- Handlers compose with handlers, using e.g. "Data.Functor.Compose".'Compose' to ensure that the result is itself well-typed as a 'Handler':
+--
+-- @
+-- 'fmap' 'Compose' '.' handler1 '.' 'fmap' handler2 '.' 'getCompose'
+-- @
+--
+-- with monad homomorphisms on the left and right:
+--
+-- @
+-- hom '.' handler
+-- @
+-- @
+-- handler '.' 'fmap' hom
+-- @
+type Handler ctx m n = forall x . ctx (m x) -> n (ctx x)
