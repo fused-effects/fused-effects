e0635480788892c512b5f591772cc660b4e7920c
Rob Rix <rob.rix@me.com>
2020-03-13 02:04:07 -0400

    Link right to the type.

--- src/Control/Effect/Empty.hs
+++ src/Control/Effect/Empty.hs
@@ -4,7 +4,7 @@ This can be seen as similar to 'Control.Effect.Fail.Fail', but without an error 
 
 Predefined carriers:
 
-* "Control.Carrier.Empty.Maybe".
+* @"Control.Carrier.Empty.Maybe".'Control.Carrier.Empty.Maybe.MaybeC'@
 * If 'Empty' is the last effect in a stack, it can be interpreted directly to a 'Maybe'.
 
 @since 1.0.0.0
