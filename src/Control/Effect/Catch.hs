5b318f14409e977d3bf290941b1dad364bfea888
Rob Rix <rob.rix@me.com>
2020-03-13 20:17:42 -0400

    Inline catchError.

--- src/Control/Effect/Catch.hs
+++ src/Control/Effect/Catch.hs
@@ -30,3 +30,4 @@ import Control.Effect.Catch.Internal (Catch(..))
 -- @since 0.1.0.0
 catchError :: Has (Catch e) sig m => m a -> (e -> m a) -> m a
 catchError m h = send (Catch m h)
+{-# INLINE catchError #-}
