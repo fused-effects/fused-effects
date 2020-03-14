c2fc73a1479f084a16bd6bc13888581cdb074265
Rob Rix <rob.rix@me.com>
2020-03-14 11:31:12 -0400

    Redefine runState in CPS.

--- src/Control/Carrier/State/Church.hs
+++ src/Control/Carrier/State/Church.hs
@@ -34,40 +34,40 @@ import Control.Monad.Trans.Class
 -- | Run a 'State' effect starting from the passed value.
 --
 -- @
--- 'runState' s ('pure' a) = 'pure' (s, a)
+-- 'runState' ('curry' 'pure') s ('pure' a) = 'pure' (a, s)
 -- @
 -- @
--- 'runState' s 'get' = 'pure' (s, s)
+-- 'runState' ('curry' 'pure') s 'get' = 'pure' (s, s)
 -- @
 -- @
--- 'runState' s ('put' t) = 'pure' (t, ())
+-- 'runState' ('curry' 'pure') s ('put' t) = 'pure' ((), t)
 -- @
 --
 -- @since 1.1.0.0
-runState :: forall s m a . Applicative m => s -> StateC s m a -> m (s, a)
-runState s (StateC m) = m (flip (curry pure)) s
+runState :: forall s m a b . (a -> s -> m b) -> s -> StateC s m a -> m b
+runState f s (StateC m) = m f s
 {-# INLINE runState #-}
 
 -- | Run a 'State' effect, yielding the result value and discarding the final state.
 --
 -- @
--- 'evalState' s m = 'fmap' 'snd' ('runState' s m)
+-- 'evalState' s m = 'runState' ('const' '.' 'pure') s m
 -- @
 --
 -- @since 1.1.0.0
 evalState :: forall s m a . Applicative m => s -> StateC s m a -> m a
-evalState s (StateC m) = m (const . pure) s
+evalState = runState (const . pure)
 {-# INLINE evalState #-}
 
 -- | Run a 'State' effect, yielding the final state and discarding the return value.
 --
 -- @
--- 'execState' s m = 'fmap' 'fst' ('runState' s m)
+-- 'execState' s m = 'runState' ('const' 'pure') s m
 -- @
 --
 -- @since 1.1.0.0
 execState :: forall s m a . Applicative m => s -> StateC s m a -> m s
-execState s (StateC m) = m (const pure) s
+execState = runState (const pure)
 {-# INLINE execState #-}
 
 -- | @since 1.1.0.0
@@ -97,7 +97,7 @@ instance Fail.MonadFail m => Fail.MonadFail (StateC s m) where
   {-# INLINE fail #-}
 
 instance MonadFix m => MonadFix (StateC s m) where
-  mfix f = StateC $ \ k s -> mfix (runState s . f . snd) >>= uncurry (flip k)
+  mfix f = StateC $ \ k s -> mfix (runState (flip (curry pure)) s . f . snd) >>= uncurry (flip k)
   {-# INLINE mfix #-}
 
 instance MonadIO m => MonadIO (StateC s m) where
@@ -114,5 +114,5 @@ instance Algebra sig m => Algebra (State s :+: sig) (StateC s m) where
   alg hdl sig ctx = StateC $ \ k s -> case sig of
     L Get     -> k (s <$ ctx) s
     L (Put s) -> k       ctx  s
-    R other   -> thread (uncurry runState ~<~ hdl) other (s, ctx) >>= uncurry (flip k)
+    R other   -> thread (uncurry (runState (flip (curry pure))) ~<~ hdl) other (s, ctx) >>= uncurry (flip k)
   {-# INLINE alg #-}
