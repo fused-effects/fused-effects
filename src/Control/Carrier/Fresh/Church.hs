330b897f26725f4b19100475418a4972a7ef4b6f
Rob Rix <rob.rix@me.com>
2020-03-15 19:56:45 -0400

    Define an Algebra for FreshC.

--- src/Control/Carrier/Fresh/Church.hs
+++ src/Control/Carrier/Fresh/Church.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Control.Carrier.Fresh.Church
 ( -- * Fresh carrier
-  FreshC(..)
+  FreshC(FreshC)
   -- * Fresh effect
 , module Control.Effect.Fresh
 ) where
 
+import Control.Algebra
 import Control.Applicative (Alternative)
 import Control.Carrier.State.Church
 import Control.Effect.Fresh
@@ -15,5 +21,11 @@ import Control.Monad.Fix
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 
-newtype FreshC m a = FreshC (StateC Int m a)
+newtype FreshC m a = FreshC { runFreshC :: StateC Int m a }
   deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO, MonadPlus, MonadTrans)
+
+instance Algebra sig m => Algebra (Fresh :+: sig) (FreshC m) where
+  alg hdl sig ctx = FreshC $ case sig of
+    L Fresh -> gets (<$ ctx) <* modify (+ (1 :: Int))
+    R other -> alg (runFreshC . hdl) (R other) ctx
+  {-# INLINE alg #-}
