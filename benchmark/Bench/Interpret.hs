bd38c81223c51b93b0742b81db4f9dfa76e95e63
Rob Rix <rob.rix@me.com>
2020-03-15 13:22:49 -0400

    Group the benchmarks re: Identity.

--- benchmark/Bench/Interpret.hs
+++ benchmark/Bench/Interpret.hs
@@ -14,16 +14,18 @@ import Gauge hiding (benchmark)
 
 benchmark :: Gauge.Benchmark
 benchmark = bgroup "Interpret"
-  [ bench "InterpretC" $
-    whnf (\ n -> run $ execState @Int 0 $ runInterpret (\ _ (sig :: State Int m k) ctx -> case sig of
-      Get   -> gets @Int (<$ ctx)
-      Put s -> ctx <$ put s) $ modLoop n) n
-  , bench "InterpretStateC" $
-    whnf (\ n -> fst . run $ runInterpretState (\ _ (sig :: State Int m k) (s :: Int) ctx -> case sig of
-      Get   -> pure (s, s <$ ctx)
-      Put s -> pure (s, ctx)) 0 $ modLoop n) n
-  , bench "StateC" $
-    whnf (run . execState @Int 0 . modLoop) n
+  [ bgroup "Identity"
+    [ bench "InterpretC" $
+      whnf (\ n -> run $ execState @Int 0 $ runInterpret (\ _ (sig :: State Int m k) ctx -> case sig of
+        Get   -> gets @Int (<$ ctx)
+        Put s -> ctx <$ put s) $ modLoop n) n
+    , bench "InterpretStateC" $
+      whnf (\ n -> fst . run $ runInterpretState (\ _ (sig :: State Int m k) (s :: Int) ctx -> case sig of
+        Get   -> pure (s, s <$ ctx)
+        Put s -> pure (s, ctx)) 0 $ modLoop n) n
+    , bench "StateC" $
+      whnf (run . execState @Int 0 . modLoop) n
+    ]
   ]
   where
   n = 100000
