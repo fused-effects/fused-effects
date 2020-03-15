7e15e0ccaf5b6340b7bb2a2ef00e8e7374f976df
Rob Rix <rob.rix@me.com>
2020-03-15 11:23:33 -0400

    Factor out the number of iterations.

--- benchmark/Bench/Error.hs
+++ benchmark/Bench/Error.hs
@@ -13,22 +13,24 @@ import Gauge hiding (benchmark)
 
 benchmark :: Gauge.Benchmark
 benchmark = bgroup "Error"
-  [ bench "Either" $ whnf (errorLoop :: Int -> Either Int ()) 10000
+  [ bench "Either" $ whnf (errorLoop :: Int -> Either Int ()) n
   , bgroup "Identity"
-    [ bench "Church.ErrorC" $ whnf (run . Church.runError @Int (pure . Left) (pure . Right) . errorLoop) 10000
-    , bench "Cont.ErrorC"   $ whnf (run . Cont.runError @Int (pure . Right) . errorLoop) 10000
-    , bench "CPS.ErrorC"    $ whnf (run . CPS.runError @Int pure . errorLoop) 10000
-    , bench "Either.ErrorC" $ whnf (run . Either.runError @Int . errorLoop) 10000
-    , bench "ExceptT"       $ whnf (run . Except.runExceptT @Int . errorLoop) 10000
+    [ bench "Church.ErrorC" $ whnf (run . Church.runError @Int (pure . Left) (pure . Right) . errorLoop) n
+    , bench "Cont.ErrorC"   $ whnf (run . Cont.runError @Int (pure . Right) . errorLoop) n
+    , bench "CPS.ErrorC"    $ whnf (run . CPS.runError @Int pure . errorLoop) n
+    , bench "Either.ErrorC" $ whnf (run . Either.runError @Int . errorLoop) n
+    , bench "ExceptT"       $ whnf (run . Except.runExceptT @Int . errorLoop) n
     ]
   , bgroup "IO"
-    [ bench "Church.ErrorC IO" $ whnfAppIO (Church.runError @Int (pure . Left) (pure . Right) . errorLoop) 10000
-    , bench "Cont.ErrorC IO"   $ whnfAppIO (Cont.runError @Int (pure . Right) . errorLoop) 10000
-    , bench "CPS.ErrorC IO"    $ whnfAppIO (CPS.runError @Int pure . errorLoop) 10000
-    , bench "Either.ErrorC IO" $ whnfAppIO (Either.runError @Int . errorLoop) 10000
-    , bench "ExceptT IO"       $ whnfAppIO (Except.runExceptT @Int . errorLoop) 10000
+    [ bench "Church.ErrorC IO" $ whnfAppIO (Church.runError @Int (pure . Left) (pure . Right) . errorLoop) n
+    , bench "Cont.ErrorC IO"   $ whnfAppIO (Cont.runError @Int (pure . Right) . errorLoop) n
+    , bench "CPS.ErrorC IO"    $ whnfAppIO (CPS.runError @Int pure . errorLoop) n
+    , bench "Either.ErrorC IO" $ whnfAppIO (Either.runError @Int . errorLoop) n
+    , bench "ExceptT IO"       $ whnfAppIO (Except.runExceptT @Int . errorLoop) n
     ]
   ]
+  where
+  n = 10000
 
 errorLoop :: Has (Error Int) sig m => Int -> m ()
 errorLoop i = replicateM_ i (throwError i `catchError` pure @_ @Int)
