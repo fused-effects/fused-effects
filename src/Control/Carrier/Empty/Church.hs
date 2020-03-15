c3294850c4107ee75bc855598b0da5e084810565
Rob Rix <rob.rix@me.com>
2020-03-14 22:53:52 -0400

    Flip EmptyC.

--- src/Control/Carrier/Empty/Church.hs
+++ src/Control/Carrier/Empty/Church.hs
@@ -23,35 +23,35 @@ import Data.Coerce (coerce)
 import Data.Functor.Identity
 
 runEmpty :: m b -> (a -> m b) -> EmptyC m a -> m b
-runEmpty nil leaf (EmptyC m) = m leaf nil
+runEmpty nil leaf (EmptyC m) = m nil leaf
 {-# INLINE runEmpty #-}
 
-newtype EmptyC m a = EmptyC (forall b . (a -> m b) -> m b -> m b)
+newtype EmptyC m a = EmptyC (forall b . m b -> (a -> m b) -> m b)
   deriving (Functor)
 
 instance Applicative (EmptyC m) where
-  pure a = EmptyC $ \ leaf _ -> leaf a
+  pure a = EmptyC $ \ _ leaf -> leaf a
   {-# INLINE pure #-}
 
-  EmptyC f <*> EmptyC a = EmptyC $ \ leaf nil ->
-    f (\ f' -> a (leaf . f') nil) nil
+  EmptyC f <*> EmptyC a = EmptyC $ \ nil leaf ->
+    f nil (\ f' -> a nil (leaf . f'))
   {-# INLINE (<*>) #-}
 
-  liftA2 f (EmptyC a) (EmptyC b) = EmptyC $ \ leaf nil ->
-    a (\ a' -> b (leaf . f a') nil) nil
+  liftA2 f (EmptyC a) (EmptyC b) = EmptyC $ \ nil leaf ->
+    a nil (\ a' -> b nil (leaf . f a'))
   {-# INLINE liftA2 #-}
 
-  EmptyC a *> EmptyC b = EmptyC $ \ leaf nil ->
-    a (\ _ -> b leaf nil) nil
+  EmptyC a *> EmptyC b = EmptyC $ \ nil leaf ->
+    a nil (\ _ -> b nil leaf)
   {-# INLINE (*>) #-}
 
-  EmptyC a <* EmptyC b = EmptyC $ \ leaf nil ->
-    a (\ a' -> b (const (leaf a')) nil) nil
+  EmptyC a <* EmptyC b = EmptyC $ \ nil leaf ->
+    a nil (\ a' -> b nil (const (leaf a')))
   {-# INLINE (<*) #-}
 
 instance Monad (EmptyC m) where
-  EmptyC a >>= f = EmptyC $ \ leaf nil ->
-    a (runEmpty nil leaf . f) nil
+  EmptyC a >>= f = EmptyC $ \ nil leaf ->
+    a nil (runEmpty nil leaf . f)
   {-# INLINE (>>=) #-}
 
   (>>) = (*>)
@@ -62,7 +62,7 @@ instance Fail.MonadFail m => Fail.MonadFail (EmptyC m) where
   {-# INLINE fail #-}
 
 instance MonadFix m => MonadFix (EmptyC m) where
-  mfix f = EmptyC $ \ leaf nil ->
+  mfix f = EmptyC $ \ nil leaf ->
     mfix (toEmpty . f . run . fromEmpty)
     >>= run . runEmpty (coerce nil) (coerce leaf)
     where
@@ -75,11 +75,11 @@ instance MonadIO m => MonadIO (EmptyC m) where
   {-# INLINE liftIO #-}
 
 instance MonadTrans EmptyC where
-  lift m = EmptyC $ \ leaf _ -> m >>= leaf
+  lift m = EmptyC $ \ _ leaf -> m >>= leaf
   {-# INLINE lift #-}
 
 instance Algebra sig m => Algebra (Empty :+: sig) (EmptyC m) where
-  alg hdl sig ctx = EmptyC $ \ leaf nil -> case sig of
+  alg hdl sig ctx = EmptyC $ \ nil leaf -> case sig of
     L Empty -> nil
     R other -> thread (dst ~<~ hdl) other (pure ctx) >>= run . runEmpty (coerce nil) (coerce leaf)
     where
