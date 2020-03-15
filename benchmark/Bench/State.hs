d439508760cce53c170ad6ebe84d43ca1b3f18b4
Rob Rix <rob.rix@me.com>
2020-03-15 17:24:23 -0400

    Obviate the need for type applications.

--- benchmark/Bench/State.hs
+++ benchmark/Bench/State.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE TypeApplications #-}
 module Bench.State
 ( benchmark
 ) where
@@ -12,17 +11,18 @@ import Gauge hiding (benchmark)
 benchmark :: Benchmark
 benchmark = bgroup "State"
   [ bgroup "Identity"
-    [ bench "Church.StateC" $ whnf (run . C.Church.execState @Int 0 . modLoop) n
-    , bench "Lazy.StateC"   $ whnf (run . C.Lazy.execState @Int 0 . modLoop) n
-    , bench "Strict.StateC" $ whnf (run . C.Strict.execState @Int 0 . modLoop) n
+    [ bench "Church.StateC" $ whnf (run . C.Church.execState from . modLoop) n
+    , bench "Lazy.StateC"   $ whnf (run . C.Lazy.execState from . modLoop) n
+    , bench "Strict.StateC" $ whnf (run . C.Strict.execState from . modLoop) n
     ]
   , bgroup "IO"
-    [ bench "Church.StateC" $ whnfAppIO (C.Church.execState @Int 0 . modLoop) n
-    , bench "Lazy.StateC"   $ whnfAppIO (C.Lazy.execState @Int 0 . modLoop) n
-    , bench "Strict.StateC" $ whnfAppIO (C.Strict.execState @Int 0 . modLoop) n
+    [ bench "Church.StateC" $ whnfAppIO (C.Church.execState from . modLoop) n
+    , bench "Lazy.StateC"   $ whnfAppIO (C.Lazy.execState from . modLoop) n
+    , bench "Strict.StateC" $ whnfAppIO (C.Strict.execState from . modLoop) n
     ]
   ]
   where
+  from = 0 :: Int
   n = 100000
 
 modLoop :: Has (State Int) sig m => Int -> m ()
