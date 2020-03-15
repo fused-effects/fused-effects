ffca0bb78b08ac99ba71bb8de34be14f9dd31bb5
Rob Rix <rob.rix@me.com>
2020-03-15 10:44:35 -0400

    Derive a Functor instance for ErrorC.

--- src/Control/Carrier/Error/CPS.hs
+++ src/Control/Carrier/Error/CPS.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE RankNTypes #-}
 module Control.Carrier.Error.CPS
 ( -- * Error carrier
@@ -9,3 +10,4 @@ module Control.Carrier.Error.CPS
 import Control.Effect.Error
 
 newtype ErrorC e m a = ErrorC (forall b . (Either e a -> m b) -> m b)
+  deriving (Functor)
