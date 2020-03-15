a4b73f24d0af41cb5ff29ffdb5e45b7dd1891b5b
Rob Rix <rob.rix@me.com>
2020-03-15 10:44:16 -0400

    Define an ErrorC carrier.

--- src/Control/Carrier/Error/CPS.hs
+++ src/Control/Carrier/Error/CPS.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE RankNTypes #-}
 module Control.Carrier.Error.CPS
-( -- * Error effect
-  module Control.Effect.Error
+( -- * Error carrier
+  ErrorC(..)
+  -- * Error effect
+, module Control.Effect.Error
 ) where
 
 import Control.Effect.Error
+
+newtype ErrorC e m a = ErrorC (forall b . (Either e a -> m b) -> m b)
