af1ae12cc5b701d6ed1dde918ab62f40a6581a7e
Rob Rix <rob.rix@me.com>
2020-03-15 19:59:15 -0400

    Define an eval-style handler for FreshC.

--- src/Control/Carrier/Fresh/Church.hs
+++ src/Control/Carrier/Fresh/Church.hs
@@ -7,6 +7,7 @@
 module Control.Carrier.Fresh.Church
 ( -- * Fresh carrier
   runFresh
+, evalFresh
 , FreshC(FreshC)
   -- * Fresh effect
 , module Control.Effect.Fresh
@@ -36,6 +37,21 @@ runFresh :: (Int -> a -> m b) -> Int -> FreshC m a -> m b
 runFresh k n = runState k n . runFreshC
 {-# INLINE runFresh #-}
 
+-- | Run a 'Fresh' effect counting up from an initial value, and forgetting the final value.
+--
+-- @
+-- 'evalFresh' n ('pure' a) = 'pure' a
+-- @
+-- @
+-- 'evalFresh' n 'fresh' = 'pure' n
+-- @
+--
+-- @since 1.1.0.0
+evalFresh :: Applicative m => Int -> FreshC m a -> m a
+evalFresh n = evalState n . runFreshC
+{-# INLINE evalFresh #-}
+
+-- | @since 1.1.0.0
 newtype FreshC m a = FreshC { runFreshC :: StateC Int m a }
   deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO, MonadPlus, MonadTrans)
 
