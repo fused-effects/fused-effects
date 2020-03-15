3dfc07e5b23a655d63a88afdfb4c62e616af965b
Rob Rix <rob.rix@me.com>
2020-03-15 19:52:14 -0400

    Define a Fresh carrier.

--- src/Control/Carrier/Fresh/Church.hs
+++ src/Control/Carrier/Fresh/Church.hs
@@ -1,6 +1,11 @@
 module Control.Carrier.Fresh.Church
-( -- * Fresh effect
-  module Control.Effect.Fresh
+( -- * Fresh carrier
+  FreshC(..)
+  -- * Fresh effect
+, module Control.Effect.Fresh
 ) where
 
+import Control.Carrier.State.Church
 import Control.Effect.Fresh
+
+newtype FreshC m a = FreshC (StateC Int m a)
