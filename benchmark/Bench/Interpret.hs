dd41bff060d013e101ae15370437a4f057155aba
Rob Rix <rob.rix@me.com>
2020-03-15 13:24:59 -0400

    Benchmark in IO as well.

--- benchmark/Bench/Interpret.hs
+++ benchmark/Bench/Interpret.hs
@@ -26,6 +26,18 @@ benchmark = bgroup "Interpret"
     , bench "StateC" $
       whnf (run . execState @Int 0 . modLoop) n
     ]
+  , bgroup "IO"
+    [ bench "InterpretC" $
+      whnfAppIO (\ n -> execState @Int 0 $ runInterpret (\ _ (sig :: State Int m k) ctx -> case sig of
+        Get   -> gets @Int (<$ ctx)
+        Put s -> ctx <$ put s) $ modLoop n) n
+    , bench "InterpretStateC" $
+      whnfAppIO (\ n -> fmap fst $ runInterpretState (\ _ (sig :: State Int m k) (s :: Int) ctx -> case sig of
+        Get   -> pure (s, s <$ ctx)
+        Put s -> pure (s, ctx)) 0 $ modLoop n) n
+    , bench "StateC" $
+      whnfAppIO (execState @Int 0 . modLoop) n
+    ]
   ]
   where
   n = 100000
