0c5b7ca9f5a1e50cb9e6e112468e6b67b20fbec4
Rob Rix <rob.rix@me.com>
2020-03-15 16:02:23 -0400

    Rename some variables.

--- src/Control/Carrier/State/Lazy.hs
+++ src/Control/Carrier/State/Lazy.hs
@@ -81,10 +81,10 @@ instance Monad m => Applicative (StateC s m) where
   pure a = StateC $ \ s -> pure (s, a)
   {-# INLINE pure #-}
 
-  StateC mf <*> StateC mx = StateC $ \ s -> do
-    ~(s',  f) <- mf s
-    ~(s'', x) <- mx s'
-    pure (s'', f x)
+  StateC f <*> StateC a = StateC $ \ s -> do
+    ~(s',  f') <- f s
+    ~(s'', a') <- a s'
+    pure (s'', f' a')
   {-# INLINE (<*>) #-}
 
   StateC a *> StateC b = StateC $ \ s -> do
