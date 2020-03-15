b124b712682970c143d058638b182b983e92a08e
Rob Rix <rob.rix@me.com>
2020-03-15 09:51:06 -0400

    Add an ErrorC carrier type.

--- src/Control/Carrier/Error/Cont.hs
+++ src/Control/Carrier/Error/Cont.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE RankNTypes #-}
 module Control.Carrier.Error.Cont
-( -- * Error effect
-  module Control.Effect.Error
+( -- * Error carrier
+  ErrorC(..)
+  -- * Error effect
+, module Control.Effect.Error
 ) where
 
 import Control.Effect.Error
+
+newtype ErrorC e m a = ErrorC (forall b . (a -> m (Either e b)) -> m (Either e b))
