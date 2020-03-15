689cc2c23e99e6d2dca436511f09ca41c8ac7097
Rob Rix <rob.rix@me.com>
2020-03-15 17:27:11 -0400

    Benchmark strict & lazy StateT.

--- benchmark/Bench/State.hs
+++ benchmark/Bench/State.hs
@@ -5,6 +5,8 @@ module Bench.State
 import Control.Carrier.State.Church as C.Church
 import Control.Carrier.State.Lazy as C.Lazy
 import Control.Carrier.State.Strict as C.Strict
+import Control.Monad.Trans.State.Lazy as T.Lazy (execStateT)
+import Control.Monad.Trans.State.Strict as T.Strict (execStateT)
 import Data.Foldable (for_)
 import Gauge hiding (benchmark)
 
@@ -14,11 +16,15 @@ benchmark = bgroup "State"
     [ bench "Church.StateC" $ whnf (run . C.Church.execState from . modLoop) n
     , bench "Lazy.StateC"   $ whnf (run . C.Lazy.execState from . modLoop) n
     , bench "Strict.StateC" $ whnf (run . C.Strict.execState from . modLoop) n
+    , bench "Lazy.StateT"   $ whnf (run . flip T.Lazy.execStateT from . modLoop) n
+    , bench "Strict.StateT" $ whnf (run . flip T.Strict.execStateT from . modLoop) n
     ]
   , bgroup "IO"
     [ bench "Church.StateC" $ whnfAppIO (C.Church.execState from . modLoop) n
     , bench "Lazy.StateC"   $ whnfAppIO (C.Lazy.execState from . modLoop) n
     , bench "Strict.StateC" $ whnfAppIO (C.Strict.execState from . modLoop) n
+    , bench "Lazy.StateT"   $ whnfAppIO (flip T.Lazy.execStateT from . modLoop) n
+    , bench "Strict.StateT" $ whnfAppIO (flip T.Strict.execStateT from . modLoop) n
     ]
   ]
   where
