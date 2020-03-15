aa03d0516105dac0cb80779a239accf660e605f6
Rob Rix <rob.rix@me.com>
2020-03-15 17:19:18 -0400

    Add a benchmark of Lazy.StateC.

--- benchmark/Bench/State.hs
+++ benchmark/Bench/State.hs
@@ -3,13 +3,15 @@ module Bench.State
 ( benchmark
 ) where
 
+import Control.Carrier.State.Lazy as Lazy
 import Control.Carrier.State.Strict as Strict
 import Data.Foldable (for_)
 import Gauge hiding (benchmark)
 
 benchmark :: Benchmark
 benchmark = bgroup "State"
-  [ bench "Strict.StateC" $ whnf (run . Strict.execState @Int 0 . modLoop) n
+  [ bench "Lazy.StateC" $ whnf (run . Lazy.execState @Int 0 . modLoop) n
+  , bench "Strict.StateC" $ whnf (run . Strict.execState @Int 0 . modLoop) n
   ]
   where
   n = 100000
