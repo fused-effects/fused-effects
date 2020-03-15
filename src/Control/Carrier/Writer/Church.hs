5271f99b013078e099b844c031f7dc8dcb7f6ef5
Rob Rix <rob.rix@me.com>
2020-03-15 14:14:06 -0400

    Define a handler for WriterC.

--- src/Control/Carrier/Writer/Church.hs
+++ src/Control/Carrier/Writer/Church.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Carrier.Writer.Church
 ( -- * Writer carrier
-  WriterC(..)
+  runWriter
+, WriterC(WriterC)
   -- * Writer effect
 , module Control.Effect.Writer
 ) where
@@ -14,6 +15,10 @@ import Control.Monad.Fail as Fail
 import Control.Monad.Fix
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
+
+runWriter :: Monoid w => (w -> a -> m b) -> WriterC w m a -> m b
+runWriter k = runState k mempty . runWriterC
+{-# INLINE runWriter #-}
 
 newtype WriterC w m a = WriterC (StateC w m a)
   deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO, MonadPlus, MonadTrans)
