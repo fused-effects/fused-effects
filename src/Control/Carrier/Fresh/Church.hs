36a4dc3c7a53bcc896547eab76b9e21939f11b48
Rob Rix <rob.rix@me.com>
2020-03-15 19:53:01 -0400

    Derive a whole mess of instances.

--- src/Control/Carrier/Fresh/Church.hs
+++ src/Control/Carrier/Fresh/Church.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Carrier.Fresh.Church
 ( -- * Fresh carrier
   FreshC(..)
@@ -5,7 +6,14 @@ module Control.Carrier.Fresh.Church
 , module Control.Effect.Fresh
 ) where
 
+import Control.Applicative (Alternative)
 import Control.Carrier.State.Church
 import Control.Effect.Fresh
+import Control.Monad (MonadPlus)
+import Control.Monad.Fail as Fail
+import Control.Monad.Fix
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
 
 newtype FreshC m a = FreshC (StateC Int m a)
+  deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO, MonadPlus, MonadTrans)
