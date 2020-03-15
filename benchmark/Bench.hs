13b8dc329ff1c8e20ed3914ddb42620786c8c8fa
Rob Rix <rob.rix@me.com>
2020-03-15 13:31:29 -0400

    Move the Writer benchmarks into their own module.

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -1,18 +1,14 @@
-{-# LANGUAGE TypeApplications #-}
 module Main
 ( main
 ) where
 
-import           Control.Algebra
-import           Control.Carrier.Writer.Strict
-import           Control.Monad (replicateM_)
-import           Data.Monoid (Sum(..))
 import           Gauge
 
 import qualified Bench.Error as Error
 import qualified Bench.Interpret as Interpret
 import qualified Bench.NonDet as NonDet
 import qualified Bench.Reader as Reader
+import qualified Bench.Writer as Writer
 
 main :: IO ()
 main = defaultMain
@@ -20,13 +16,5 @@ main = defaultMain
   , Interpret.benchmark
   , NonDet.benchmark
   , Reader.benchmark
-
-  , bgroup "WriterC"
-    [ bench "100"   $ whnf (run . execWriter @(Sum Int) . tellLoop) 100
-    , bench "1000"  $ whnf (run . execWriter @(Sum Int) . tellLoop) 1000
-    , bench "10000" $ whnf (run . execWriter @(Sum Int) . tellLoop) 10000
-    ]
+  , Writer.benchmark
   ]
-
-tellLoop :: Has (Writer (Sum Int)) sig m => Int -> m ()
-tellLoop i = replicateM_ i (tell (Sum (1 :: Int)))
--- benchmark/Bench/Writer.hs
+++ benchmark/Bench/Writer.hs
@@ -1,2 +1,19 @@
+{-# LANGUAGE TypeApplications #-}
 module Bench.Writer
-() where
+( benchmark
+) where
+
+import Control.Carrier.Writer.Strict as C.Strict
+import Control.Monad (replicateM_)
+import Data.Monoid (Sum(..))
+import Gauge hiding (benchmark)
+
+benchmark :: Gauge.Benchmark
+benchmark = bgroup "Writer"
+  [ bench "Strict.WriterC" $ whnf (run . execWriter @(Sum Int) . tellLoop) n
+  ]
+  where
+  n = 100000
+
+tellLoop :: Has (Writer (Sum Int)) sig m => Int -> m ()
+tellLoop i = replicateM_ i (tell (Sum (1 :: Int)))
