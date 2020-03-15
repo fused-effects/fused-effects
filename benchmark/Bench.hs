244482cd7cc72c299a398983a6f2d7605f8e2439
Rob Rix <rob.rix@me.com>
2020-03-15 09:30:07 -0400

    Move the Error benchmarks into their own module.

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -13,8 +13,6 @@ module Main
 ) where
 
 import           Control.Algebra
-import           Control.Carrier.Error.Church as Church
-import           Control.Carrier.Error.Either as Either
 import           Control.Carrier.Interpret
 import           Control.Carrier.State.Strict
 import           Control.Carrier.Writer.Strict
@@ -22,13 +20,16 @@ import           Control.Monad (replicateM_)
 import           Data.Monoid (Sum(..))
 import           Gauge
 
+import qualified Bench.Error as Error
 import qualified Bench.NonDet as NonDet
 import qualified Bench.Reader as Reader
 
 main :: IO ()
 main = defaultMain
-  [ Reader.benchmark
+  [ Error.benchmark
   , NonDet.benchmark
+  , Reader.benchmark
+
   , bgroup "WriterC"
     [ bench "100"   $ whnf (run . execWriter @(Sum Int) . tellLoop) 100
     , bench "1000"  $ whnf (run . execWriter @(Sum Int) . tellLoop) 1000
@@ -52,24 +53,7 @@ main = defaultMain
       , bench "10000" $ whnf (run . execState @Int 0 . modLoop) 10000
       ]
     ]
-
-  , bgroup "Error"
-    [ bgroup "Either"
-      [ bench "100"   $ whnf (run . Either.runError @Int . errorLoop) 100
-      , bench "1000"  $ whnf (run . Either.runError @Int . errorLoop) 1000
-      , bench "10000" $ whnf (run . Either.runError @Int . errorLoop) 10000
-      ]
-    , bgroup "Church"
-      [ bench "100"   $ whnf (run . Church.runError @Int (pure . Left) (pure . Right) . errorLoop) 100
-      , bench "1000"  $ whnf (run . Church.runError @Int (pure . Left) (pure . Right) . errorLoop) 1000
-      , bench "10000" $ whnf (run . Church.runError @Int (pure . Left) (pure . Right) . errorLoop) 10000
-      ]
-    ]
   ]
-
-errorLoop :: Has (Error Int) sig m => Int -> m ()
-errorLoop i = replicateM_ i (throwError i `catchError` pure @_ @Int)
-{-# INLINE errorLoop #-}
 
 tellLoop :: Has (Writer (Sum Int)) sig m => Int -> m ()
 tellLoop i = replicateM_ i (tell (Sum (1 :: Int)))
--- benchmark/Bench/Error.hs
+++ benchmark/Bench/Error.hs
@@ -1,2 +1,27 @@
+{-# LANGUAGE TypeApplications #-}
 module Bench.Error
-() where
+( benchmark
+) where
+
+import Control.Carrier.Error.Church as Church
+import Control.Carrier.Error.Either as Either
+import Control.Monad (replicateM_)
+import Gauge hiding (benchmark)
+
+benchmark :: Gauge.Benchmark
+benchmark = bgroup "Error"
+  [ bgroup "Either"
+    [ bench "100"   $ whnf (run . Either.runError @Int . errorLoop) 100
+    , bench "1000"  $ whnf (run . Either.runError @Int . errorLoop) 1000
+    , bench "10000" $ whnf (run . Either.runError @Int . errorLoop) 10000
+    ]
+  , bgroup "Church"
+    [ bench "100"   $ whnf (run . Church.runError @Int (pure . Left) (pure . Right) . errorLoop) 100
+    , bench "1000"  $ whnf (run . Church.runError @Int (pure . Left) (pure . Right) . errorLoop) 1000
+    , bench "10000" $ whnf (run . Church.runError @Int (pure . Left) (pure . Right) . errorLoop) 10000
+    ]
+  ]
+
+errorLoop :: Has (Error Int) sig m => Int -> m ()
+errorLoop i = replicateM_ i (throwError i `catchError` pure @_ @Int)
+{-# INLINE errorLoop #-}
