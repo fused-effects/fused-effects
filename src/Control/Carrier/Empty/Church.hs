02ad1577cdedeeefb2ed205425874bb966c7dc35
Rob Rix <rob.rix@me.com>
2020-03-14 22:03:43 -0400

    Define an Empty carrier.

--- src/Control/Carrier/Empty/Church.hs
+++ src/Control/Carrier/Empty/Church.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE RankNTypes #-}
 module Control.Carrier.Empty.Church
-( -- * Empty effect
-  module Control.Effect.Empty
+( -- * Empty carrier
+  EmptyC(..)
+  -- * Empty effect
+, module Control.Effect.Empty
 ) where
 
 import Control.Effect.Empty
+
+newtype EmptyC m a = EmptyC (forall b . (a -> m b) -> m b -> m b)
