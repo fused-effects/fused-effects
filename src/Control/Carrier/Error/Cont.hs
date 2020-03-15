1940565c23865a8f83dfd24c02e242f5d2324c92
Rob Rix <rob.rix@me.com>
2020-03-15 09:52:50 -0400

    Define a handler for ErrorC.

--- src/Control/Carrier/Error/Cont.hs
+++ src/Control/Carrier/Error/Cont.hs
@@ -1,11 +1,16 @@
 {-# LANGUAGE RankNTypes #-}
 module Control.Carrier.Error.Cont
 ( -- * Error carrier
-  ErrorC(..)
+  runError
+, ErrorC(..)
   -- * Error effect
 , module Control.Effect.Error
 ) where
 
 import Control.Effect.Error
+
+runError :: (a -> m (Either e b)) -> ErrorC e m a -> m (Either e b)
+runError f (ErrorC m) = m f
+{-# INLINE runError #-}
 
 newtype ErrorC e m a = ErrorC (forall b . (a -> m (Either e b)) -> m (Either e b))
