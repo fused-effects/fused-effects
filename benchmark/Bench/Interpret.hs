dc55b935dc329320812da9013a2dad61ad9a81a1
Rob Rix <rob.rix@me.com>
2020-03-15 13:21:39 -0400

    Reformat.

--- benchmark/Bench/Interpret.hs
+++ benchmark/Bench/Interpret.hs
@@ -15,9 +15,13 @@ import Gauge hiding (benchmark)
 benchmark :: Gauge.Benchmark
 benchmark = bgroup "Interpret"
   [ bench "InterpretC" $
-    whnf (\n -> run $ execState @Int 0 $ runInterpret (\ _ (sig :: State Int m k) ctx -> case sig of { Get -> gets @Int (<$ ctx) ; Put s -> ctx <$ put s }) $ modLoop n) n
+    whnf (\ n -> run $ execState @Int 0 $ runInterpret (\ _ (sig :: State Int m k) ctx -> case sig of
+      Get   -> gets @Int (<$ ctx)
+      Put s -> ctx <$ put s) $ modLoop n) n
   , bench "InterpretStateC" $
-    whnf (\n -> fst . run $ runInterpretState (\ _ (sig :: State Int m k) (s :: Int) ctx -> case sig of { Get -> pure (s, s <$ ctx) ; Put s -> pure (s, ctx) }) 0 $ modLoop n) n
+    whnf (\ n -> fst . run $ runInterpretState (\ _ (sig :: State Int m k) (s :: Int) ctx -> case sig of
+      Get   -> pure (s, s <$ ctx)
+      Put s -> pure (s, ctx)) 0 $ modLoop n) n
   , bench "StateC" $
     whnf (run . execState @Int 0 . modLoop) n
   ]
