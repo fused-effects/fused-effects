82162ec7a232d420d7bdad26e9d130df4e5185d9
Rob Rix <rob.rix@me.com>
2020-03-15 17:18:55 -0400

    Add a benchmark of Strict.StateC.

--- benchmark/Bench/State.hs
+++ benchmark/Bench/State.hs
@@ -1,9 +1,19 @@
+{-# LANGUAGE TypeApplications #-}
 module Bench.State
 ( benchmark
 ) where
 
+import Control.Carrier.State.Strict as Strict
+import Data.Foldable (for_)
 import Gauge hiding (benchmark)
 
 benchmark :: Benchmark
 benchmark = bgroup "State"
-  []
+  [ bench "Strict.StateC" $ whnf (run . Strict.execState @Int 0 . modLoop) n
+  ]
+  where
+  n = 100000
+
+modLoop :: Has (State Int) sig m => Int -> m ()
+modLoop i = for_ [1..i] (modify . (+))
+{-# INLINE modLoop #-}
