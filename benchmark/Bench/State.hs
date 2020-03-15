05cb6f19d67ce1e90d2546c377daa1bc6cee6f0e
Rob Rix <rob.rix@me.com>
2020-03-15 17:22:35 -0400

    Rename the imports.

--- benchmark/Bench/State.hs
+++ benchmark/Bench/State.hs
@@ -3,23 +3,23 @@ module Bench.State
 ( benchmark
 ) where
 
-import Control.Carrier.State.Church as Church
-import Control.Carrier.State.Lazy as Lazy
-import Control.Carrier.State.Strict as Strict
+import Control.Carrier.State.Church as C.Church
+import Control.Carrier.State.Lazy as C.Lazy
+import Control.Carrier.State.Strict as C.Strict
 import Data.Foldable (for_)
 import Gauge hiding (benchmark)
 
 benchmark :: Benchmark
 benchmark = bgroup "State"
   [ bgroup "Identity"
-    [ bench "Church.StateC" $ whnf (run . Church.execState @Int 0 . modLoop) n
-    , bench "Lazy.StateC"   $ whnf (run . Lazy.execState @Int 0 . modLoop) n
-    , bench "Strict.StateC" $ whnf (run . Strict.execState @Int 0 . modLoop) n
+    [ bench "Church.StateC" $ whnf (run . C.Church.execState @Int 0 . modLoop) n
+    , bench "Lazy.StateC"   $ whnf (run . C.Lazy.execState @Int 0 . modLoop) n
+    , bench "Strict.StateC" $ whnf (run . C.Strict.execState @Int 0 . modLoop) n
     ]
   , bgroup "IO"
-    [ bench "Church.StateC" $ whnfAppIO (Church.execState @Int 0 . modLoop) n
-    , bench "Lazy.StateC"   $ whnfAppIO (Lazy.execState @Int 0 . modLoop) n
-    , bench "Strict.StateC" $ whnfAppIO (Strict.execState @Int 0 . modLoop) n
+    [ bench "Church.StateC" $ whnfAppIO (C.Church.execState @Int 0 . modLoop) n
+    , bench "Lazy.StateC"   $ whnfAppIO (C.Lazy.execState @Int 0 . modLoop) n
+    , bench "Strict.StateC" $ whnfAppIO (C.Strict.execState @Int 0 . modLoop) n
     ]
   ]
   where
