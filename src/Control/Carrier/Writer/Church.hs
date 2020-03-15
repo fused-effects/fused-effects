3f687f8cdf528903aa2360f36919da6f48340050
Rob Rix <rob.rix@me.com>
2020-03-15 14:04:32 -0400

    Define a WriterC newtype.

--- src/Control/Carrier/Writer/Church.hs
+++ src/Control/Carrier/Writer/Church.hs
@@ -1,6 +1,11 @@
 module Control.Carrier.Writer.Church
-( -- * Writer effect
-  module Control.Effect.Writer
+( -- * Writer carrier
+  WriterC(..)
+  -- * Writer effect
+, module Control.Effect.Writer
 ) where
 
+import Control.Carrier.State.Church
 import Control.Effect.Writer
+
+newtype WriterC w m a = WriterC (StateC w m a)
