67d24d371da2358a5a0ac50b8d01cbde4e444730
Rob Rix <rob.rix@me.com>
2020-03-15 13:35:32 -0400

    Benchmark the strict & lazy WriterT algebras.

--- benchmark/Bench/Writer.hs
+++ benchmark/Bench/Writer.hs
@@ -4,13 +4,17 @@ module Bench.Writer
 ) where
 
 import Control.Carrier.Writer.Strict as C.Strict
+import Control.Monad.Trans.Writer.Lazy as T.Lazy (execWriterT)
+import Control.Monad.Trans.Writer.Strict as T.Strict (execWriterT)
 import Data.Foldable (for_)
 import Data.Monoid (Sum(..))
 import Gauge hiding (benchmark)
 
 benchmark :: Gauge.Benchmark
 benchmark = bgroup "Writer"
-  [ bench "Strict.WriterC" $ whnf (run . execWriter @(Sum Int) . tellLoop) n
+  [ bench "Strict.WriterC" $ whnf (run . C.Strict.execWriter @(Sum Int) . tellLoop) n
+  , bench "Lazy.WriterT" $ whnf (run . T.Lazy.execWriterT @_ @(Sum Int) . tellLoop) n
+  , bench "Strict.WriterT" $ whnf (run . T.Strict.execWriterT @_ @(Sum Int) . tellLoop) n
   ]
   where
   n = 100000
