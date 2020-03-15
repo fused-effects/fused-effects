79142e87af96c68ed1705309f1d623bf055243ba
Rob Rix <rob.rix@me.com>
2020-03-15 14:05:37 -0400

    Derive a lot of instances.

--- src/Control/Carrier/Writer/Church.hs
+++ src/Control/Carrier/Writer/Church.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Carrier.Writer.Church
 ( -- * Writer carrier
   WriterC(..)
@@ -5,7 +6,14 @@ module Control.Carrier.Writer.Church
 , module Control.Effect.Writer
 ) where
 
+import Control.Applicative (Alternative)
 import Control.Carrier.State.Church
 import Control.Effect.Writer
+import Control.Monad (MonadPlus)
+import Control.Monad.Fail as Fail
+import Control.Monad.Fix
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
 
 newtype WriterC w m a = WriterC (StateC w m a)
+  deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO, MonadPlus, MonadTrans)
