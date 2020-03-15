8458c893c4a4356c5862cc5e5316cc3376e04560
Rob Rix <rob.rix@me.com>
2020-03-15 14:27:54 -0400

    Define an Algebra instance for Church.WriterC.

--- src/Control/Carrier/Writer/Church.hs
+++ src/Control/Carrier/Writer/Church.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Control.Carrier.Writer.Church
 ( -- * Writer carrier
   runWriter
@@ -7,6 +12,7 @@ module Control.Carrier.Writer.Church
 , module Control.Effect.Writer
 ) where
 
+import Control.Algebra
 import Control.Applicative (Alternative)
 import Control.Carrier.State.Church
 import Control.Effect.Writer
@@ -20,5 +26,14 @@ runWriter :: Monoid w => (w -> a -> m b) -> WriterC w m a -> m b
 runWriter k = runState k mempty . runWriterC
 {-# INLINE runWriter #-}
 
-newtype WriterC w m a = WriterC (StateC w m a)
+newtype WriterC w m a = WriterC { runWriterC :: StateC w m a }
   deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO, MonadPlus, MonadTrans)
+
+instance (Algebra sig m, Monoid w) => Algebra (Writer w :+: sig) (WriterC w m) where
+  alg hdl sig ctx = WriterC $ case sig of
+    L writer -> StateC $ \ k w -> case writer of
+      Tell w'    -> (k $! mappend w w') ctx
+      Listen   m -> runWriter (\ w' a -> (k $! mappend w w') ((,) w' <$> a)) (hdl (m <$ ctx))
+      Censor f m -> runWriter (\ w' a -> (k $! mappend w (f $! w')) a) (hdl (m <$ ctx))
+    R other  -> alg (runWriterC . hdl) (R other) ctx
+  {-# INLINE alg #-}
