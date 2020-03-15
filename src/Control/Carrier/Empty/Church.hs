279759e16f5b430911a758fa8020d079707d68d1
Rob Rix <rob.rix@me.com>
2020-03-14 22:08:36 -0400

    Define an Applicative instance for EmptyC.

--- src/Control/Carrier/Empty/Church.hs
+++ src/Control/Carrier/Empty/Church.hs
@@ -11,3 +11,11 @@ import Control.Effect.Empty
 
 newtype EmptyC m a = EmptyC (forall b . (a -> m b) -> m b -> m b)
   deriving (Functor)
+
+instance Applicative (EmptyC m) where
+  pure a = EmptyC $ \ leaf _ -> leaf a
+  {-# INLINE pure #-}
+
+  EmptyC f <*> EmptyC a = EmptyC $ \ leaf nil ->
+    f (\ f' -> a (leaf . f') nil) nil
+  {-# INLINE (<*>) #-}
