260f350db4581ed6af9a328299ea156d28ca50f3
Rob Rix <rob.rix@me.com>
2022-02-27 16:17:18 -0500

    Typo.

--- src/Control/Effect/Choose.hs
+++ src/Control/Effect/Choose.hs
@@ -94,7 +94,7 @@ many a = go where go = (:) <$> a <*> go <|> pure []
 --
 -- @
 -- 'some' m = (:) '<$>' m '<*>' 'many' m
--- @-
+-- @
 --
 -- @since 1.0.0.0
 some :: Has Choose sig m => m a -> m [a]
