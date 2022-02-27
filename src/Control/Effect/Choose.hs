e584f4810f0c2885f031d679d5a6da5c0cd7794c
Rob Rix <rob.rix@me.com>
2022-02-27 15:58:34 -0500

    Derive an Algebra instance for Choosing.

--- src/Control/Effect/Choose.hs
+++ src/Control/Effect/Choose.hs
@@ -108,7 +108,7 @@ some1 a = (:|) <$> a <*> many a
 
 -- | @since 1.0.0.0
 newtype Choosing m a = Choosing { getChoosing :: m a }
-  deriving (Applicative, Functor, Monad)
+  deriving (Algebra sig, Applicative, Functor, Monad)
 
 instance Has Choose sig m => S.Semigroup (Choosing m a) where
   Choosing m1 <> Choosing m2 = Choosing (m1 <|> m2)
